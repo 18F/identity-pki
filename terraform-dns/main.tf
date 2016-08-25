19433b6dc42f94ac2edfc75cfaaa72fdfd207f1f
Justin Grevich <justin@grevi.ch>
2016-08-25 14:12:31 -0700

    Add DNS TXT records so we can use login.gov with Mandrill
    __Why__
    This is required in order to configure login.gov as a sending domain with
    Mandrill and to receive email on that domain using their service. It's also a
    great security measure to have in place for any email communications.
    
    __How__
    Add the appropriate TXT records to the login.gov r53 zone to support DKIM + SPF.

--- terraform-dns/main.tf
+++ terraform-dns/main.tf
@@ -12,3 +12,19 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region = "${var.region}"
 }
+
+resource "aws_route53_record" "txt_mandrill_dkim" {
+  name = "mandrill._domainkey.login.gov"
+  records = ["v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCrLHiExVd55zd/IQ/J/mRwSRMAocV/hMB3jXwaHH36d9NaVynQFYV8NaWi69c1veUtRzGt7yAioXqLj7Z4TeEUoOLgrKsn8YnckGs9i3B3tVFB+Ch/4mPhXWiNfNdynHWBcPcbJ8kjEQ2U8y78dHZj1YeRXXVvWob2OaKynO8/lQIDAQAB;"]
+  ttl = "300"
+  type = "TXT"
+  zone_id = "${var.zone_id}"
+}
+
+resource "aws_route53_record" "txt_mandrill_spf" {
+  name = "login.gov"
+  records = ["v=spf1 include:spf.mandrillapp.com ?all"]
+  ttl = "300"
+  type = "TXT"
+  zone_id = "${var.zone_id}"
+}
