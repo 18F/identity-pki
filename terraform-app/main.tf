c1550feee45a61c4eb7904de67a4c73bb484a123
Justin Grevich <justin@grevi.ch>
2016-10-24 15:46:37 -0700

    Update default parameter group for redis cluster
    https://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/ParameterGroups.Redis.html#ParameterGroups.Redis.3-2-4

--- terraform-app/main.tf
+++ terraform-app/main.tf
@@ -44,7 +44,7 @@ resource "aws_elasticache_cluster" "app" {
   engine = "redis"
   node_type = "cache.t2.micro"
   num_cache_nodes = 1
-  parameter_group_name = "default.redis2.8"
+  parameter_group_name = "default.redis3.2"
   port = 6379
   security_group_ids = ["${aws_security_group.cache.id}"]
   subnet_group_name = "${aws_elasticache_subnet_group.app.name}"
