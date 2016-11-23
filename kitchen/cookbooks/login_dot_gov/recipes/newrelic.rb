54587e8e786a8dd9dbfd9323c1d55c025bc4b25f
Justin Grevich <justin@grevi.ch>
2016-11-23 15:37:28 -0800

    Refactor how we set the license key for nrsysmond.cfg (#26)
    __Why__
    
    Recent updates to the newrelic cookbook caused nrsysmond to be started before
    the license key was set with the login_dot_gov::newrelic recipe.
    
    __How__
    
    Leverage the notifications system in chef to have our custom recipe subscribe to
    changes in the template for nrsysmond.cfg.

--- kitchen/cookbooks/login_dot_gov/recipes/newrelic.rb
+++ kitchen/cookbooks/login_dot_gov/recipes/newrelic.rb
@@ -2,7 +2,8 @@ include_recipe "newrelic"
 
 license = Chef::EncryptedDataBagItem.load('config', 'app')["#{node.chef_environment}"]['newrelic_license_key']
 
-execute 'add NR license to sysmon config' do
-  command "sed -i s/^license_key=/license_key=#{license}/ /etc/newrelic/nrsysmond.cfg"
-  notifies :restart, "service[#{node['newrelic']['server_monitor_agent']['service_name']}]"
+execute 'nrsysmond-config --set license_key=' do
+  action :nothing
+  command "nrsysmond-config --set license_key=#{license}"
+  subscribes :run,  'template[/etc/newrelic/nrsysmond.cfg]', :immediately
 end
