392b7e3e2c464edc364ff3f076e78fd0b0b65248
Justin Grevich <justin@grevi.ch>
2016-12-06 15:00:02 -0800

    Provide SP_NAME and SP_PASS env vars to worker hosts
    __Why__
    The sidekiq process on worker hosts needs access to the SP_NAME and SP_PASS env
    variables.
    
    __How__
    As a quick fix I am copying code from the sp_rails recipe that will generate a
    .bash_profile with the required variables.

--- kitchen/cookbooks/login_dot_gov/recipes/idp_worker.rb
+++ kitchen/cookbooks/login_dot_gov/recipes/idp_worker.rb
@@ -12,6 +12,16 @@ service 'sidekiq' do
   stop_command '/usr/bin/monit stop sidekiq_idp_production0'
 end
 
+encrypted_config = Chef::EncryptedDataBagItem.load('config', 'app')["#{node.chef_environment}"]
+
+template "/home/ubuntu/.bash_profile" do
+  owner node['login_dot_gov']['system_user']
+  variables({
+    sp_name: '18f',
+    sp_pass: encrypted_config['db_password']
+  })
+end
+
 template '/etc/monit/monitrc' do
   notifies :restart, 'service[monit]'
 end
