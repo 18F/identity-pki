b35deb488070f549645df31f1040bd94dfc68254
Justin Grevich <justin@grevi.ch>
2017-01-06 11:46:20 -0800

    Add a comment to the r53 zone with the project and env name (#64)
    __Why__
    
    * It was not easy to distinguish which env each zone belonged to when viewing in
    the console or CLI.
    
    __How__
    
    * Add the project and env name to the comment field of the r53 zone.

--- terraform-app/dns.tf
+++ terraform-app/dns.tf
@@ -1,11 +1,13 @@
 # Set up an internal dns zone that we can use for service discovery
 
 resource "aws_route53_zone" "internal" {
+  comment = "${var.name}-zone-${var.env_name}"
   name = "login.gov.internal"
   vpc_id = "${aws_vpc.default.id}"
 }
 
 resource "aws_route53_zone" "internal-reverse" {
+  comment = "${var.name}-zone-${var.env_name}"
   name = "16.172.in-addr.arpa"
   vpc_id = "${aws_vpc.default.id}"
 }
@@ -22,4 +24,3 @@ resource "aws_route53_record" "internal-ns" {
     "${aws_route53_zone.internal.name_servers.3}"
   ]
 }
-
