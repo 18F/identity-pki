ddaa5e460c9aa6e739e4882cd6489c5536ed5a0f
Justin Grevich <justin@grevi.ch>
2017-01-18 16:38:23 -0700

    Add log validation to cloudtrail and SS encrytion to the s3 bucket (#87)
    __Why__
    
    * Log validation helps us to make sure that the logs were not tampered with
    after delivery by cloudtrail. The s3 bucket where the logs were stored was not
    using the built in server-side encryption on the s3 bucket.
    
    __How__
    
    * Add the enable_log_file_validation and server_side_encryption arguments to the
    cloudtrail and s3 bucket resources, respectively, in Terraform.

--- terraform-cloudtrail/main.tf
+++ terraform-cloudtrail/main.tf
@@ -83,11 +83,12 @@ resource "aws_s3_bucket" "cloudtrail" {
   bucket = "login-gov-cloudtrail-${data.aws_caller_identity.current.account_id}"
   force_destroy = true
   policy = "${data.aws_iam_policy_document.cloudtrail.json}"
+  server_side_encryption = true
 
   lifecycle_rule {
     id = "logexpire"
-    prefix = ""
     enabled = true
+    prefix = ""
 
     expiration {
       days = 30
@@ -96,8 +97,8 @@ resource "aws_s3_bucket" "cloudtrail" {
 }
 
 resource "aws_cloudtrail" "cloudtrail" {
+  enable_log_file_validation = true
+  include_global_service_events = false
   name = "login-gov-cloudtrail"
   s3_bucket_name = "${aws_s3_bucket.cloudtrail.id}"
-  include_global_service_events = false
 }
-
