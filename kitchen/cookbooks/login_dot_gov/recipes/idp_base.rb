ba50c852bc1b8ed1d2d25bcf110f7487712da890
Amos Stone <amos@amosarts.com>
2017-01-23 16:14:47 -0800

    Reverts schema:load for db:migrate (#111)

--- kitchen/cookbooks/login_dot_gov/recipes/idp_base.rb
+++ kitchen/cookbooks/login_dot_gov/recipes/idp_base.rb
@@ -95,7 +95,7 @@ application release_path do
 
   execute 'chown -R ubuntu /home/ubuntu/.bundle /usr/local/src'
 
-  execute "/opt/ruby_build/builds/#{node['login_dot_gov']['ruby_version']}/bin/bundle exec rake db:create db:schema:load db:seed --trac" do
+  execute "/opt/ruby_build/builds/#{node['login_dot_gov']['ruby_version']}/bin/bundle exec rake db:create db:migrate db:seed --trac" do
     cwd '/srv/idp/releases/chef'
     environment({
       'RAILS_ENV' => "production"
