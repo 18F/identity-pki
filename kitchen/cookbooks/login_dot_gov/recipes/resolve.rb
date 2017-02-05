e9b77df0ac09ac59e620520bc331b01c7fe302bd
Justin Grevich <justin@grevi.ch>
2017-02-04 22:52:19 -0800

    Add login.gov.internal to resolve.conf's search domains (#144)
    __Why__
    
    * This allows us to connect to hosts without using the fqdn. `ssh ubuntu@worker`
    
    __How__
    
    * Add a template to configure `/etc/resolveconf/resolve.conf.d/tail` and add
    login.gov.internal as a search domain.

---
+++ kitchen/cookbooks/login_dot_gov/recipes/resolve.rb
@@ -0,0 +1,5 @@
+# add login.gov.internal as a search domain
+template '/etc/resolvconf/resolv.conf.d/tail'
+
+# force an update of resolve.conf based on settings in /etc/resolveconf/*
+execute '/sbin/resolvconf -u'
