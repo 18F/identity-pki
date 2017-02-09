4254de3e021279ee3a1df1d235f3092b40e4ddf2
Tim Spencer <timothy.spencer@gsa.gov>
2017-02-08 17:12:45 -0800

    Remove ES from audit logs (#157)
    __Why__
    
    * ES generates waaaaay too many audit logs.  They are not useful, and they
      fill up the ES disk..
    
    __How__
    
    * Edit the audit.rules.  Ugly, but until we get this into the base image,
      it will have to do.

--- kitchen/cookbooks/identity-elk/recipes/es.rb
+++ kitchen/cookbooks/identity-elk/recipes/es.rb
@@ -156,3 +156,16 @@ node['elk']['indextypes'].each do |index|
   end
 end
 
+# turn off audit logging of elasticsearch activities in the elasticsearch dir
+# (this totally spams the logs)
+# XXX remove this as soon as we get this into our base AMI and it is deployed everywhere!
+#auditctl -A exit,never -F dir=/var/lib/elasticsearch/nodes -F uid=elasticsearch
+ruby_block 'removeESauditlogs' do
+  block do
+    fe = Chef::Util::FileEdit.new('/etc/audit/audit.rules')
+    fe.insert_line_after_match(/^-f 1$/, '-a exit,never -F dir=/var/lib/elasticsearch/nodes -F uid=elasticsearch')
+    fe.write_file
+  end
+  not_if { File.readlines('/etc/audit/audit.rules').grep(/dir=\/var\/lib\/elasticsearch\/nodes -F uid=elasticsearch/).any? }
+end
+
