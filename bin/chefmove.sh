dbc34c2941bf0f9a4f1757b6eaf47ba32eb8fdac
Justin Grevich <justin@grevi.ch>
2017-03-08 19:11:47 -0800

    Some fixes and refactoring of the chefmove.sh script (#194)
    __Why__
    
    * This was failing for me and I saw a few places that I could refactor and make
    things better.
    
    __How__
    
    * Provide code comments for every step, assume no bash experience
    * Remove requirement for jumphost hostname argument
    * Provide more explanatory error messages
    * Move env config settings to dedicated folder ~/.env
    * Make ssh remote command more readable by breaking down to multiple lines
    * Use timestamp instead of PID for unique filenames. PID does not work when
    ControlMaster is set in one's ssh config
    * Use quiet mode with commands to make errors more clear (less signal:noise)
    * Refactor use of tar (zip in tar command and break down file list to multiple
    lines)
    * Rsync local repo instead of cloning (this can copy .gitignored files and allow
    us to merge into our home folder to simplify our operations)
    * Refactor symlink creating (remove rm command and use -f in ln)
    * Specify username when rsync/scp/ssh'ing to the jumphost

--- bin/chefmove.sh
+++ bin/chefmove.sh
@@ -1,34 +1,94 @@
 #!/bin/sh
+
+# define usage function
 usage() {
-    echo "usage:  $0 <env> <jumphost> <username>"
-    exit 1
+  echo "usage:  $0 <env> <username>"
+  exit 1
 }
-if [ -z "$1" -o -z "$2" -o -z "$3"] ; then
-    usage
+
+# check to assure that required arguments exist
+#   -z assures that the variable is set
+#   -o is used for additonal arguments in the condition)
+if [ -z $1 -o -z $2 ] ; then
+  echo "required argument not found, you must provide an environment and username"
+  usage
 fi
+
+# rsync local repo (excluding .git folder) to your user's home folder on the jumpbox
+rsync -auvq --exclude='.git' . ${2}@jumphost.${1}.login.gov:
+
+# define list of files to compress in an archive
 FILES="
  .chef/knife-$1.rb
  .chef/$1-databag.key
  .chef/$1-login-dev-validator.pem
- .chef/$3-$1.pem
- ./$1-env.sh
+ .chef/$2-$1.pem
+ .env/$1.sh
 "
+
+# change to home directory since above files are assumed to be in your user's local home folder
 cd ~
+
+# check to make sure that each file exists (-f checks for file presence, ! negates the condition)
 for i in $FILES ; do
-    if [ ! -f $i ] ; then
-        usage
-    fi
+  if [ ! -f "$i" ] ; then
+    echo "required file not found. This requires the following files to be in place:\n ${FILES}"
+    usage
+  fi
 done
-rm -f /tmp/$$.tar.gz
-tar cf - $FILES | gzip -9 > /tmp/$$.tar.gz
-scp /tmp/$$.tar.gz $2:.
-rm -f /tmp/$$.tar.gz
-ssh -A $2 "tar zxpf $$.tar.gz ; rm $$.tar.gz ; mkdir .ssh ; chmod 700 .ssh ; git clone git@github.com:18F/identity-devops.git ; rm -f .chef/knife.rb ; ln -s .chef/knife-$1.rb .chef/knife.rb"
-rm -f /tmp/$$.config
-cat <<EOF >/tmp/$$.config
+
+# generate timestamp to use for unique filenames
+TIMESTAMP=`date "+%Y%m%d%H%M%S"`
+
+# create var for archive filename
+ARCHIVE_FILENAME="login-conf.${TIMESTAMP}.tgz"
+
+# remove any archives with the same name
+rm -f /tmp/${ARCHIVE_FILENAME}
+# tar and gunzip the list of files and name using the PID of the script ($$)
+tar czf /tmp/${ARCHIVE_FILENAME} $FILES
+# copy archive to jumphost user's home folder
+scp -q /tmp/${ARCHIVE_FILENAME} ${2}@jumphost.${1}.login.gov:.
+# remove the local copy of the archive
+rm -f /tmp/${ARCHIVE_FILENAME}
+
+# build remote commands string
+#  - extract the archive
+RC="tar xzpf ${ARCHIVE_FILENAME};"
+#  - delete the archive
+RC="$RC rm ${ARCHIVE_FILENAME};"
+#  - create a .ssh directory and do not fail if one already exists (-p)
+RC="$RC mkdir -p .ssh;"
+#  - set proper perms on the .ssh directory
+RC="$RC chmod 700 .ssh;"
+
+#  - add github.com host key to known_hosts file
+# RC="$RC ssh-keyscan -H github.com >> .ssh/known_hosts;"
+#  - clone the identity-devops repo
+# RC="$RC git clone git@github.com:18F/identity-devops.git /tmp;"
+
+#  - create a new symlink to the correct env
+RC="$RC ln -fs .chef/knife-$1.rb .chef/knife.rb;"
+
+# execute remote commands on the jumphost
+ssh -q -A ${2}@jumphost.${1}.login.gov ${RC}
+
+# create unique name for tmp ssh config
+SSH_CONFIG_FILENAME="login-ssh.${TIMESTAMP}.config"
+
+# remove tmp ssh config file if it already exists
+rm -f /tmp/${SSH_CONFIG_FILENAME}
+
+# add contents to tmp ssh config
+cat <<EOF >/tmp/${SSH_CONFIG_FILENAME}
 Host *
     User ubuntu
 EOF
-scp /tmp/$$.config $2:.ssh/config
-rm -f /tmp/$$.config
-echo "XXX:  you will probably need to edit .chef/knife.rb and $1-env.sh to have proper paths in it"
+
+# copy .ssh config to jumpbox users's .ssh folder
+scp -q /tmp/${SSH_CONFIG_FILENAME} ${2}@jumphost.${1}.login.gov:.ssh/config
+
+# remove local tmp ssh config
+rm -f /tmp/${SSH_CONFIG_FILENAME}
+
+echo "\nIf you see no errors above then SUCCESS!\n\nNOTE: you will probably need to edit .chef/knife.rb and .env/$1.sh to have proper paths in it\n"
