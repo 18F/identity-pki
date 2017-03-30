c7896cb51af1199f7a1b17eaeb83b5c95049a262
Justin Grevich <justin@grevi.ch>
2017-03-30 13:29:54 -0700

    Add DNS records for GSA GSuite incoming mail support (#230)
    __Why__
    
    * We need to add a few DNS records in order to use the GSuite enterprise Gmail
    for incoming email.
    
    __How__
    
    * Add TXT records for SPF and Google Site Verification
    * Add MX records for google servers
    
    __Misc Fixes/Too lazy for new PR__
    
    * Modified the preview.login.gov record so that it's an A record with alias as
    suggested by: http://docs.aws.amazon.com/Route53/latest/DeveloperGuide/routing-to-cloudfront-distribution.html
    * Reorded TF code blocks alphabetically along with the internal attributes for each block.

--- terraform-dns/main.tf
+++ terraform-dns/main.tf
@@ -2,8 +2,69 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_route53_zone" "primary" {
+resource "aws_route53_record" "a_root" {
   name = "login.gov"
+  type = "A"
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  alias {
+    evaluate_target_health = false
+    name = "dz4hcx2jg1w8s.cloudfront.net"
+    zone_id = "Z2FDTNDATAQYW2"
+  }
+}
+
+resource "aws_route53_record" "a_marketing_preview" {
+  name = "preview.login.gov"
+  type = "CNAME"
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  alias {
+    evaluate_target_health = false
+    name = "d1xv3mp8jrwxno.cloudfront.net"
+    zone_id = "Z2FDTNDATAQYW2"
+  }
+}
+
+resource "aws_route53_record" "a_www" {
+  name = "www.login.gov"
+  type = "A"
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  alias {
+    evaluate_target_health = false
+    name = "dz4hcx2jg1w8s.cloudfront.net"
+    zone_id = "Z2FDTNDATAQYW2"
+  }
+}
+
+resource "aws_route53_record" "mx_google" {
+  name = "login.gov"
+  records = [
+    "10 aspmx.l.google.com.",
+    "20 alt1.aspmx.l.google.com.",
+    "20 alt2.aspmx.l.google.com.",
+    "30 aspmx2.googlemail.com.",
+    "30 aspmx3.googlemail.com.",
+    "30 aspmx4.googlemail.com.",
+    "30 aspmx5.googlemail.com."
+  ]
+  ttl = "300"
+  type = "TXT"
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+}
+
+resource "aws_route53_record" "txt_google_site_verification" {
+  name = "login.gov"
+  records = ["google-site-verification=x8WM0Sy9Q4EmkHypuULXjTibNOJmPEoOxDGUmBppws8"]
+  ttl = "300"
+  type = "TXT"
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+}
+
+resource "aws_route53_record" "txt_google_spf" {
+  name = "login.gov"
+  records = ["v=spf1 a mx include:gsa.gov ~all"]
+  ttl = "300"
+  type = "TXT"
+  zone_id = "${aws_route53_zone.primary.zone_id}"
 }
 
 resource "aws_route53_record" "txt_mandrill_dkim" {
@@ -22,51 +83,6 @@ resource "aws_route53_record" "txt_mandrill_spf" {
   zone_id = "${aws_route53_zone.primary.zone_id}"
 }
 
-resource "aws_route53_record" "mx" {
+resource "aws_route53_zone" "primary" {
   name = "login.gov"
-  records = ["10 30288227.in1.mandrillapp.com", "20 30288227.in2.mandrillapp.com"]
-  ttl = "300"
-  type = "MX"
-  zone_id = "${aws_route53_zone.primary.zone_id}"
-}
-
-
-# hardcode in www
-resource "aws_route53_record" "a_root" {
-  name = "login.gov"
-  type = "A"
-  zone_id = "${aws_route53_zone.primary.zone_id}"
-  alias {
-    name = "dz4hcx2jg1w8s.cloudfront.net"
-    zone_id = "Z2FDTNDATAQYW2"
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "c_www" {
-  name = "www.login.gov"
-  type = "A"
-  zone_id = "${aws_route53_zone.primary.zone_id}"
-  alias {
-    name = "dz4hcx2jg1w8s.cloudfront.net"
-    zone_id = "Z2FDTNDATAQYW2"
-    evaluate_target_health = false
-  }
-}
-
-# hardcode in old chef
-resource "aws_route53_record" "a_chef" {
-  name = "chef.login.gov"
-  records = ["35.162.6.23"]
-  ttl = "300"
-  type = "A"
-  zone_id = "${aws_route53_zone.primary.zone_id}"
-}
-
-resource "aws_route53_record" "c_marketing_preview" {
-  name = "preview.login.gov"
-  records = ["d1xv3mp8jrwxno.cloudfront.net"]
-  ttl = "300"
-  type = "CNAME"
-  zone_id = "${aws_route53_zone.primary.zone_id}"
 }
