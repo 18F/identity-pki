357074df4482d3441e9827d03d77c254a45ca6c8
Tim Spencer <timothy.spencer@gsa.gov>
2017-04-03 20:12:59 -0700

    Fix problems that cause terraform/AWS to be sad (#247)
    __Why__
    
    * Because TXT records need to be one record
    * The MX record needs to be an MX record, not TXT
    * The preview site is gone, and that was causing problems
    
    __How__
    
    * Wiggle fingers in proper ways

--- terraform-dns/main.tf
+++ terraform-dns/main.tf
@@ -13,16 +13,16 @@ resource "aws_route53_record" "a_root" {
   }
 }
 
-resource "aws_route53_record" "a_marketing_preview" {
-  name = "preview.login.gov"
-  type = "CNAME"
-  zone_id = "${aws_route53_zone.primary.zone_id}"
-  alias {
-    evaluate_target_health = false
-    name = "d1xv3mp8jrwxno.cloudfront.net"
-    zone_id = "Z2FDTNDATAQYW2"
-  }
-}
+#resource "aws_route53_record" "a_marketing_preview" {
+#  name = "preview.login.gov"
+#  type = "CNAME"
+#  zone_id = "${aws_route53_zone.primary.zone_id}"
+#  alias {
+#    evaluate_target_health = false
+#    name = "d1xv3mp8jrwxno.cloudfront.net"
+#    zone_id = "Z2FDTNDATAQYW2"
+#  }
+#}
 
 resource "aws_route53_record" "a_www" {
   name = "www.login.gov"
@@ -47,21 +47,13 @@ resource "aws_route53_record" "mx_google" {
     "30 aspmx5.googlemail.com."
   ]
   ttl = "300"
-  type = "TXT"
+  type = "MX"
   zone_id = "${aws_route53_zone.primary.zone_id}"
 }
 
-resource "aws_route53_record" "txt_google_site_verification" {
+resource "aws_route53_record" "txt" {
   name = "login.gov"
-  records = ["google-site-verification=x8WM0Sy9Q4EmkHypuULXjTibNOJmPEoOxDGUmBppws8"]
-  ttl = "300"
-  type = "TXT"
-  zone_id = "${aws_route53_zone.primary.zone_id}"
-}
-
-resource "aws_route53_record" "txt_google_spf" {
-  name = "login.gov"
-  records = ["v=spf1 a mx include:gsa.gov ~all"]
+  records = ["google-site-verification=x8WM0Sy9Q4EmkHypuULXjTibNOJmPEoOxDGUmBppws8", "v=spf1 a mx include:gsa.gov ~all", "v=spf1 include:spf.mandrillapp.com ?all"]
   ttl = "300"
   type = "TXT"
   zone_id = "${aws_route53_zone.primary.zone_id}"
@@ -70,14 +62,6 @@ resource "aws_route53_record" "txt_google_spf" {
 resource "aws_route53_record" "txt_mandrill_dkim" {
   name = "mandrill._domainkey.login.gov"
   records = ["v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCrLHiExVd55zd/IQ/J/mRwSRMAocV/hMB3jXwaHH36d9NaVynQFYV8NaWi69c1veUtRzGt7yAioXqLj7Z4TeEUoOLgrKsn8YnckGs9i3B3tVFB+Ch/4mPhXWiNfNdynHWBcPcbJ8kjEQ2U8y78dHZj1YeRXXVvWob2OaKynO8/lQIDAQAB;"]
-  ttl = "300"
-  type = "TXT"
-  zone_id = "${aws_route53_zone.primary.zone_id}"
-}
-
-resource "aws_route53_record" "txt_mandrill_spf" {
-  name = "login.gov"
-  records = ["v=spf1 include:spf.mandrillapp.com ?all"]
   ttl = "300"
   type = "TXT"
   zone_id = "${aws_route53_zone.primary.zone_id}"
