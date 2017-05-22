365e0f30215a9efc55326887b5abddd44ac65896
Justin Grevich <justin@grevi.ch>
2017-05-22 15:37:47 -0700

    Add OIDC example SP to app host (#206)
    * Add OIDC example SP to app host
    
    __Why__
    
    * This is needed most immediately so pentesters can more easily test the new
    OIDC code. It is also needed to provide our clients a working example of an OIDC
    based service provider.
    
    __How__
    
    * Create a new recipe in the login_dot_gov cookbook using the sp-sinatra app as
    a template.
    
    * brought up to date with ACME stuff

---
+++ kitchen/cookbooks/login_dot_gov/recipes/oidc_sinatra.rb
@@ -0,0 +1,145 @@
+execute "mount -o remount,exec,nosuid,nodev /tmp"
+
+# create dir for AWS PostgreSQL combined CA cert bundle
+directory '/usr/local/share/aws' do
+  owner 'root'
+  group 'root'
+  mode 0755
+  recursive true
+end
+
+# add AWS PostgreSQL combined CA cert bundle
+remote_file '/usr/local/share/aws/rds-combined-ca-bundle.pem' do
+  action :create
+  group 'root'
+  mode 0755
+  owner 'root'
+  sensitive true # nothing sensitive but using to remove unnecessary output
+  source 'https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem'
+end
+
+app_name = 'sp-oidc-sinatra'
+
+dhparam = Chef::EncryptedDataBagItem.load('config', 'app')["#{node.chef_environment}"]["dhparam"]
+
+# generate a stronger DHE parameter on first run
+# see: https://raymii.org/s/tutorials/Strong_SSL_Security_On_nginx.html#Forward_Secrecy_&_Diffie_Hellman_Ephemeral_Parameters
+execute "openssl dhparam -out dhparam.pem 4096" do
+  creates '/etc/ssl/certs/dhparam.pem'
+  cwd '/etc/ssl/certs'
+  notifies :stop, "service[passenger]", :before
+  only_if { dhparam == nil }
+  sensitive true
+end
+
+file '/etc/ssl/certs/dhparam.pem' do
+  content dhparam
+  not_if { dhparam == nil }
+  sensitive true
+end
+
+base_dir = "/srv/#{app_name}"
+deploy_dir = "#{base_dir}/current/public"
+
+# branch is 'master'(default) when env is dev, otherwise use stages/env 
+branch_name = (node.chef_environment == 'dev' ? node['login_dot_gov']['branch_name'] : "stages/#{node.chef_environment}")
+sha_env = (node.chef_environment == 'dev' ? node['login_dot_gov']['branch_name'] : "deploy")
+
+# setup required directories with system_user as the owner/group
+%w{cached-copy config log}.each do |dir|
+  directory "#{base_dir}/shared/#{dir}" do
+    group node['login_dot_gov']['system_user']
+    owner node['login_dot_gov']['system_user']
+    recursive true
+    subscribes :create, "deploy[/srv/#{app_name}]", :before
+  end
+end
+
+# Give system_user owner
+execute "chown -R #{node['login_dot_gov']['system_user']}:nogroup #{base_dir}"
+execute "chown -R #{node['login_dot_gov']['system_user']} /opt/ruby_build"
+execute "chown -R #{node['login_dot_gov']['system_user']} /usr/local/src"
+
+deploy "/srv/#{app_name}" do
+  action :deploy
+  before_symlink do
+    cmd = "/opt/ruby_build/builds/#{node['login_dot_gov']['ruby_version']}/bin/bundle install --deployment --jobs 3 --path /srv/#{app_name}/shared/bundle --without deploy development test"
+    execute cmd do
+      cwd release_path
+      user 'ubuntu'
+    end
+  end
+
+  repo 'https://github.com/18F/identity-openidconnect-sinatra.git'
+  branch branch_name
+  shallow_clone true
+  keep_releases 1
+
+  symlinks ({
+    "system" => "public/system",
+    "pids" => "tmp/pids",
+    "log" => "log",
+    'bundle' => '.bundle'
+  })
+  user 'ubuntu'
+end
+
+encrypted_config = Chef::EncryptedDataBagItem.load('config', 'app')["#{node.chef_environment}"]
+
+basic_auth_config 'generate basic auth config' do
+  password encrypted_config['basic_auth_password']
+  user_name encrypted_config["basic_auth_user_name"]
+end
+
+# add nginx conf for app server
+# TODO: JJG convert security_group_exceptions to hash so we can keep a note in both chef and nginx
+#       configs as to why we added the exception.
+template "/opt/nginx/conf/sites.d/#{app_name}.login.gov.conf" do
+  owner node['login_dot_gov']['system_user']
+  notifies :restart, "service[passenger]"
+  source 'nginx_server.conf.erb'
+  variables({
+    app: app_name,
+    domain: "#{node.chef_environment}.#{node['login_dot_gov']['domain_name']}",
+    elb_cidr: node['login_dot_gov']['elb_cidr'],
+    security_group_exceptions: encrypted_config['security_group_exceptions'],
+    server_name: "#{app_name}.#{node.chef_environment}.#{node['login_dot_gov']['domain_name']}"
+  })
+end
+
+ruby_block 'extract_sha_of_revision' do
+  block do
+    Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
+    release_dir = ::Dir.glob("#{base_dir}/releases/" + '201*')[0]
+    
+    # Dynamically set the file resource's attribute
+    # Obtain the desired resource from resource_collection
+    template_r = run_context.resource_collection.find(template: "#{deploy_dir}/api/deploy.json")
+    # Update the content attribute
+    template_r.variables ({
+      env: node.chef_environment,
+      branch: branch_name,
+      user: 'chef',
+      sha: ::File.read("#{::Dir.glob("#{base_dir}/releases/" + '201*')[0]}/.git/refs/heads/#{sha_env}").chomp,
+      timestamp: release_dir.split('/').last
+    })
+  end
+  action :run
+end
+
+directory "#{deploy_dir}/api" do
+  owner node['login_dot_gov']['user']
+  recursive true
+  action :create
+end
+
+template "#{deploy_dir}/api/deploy.json" do
+  owner node['login_dot_gov']['user']
+  source 'deploy.json.erb'
+end
+
+execute "mount -o remount,noexec,nosuid,nodev /tmp"
+
+# allow other execute permissions on all directories within the application folder
+# https://www.phusionpassenger.com/library/admin/nginx/troubleshooting/ruby/#upon-accessing-the-web-app-nginx-reports-a-permission-denied-error
+execute "chmod o+x -R /srv"
