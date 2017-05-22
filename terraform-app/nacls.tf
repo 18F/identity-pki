b01a2cc6271d8d42ef3e0169d172a4fe859cbae4
Andy Brody <brodygov@users.noreply.github.com>
2017-05-22 18:13:09 -0400

    Support a multiple CIDR blocks in NACL whitelist. (#312)
    * Whitelist GSA Guest CIDR block, use actual arrays.
    
    - Parameterize the Network ACL rule so that it supports multiple CIDR
      blocks. (Currently limited only by rule_number collision.)
    - In env.sh, Change GSA_INTERNAL_CIDR to be an array and add a function
      to turn it into a Terraform-compatible string representing a list.
    
    * Convert jumphost and chef NACLs to _rule format.
    
    It turns out that terraform gets very angry if you try to mix having
    rules inline within an aws_network_acl and rules in
    aws_network_acl_rule resources.
    
    Because we need to use _rule for the CIDR block iteration, convert chef
    and jumphost to use the _rule format everywhere.
    
    This diff is best viewed with ?w=1 to ignore whitespace. There are some
    extra spurious changes since terraform uses inconsistent attribute names
    (rule_no => rule_number, action => rule_action).
    
    This commit makes no effective change to the network ACL rules, but
    terraform plan doesn't seem to realize this, so I'm not sure how safe
    this is to run against a production VPC.

--- terraform-app/nacls.tf
+++ terraform-app/nacls.tf
@@ -230,89 +230,6 @@ resource "aws_network_acl" "admin" {
 
 
 resource "aws_network_acl" "chef" {
-  # allow outbound so it can get packages/git/etc.
-  egress {
-    from_port = 0
-    to_port = 65535
-    protocol = "tcp"
-    rule_no = 10
-    action = "allow"
-    cidr_block = "0.0.0.0/0"
-  }
-
-  # allow ntp (if only NACLs let us specify source ports)
-  egress {
-    from_port = 123
-    to_port = 123
-    protocol = "udp"
-    cidr_block = "0.0.0.0/0"
-    rule_no = 20
-    action = "allow"
-  }
-  ingress {
-    from_port = 0
-    to_port = 65535
-    protocol = "udp"
-    rule_no = 5
-    action = "allow"
-    cidr_block = "0.0.0.0/0"
-  }
-
-  # allow traffic back in from when hosts here initiate connections
-  # to the internet for packages and so on (ephemeral ports)
-  ingress {
-    from_port = 32768
-    to_port = 61000
-    protocol = "tcp"
-    rule_no = 10
-    action = "allow"
-    cidr_block = "0.0.0.0/0"
-  }
-
-  # This gets locked down with iptables after we bootstrap the env.
-  # XXX we will someday figure out how to bootstrap the jumphost first
-  # so that this can go away.
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    rule_no = 20
-    action = "allow"
-    # XXX this is kinda a hack.  These rules don't allow multiple nets.
-    # 0 _should_ be the GSA cidr block.
-    cidr_block = "${element(var.app_sg_ssh_cidr_blocks,0)}"
-  }
-
-  # allow ssh from jumphost
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    rule_no = 30
-    action = "allow"
-    cidr_block = "${var.jumphost_subnet_cidr_block}"
-  }
-
-  # ssh in from jenkins
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_block = "${var.admin_subnet_cidr_block}"
-    rule_no = 35
-    action = "allow"
-  }
-
-  # allow everybody in the VPC to chef
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    rule_no = 40
-    action = "allow"
-    cidr_block = "${var.vpc_cidr_block}"
-  }
-
   tags {
     client = "${var.client}"
     Name = "${var.name}-chef_network_acl-${var.env_name}"
@@ -322,78 +239,119 @@ resource "aws_network_acl" "chef" {
   subnet_ids = ["${aws_subnet.chef.id}"]
 }
 
+# -- begin chef rules --
+resource "aws_network_acl_rule" "chef-egress-tcp-all" {
+  network_acl_id = "${aws_network_acl.chef.id}"
 
-resource "aws_network_acl" "jumphost" {
-  # allow us to get out to get packages/gems/git/etc
-  egress {
-    from_port = 0
-    to_port = 65535
-    protocol = "tcp"
-    rule_no = 10
-    action = "allow"
-    cidr_block = "0.0.0.0/0"
-  }
+  # allow outbound so it can get packages/git/etc.
+  egress = true
+  from_port = 0
+  to_port = 65535
+  protocol = "tcp"
+  rule_number = 10
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "chef-egress-ntp" {
+  network_acl_id = "${aws_network_acl.chef.id}"
 
   # allow ntp (if only NACLs let us specify source ports)
-  egress {
-    from_port = 123
-    to_port = 123
-    protocol = "udp"
-    cidr_block = "0.0.0.0/0"
-    rule_no = 20
-    action = "allow"
-  }
-  # allow dns to stuff (needed for ACME cert gen)
-  egress {
-    from_port = 53
-    to_port = 53
-    protocol = "udp"
-    cidr_block = "0.0.0.0/0"
-    rule_no = 25
-    action = "allow"
-  }
-  ingress {
-    from_port = 0
-    to_port = 65535
-    protocol = "udp"
-    rule_no = 5
-    action = "allow"
-    cidr_block = "0.0.0.0/0"
-  }
+  egress = true
+  from_port = 123
+  to_port = 123
+  protocol = "udp"
+  cidr_block = "0.0.0.0/0"
+  rule_number = 20
+  rule_action = "allow"
+}
+
+resource "aws_network_acl_rule" "chef-ingress-udp-all" {
+  network_acl_id = "${aws_network_acl.chef.id}"
+
+  egress = false
+  from_port = 0
+  to_port = 65535
+  protocol = "udp"
+  rule_number = 5
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "chef-ingress-tcp-high" {
+  network_acl_id = "${aws_network_acl.chef.id}"
 
   # allow traffic back in from when hosts here initiate connections
   # to the internet for packages and so on (ephemeral ports)
-  ingress {
-    from_port = 32768
-    to_port = 61000
-    protocol = "tcp"
-    rule_no = 10
-    action = "allow"
-    cidr_block = "0.0.0.0/0"
-  }
+  egress = false
+  from_port = 32768
+  to_port = 61000
+  protocol = "tcp"
+  rule_number = 10
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+}
 
-  # allow ssh in from GSA
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    rule_no = 20
-    action = "allow"
-    # XXX this is kinda a hack.  These rules don't allow multiple nets.
-    # 0 _should_ be the GSA cidr block.
-    cidr_block = "${element(var.app_sg_ssh_cidr_blocks,0)}"
-  }
+resource "aws_network_acl_rule" "chef-ingress-tcp-ssh-jumphost" {
+  network_acl_id = "${aws_network_acl.chef.id}"
+
+  # allow ssh from jumphost
+  egress = false
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  rule_number = 30
+  rule_action = "allow"
+  cidr_block = "${var.jumphost_subnet_cidr_block}"
+}
+
+resource "aws_network_acl_rule" "chef-ingress-tcp-ssh-jenkins" {
+  network_acl_id = "${aws_network_acl.chef.id}"
 
   # ssh in from jenkins
-  ingress {
+  egress = false
     from_port = 22
     to_port = 22
     protocol = "tcp"
     cidr_block = "${var.admin_subnet_cidr_block}"
-    rule_no = 30
-    action = "allow"
-  }
+    rule_number = 35
+    rule_action = "allow"
+}
 
+resource "aws_network_acl_rule" "chef-ingress-tcp-443" {
+  network_acl_id = "${aws_network_acl.chef.id}"
+
+  # allow everybody in the VPC to chef
+  egress = false
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  rule_number = 40
+  rule_action = "allow"
+  cidr_block = "${var.vpc_cidr_block}"
+}
+
+# This gets locked down with iptables after we bootstrap the env.
+# XXX we will someday figure out how to bootstrap the jumphost first
+# so that this can go away.
+#
+# Allow SSH
+resource "aws_network_acl_rule" "chef-ingress-cidr" {
+  network_acl_id = "${aws_network_acl.chef.id}"
+
+  # iterate over CIDR blocks
+  count = "${length(var.app_sg_ssh_cidr_blocks)}"
+  rule_number = "${20 + count.index}"
+  egress = false
+  protocol = "tcp"
+  from_port = 22
+  to_port = 22
+  cidr_block = "${element(var.app_sg_ssh_cidr_blocks, count.index)}"
+  rule_action = "allow"
+}
+# -- end chef rules --
+
+resource "aws_network_acl" "jumphost" {
   tags {
     client = "${var.client}"
     Name = "${var.name}-jumphost_network_acl-${var.env_name}"
@@ -402,6 +360,99 @@ resource "aws_network_acl" "jumphost" {
   vpc_id = "${aws_vpc.default.id}"
   subnet_ids = ["${aws_subnet.jumphost.id}"]
 }
+
+# -- begin jumphost rules --
+resource "aws_network_acl_rule" "jumphost-egress-tcp-all" {
+  network_acl_id = "${aws_network_acl.jumphost.id}"
+
+  # allow us to get out to get packages/gems/git/etc
+  egress = true
+  from_port = 0
+  to_port = 65535
+  protocol = "tcp"
+  rule_number = 10
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "jumphost-egress-ntp" {
+  network_acl_id = "${aws_network_acl.jumphost.id}"
+
+  # allow ntp (if only NACLs let us specify source ports)
+  egress = true
+  from_port = 123
+  to_port = 123
+  protocol = "udp"
+  cidr_block = "0.0.0.0/0"
+  rule_number = 20
+  rule_action = "allow"
+}
+
+resource "aws_network_acl_rule" "jumphost-egress-dns" {
+  network_acl_id = "${aws_network_acl.jumphost.id}"
+
+  # allow dns to stuff (needed for ACME cert gen)
+  egress = true
+  from_port = 53
+  to_port = 53
+  protocol = "udp"
+  cidr_block = "0.0.0.0/0"
+  rule_number = 25
+  rule_action = "allow"
+}
+resource "aws_network_acl_rule" "jumphost-ingress-udp-all" {
+  network_acl_id = "${aws_network_acl.jumphost.id}"
+  egress = false
+  from_port = 0
+  to_port = 65535
+  protocol = "udp"
+  rule_number = 5
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "jumphost-ingress-tcp-high" {
+  network_acl_id = "${aws_network_acl.jumphost.id}"
+
+  # allow traffic back in from when hosts here initiate connections
+  # to the internet for packages and so on (ephemeral ports)
+  egress = false
+  from_port = 32768
+  to_port = 61000
+  protocol = "tcp"
+  rule_number = 10
+  rule_action = "allow"
+  cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_network_acl_rule" "jumphost-ingress-tcp-ssh" {
+  network_acl_id = "${aws_network_acl.jumphost.id}"
+
+  # ssh in from jenkins
+  egress = false
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_block = "${var.admin_subnet_cidr_block}"
+  rule_number = 30
+  rule_action = "allow"
+}
+
+# Allow SSH from app_sg_ssh_cidr_blocks
+resource "aws_network_acl_rule" "jumphost-ingress-cidr" {
+  network_acl_id = "${aws_network_acl.jumphost.id}"
+  # iterate over CIDR blocks
+  count = "${length(var.app_sg_ssh_cidr_blocks)}"
+  rule_number = "${20 + count.index}"
+  egress = false
+  protocol = "tcp"
+  from_port = 22
+  to_port = 22
+  cidr_block = "${element(var.app_sg_ssh_cidr_blocks, count.index)}"
+  rule_action = "allow"
+}
+# -- end jumphost rules --
+
 
 resource "aws_network_acl" "idp" {
   egress {
