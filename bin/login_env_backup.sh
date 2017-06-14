936dcfebe9eb7842c7b8800bbad53db24088837a
Justin Grevich <justin@grevi.ch>
2017-06-14 10:12:54 -0700

    Remove cert backup from env backup script (#363)
    * Remove LE cert back/restore code from backup script
    
    __Why__
    
    * We can retrieve all certs from terraform and do not need to back up or restore
    from the filesystem.
    
    __How__
    
    * Remove backup/restore code for certs
    
    * Shellcheck fixes
    __Why__
    
    * Codelinting is a good thing
    
    __How__
    
    * Follow the shellcheck output

--- bin/login_env_backup.sh
+++ bin/login_env_backup.sh
@@ -3,7 +3,6 @@
 # This script backs up the infrastructure stuff into a tarball that
 # we can use to reconstitute an env later on
 #
-
 usage() {
 	echo "usage:   $0 <env> [-h]"
 	echo "example: $0 tf"
@@ -18,48 +17,43 @@ if [ -z "$1" ] ; then
 	exit 1
 fi
 
-
-DIRNAME="envbackup-$1-`date +%Y-%m-%d-%H%M`"
+DIRNAME="envbackup-$1-$(date +%Y-%m-%d-%H%M)"
 DIR="/tmp/$DIRNAME"
-rm -rf $DIR
-mkdir $DIR
-cd $DIR
+rm -rf "$DIR"
+mkdir "$DIR"
+cd "$DIR" || exit
 
 # databag
 knife data bag show -Fj config app > config_app_databag.json
 
 # users
 mkdir users
-for i in `knife data bag show users` ; do
-	knife data bag show -Fj users $i > users/$i.json
+for i in $(knife data bag show users) ; do
+	knife data bag show -Fj users "$i" > users/"$i".json
 done
-
-# certs
-ssh -q ubuntu@idp1-0 sudo tar cf - /etc/letsencrypt | gzip -9 > idp_letsencryptdir.tar.gz
 
 # environments
 mkdir environments
-for i in `knife environment list` ; do
-	knife environment show -Fj $i > environments/$i.json
+for i in $(knife environment list) ; do
+	knife environment show -Fj "$i" > environments/"$i".json
 done
 
 # roles
 mkdir roles
-for i in `knife role list` ; do
-	knife role show -Fj $i > roles/$i.json
+for i in $(knife role list) ; do
+	knife role show -Fj "$i" > roles/"$i".json
 done
 
 # XXX back up db, maybe?
 
 # make tarball and clean up
 cd ..
-tar cf - $DIRNAME | gzip -9 > $DIRNAME.tar.gz
-rm -rf $DIR
-echo $DIR.tar.gz created
+tar cf - "$DIRNAME" | gzip -9 > "$DIRNAME".tar.gz
+rm -rf "$DIR"
+echo "$DIR".tar.gz created
 
 # homedir
 if [ "-h" = "$2" ] ; then
-	tar cf - ~ | gzip -9 > `whoami`.tar.gz
-	echo  /tmp/`whoami`.tar.gz is created
+	tar cf - ~ | gzip -9 > "$(whoami)".tar.gz
+	echo  /tmp/"$(whoami)".tar.gz is created
 fi
-
