59b131d37c20ca9aca48424fa2223b37f3942896
Shaun Verch <shaun.verch@gmail.com>
2017-06-26 17:33:13 -0700

    Match rspec chef-client version with real env (#379)
    The version that rspec was downloaded didn't match the version that we have
    deployed in our environments.
    
    The upgrade of chef-client in our environments can now go in lock step with an
    upgrade of all the cookbook chef-client versions, so we'll have regression
    tests.  chef-client seems to have some differences between 12 and 13 that might
    hit us when we do, so this will be important.

--- kitchen/cookbooks/cookbook_example/metadata.rb
+++ kitchen/cookbooks/cookbook_example/metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures cookbook_example'
 long_description 'Installs/Configures cookbook_example'
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.15.19' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
