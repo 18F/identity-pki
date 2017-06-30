fc73aadbc84a314943079140a823c5024bc66852
Justin Grevich <justin@grevi.ch>
2017-06-30 11:46:57 -0700

    Reorder sync.sh functions before main code block (#404)
    __Why__
    
    * sync.sh failed to run because of the following error:
      `./sync.sh: line 36: cmd_sync_down: command not found`
    
    __How__
    
    * Move all functions before the main code block.
    * I also converted this to 2-space indentation like the rest of the identity-*
    projects.

--- kitchen/data_bags/users/sync.sh
+++ kitchen/data_bags/users/sync.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
-
 set -eu
 
 BUCKET=login-gov-secrets-test
 USERS_PATH="s3://$BUCKET/kitchen/data_bags/users/"
 
+cmd_sync_down() {
+  cd "$(dirname "$0")"
+  run aws s3 sync "$USERS_PATH" ./
+}
+
+cmd_upload_one() {
+  cd "$(dirname "$0")"
+  filename="$1"
+  run aws s3 cp "$filename" "$USERS_PATH"
+}
+
 run() {
-    echo >&2 "+ $*"
-    "$@"
+  echo >&2 "+ $*"
+  "$@"
 }
 
 usage() {
-    cat >&2 <<EOM
+  cat >&2 <<EOM
 usage: $0 sync-down
 usage: $0 upload FILE
 
@@ -27,38 +37,23 @@ EOM
 }
 
 if [ $# -lt 1 ]; then
-    usage
-    exit 1
+  usage
+  exit 1
 fi
 
 case "$1" in
-    sync-down)
-        cmd_sync_down
-        ;;
-    upload)
-        if [ $# -lt 2 ]; then
-            usage
-            exit 2
-        fi
-        cmd_upload_one "$2"
-        ;;
-    *)
-        usage
-        exit 1
-        ;;
+  sync-down)
+    cmd_sync_down
+    ;;
+  upload)
+    if [ $# -lt 2 ]; then
+      usage
+      exit 2
+    fi
+    cmd_upload_one "$2"
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
 esac
-
-cmd_sync_down() {
-    cd "$(dirname "$0")"
-
-    run aws s3 sync "$USERS_PATH" ./
-}
-
-cmd_upload_one() {
-    cd "$(dirname "$0")"
-
-    filename="$1"
-
-    run aws s3 cp "$filename" "$USERS_PATH"
-}
-
