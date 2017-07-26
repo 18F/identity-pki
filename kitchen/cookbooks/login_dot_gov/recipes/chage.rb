49ec3403de1204f4d72a6c1491d2c02251f4a2b8
Andy Brody <brodygov@users.noreply.github.com>
2017-07-26 13:05:31 -0400

    Make chage execute idempotent. (#447)
    It's annoying to have the executes always cluttering up the chef output.
    Make them idempotent so they only need to run when the accounts are set
    to expire.

--- kitchen/cookbooks/login_dot_gov/recipes/chage.rb
+++ kitchen/cookbooks/login_dot_gov/recipes/chage.rb
@@ -1,4 +1,7 @@
-
-execute '/usr/bin/chage -E -1 root'
-execute '/usr/bin/chage -m 0 -M 99999 -I -1 -E -1 -M -1 ubuntu'
-
+# Ensure root and ubuntu accounts do not expire
+execute '/usr/bin/chage -E -1 root' do
+  not_if 'chage -l root | grep "Account expires" | grep never'
+end
+execute '/usr/bin/chage -m 0 -M 99999 -I -1 -E -1 -M -1 ubuntu' do
+  only_if 'chage -l ubuntu | grep -E "(Password|Account) expires" | grep -v never'
+end
