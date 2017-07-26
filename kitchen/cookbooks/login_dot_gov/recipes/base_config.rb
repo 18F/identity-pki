af2cf6e927d63abdae676fd95bcdadd5bc23dec5
Andy Brody <brodygov@users.noreply.github.com>
2017-07-26 12:23:24 -0400

    Add a base recipe to install common packages. (#448)
    * Add a base recipe to install common packages.
    
    * Increment cookbook version.

---
+++ kitchen/cookbooks/login_dot_gov/recipes/base_config.rb
@@ -0,0 +1,19 @@
+# Common packages and config used by all hosts
+
+package [
+  'bmon',
+  'bwm-ng',
+  'colordiff',
+  'dstat',
+  'htop',
+  'iftop',
+  'iotop',
+  'iperf',
+  'moreutils',
+  'ncdu',
+  'nethogs',
+  'pigz',
+  'pv',
+  'pydf',
+  'tree',
+]
--- kitchen/cookbooks/login_dot_gov/recipes/default.rb
+++ kitchen/cookbooks/login_dot_gov/recipes/default.rb
@@ -1,5 +1,6 @@
 include_recipe "login_dot_gov::ssh"
 include_recipe "passenger::daemon"
+include_recipe "login_dot_gov::base_config"
 include_recipe "login_dot_gov::dashboard"
 include_recipe "login_dot_gov::idp_base"
 include_recipe "login_dot_gov::idp_web"
