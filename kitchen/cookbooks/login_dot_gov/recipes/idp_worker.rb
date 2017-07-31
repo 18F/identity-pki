18F/identity-pki

commit d14ae0015560bf2d9f373ab233270966e7d7567f

Restart sidekiq process via monit by default (#329)
__Why__

* When provisioning with chef on the initial run monit will start sidekiq since
the template file has changed and hence it issues a restart command:
https://github.com/18F/identity-devops/blob/master/kitchen/cookbooks/login_dot_gov/recipes/idp_worker.rb

This causes a problem since on subsequent runs the rails code is updated but the
sidekiq proc is not restarted.

__How__

* Change the default action for the sidekiq service to restart so that the service is restarted with
each chef run.

kitchen/cookbooks/login_dot_gov/recipes/idp_worker.rb
@@ -5,7 +5,7 @@ service 'monit' do
 end
 
 service 'sidekiq' do
-  action :nothing
+  action :restart
   restart_command '/usr/bin/monit restart sidekiq_idp_production0'
   start_command '/usr/bin/monit start sidekiq_idp_production0'
   status_command '/usr/bin/monit status'

commit dc1f94ad471c8fb016a0c7365d03b8c6f3fc2962

Must install monit config before calling restart.
Bootstrapping of worker nodes was broken in #329 by enabling an
automatic monit restart. We can't restart monit until it has enough
configuration to start up its HTTP interface.

kitchen/cookbooks/login_dot_gov/recipes/idp_worker.rb
@@ -4,16 +4,8 @@ service 'monit' do
   action :nothing
 end
 
-service 'sidekiq' do
-  action :restart
-  restart_command '/usr/bin/monit restart sidekiq_idp_production0'
-  start_command '/usr/bin/monit start sidekiq_idp_production0'
-  status_command '/usr/bin/monit status'
-  stop_command '/usr/bin/monit stop sidekiq_idp_production0'
-end
-
 template '/etc/monit/monitrc' do
-  notifies :restart, 'service[monit]'
+  notifies :restart, 'service[monit]', :immediately
 end
 
 template '/etc/monit/conf.d/sidekiq_idp_production.conf' do
@@ -21,4 +13,12 @@ template '/etc/monit/conf.d/sidekiq_idp_production.conf' do
   variables({
     ruby_version: node['login_dot_gov']['ruby_version']
   })
+end
+
+service 'sidekiq' do
+  action :restart
+  restart_command '/usr/bin/monit restart sidekiq_idp_production0'
+  start_command '/usr/bin/monit start sidekiq_idp_production0'
+  status_command '/usr/bin/monit status'
+  stop_command '/usr/bin/monit stop sidekiq_idp_production0'
 end