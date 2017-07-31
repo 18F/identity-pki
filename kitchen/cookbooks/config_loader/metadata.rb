f57aa58205ee908da867183c2e7090c068185907
Shaun Verch <shaun.verch@gsa.gov>
2017-07-31 13:36:06 -0400

    Update metadata file to point to citadel_fork

--- kitchen/cookbooks/config_loader/metadata.rb
+++ kitchen/cookbooks/config_loader/metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures config_loader'
 long_description 'Installs/Configures config_loader'
-version '0.1.1'
+version '0.2.1'
 chef_version '>= 12.15.19' if respond_to?(:chef_version)
 
-depends 'citadel'
+depends 'citadel_fork'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
