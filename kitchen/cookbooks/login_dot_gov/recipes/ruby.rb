559bf1664235a1bff2f23f890219ce2e24ab3e7f
Shaun Verch <shaun.verch@gmail.com>
2017-08-31 08:03:58 -0700

    Do not try to load basic_auth_* files in prod (#586)
    See comment for the list of things that caused this issue.

--- kitchen/cookbooks/login_dot_gov/recipes/ruby.rb
+++ kitchen/cookbooks/login_dot_gov/recipes/ruby.rb
@@ -1,18 +1,42 @@
-# TODO: this file probably shouldn't be here at all
-if node.chef_environment != 'prod'
-  template '/home/ubuntu/.bash_profile' do
-    owner node['login_dot_gov']['system_user']
-    mode '0644'
-    sensitive true
-    variables({
-      idp_slo_url: "https://idp.#{node.chef_environment}.login.gov/api/saml/logout",
-      idp_sp_url: "https://#{ConfigLoader.load_config_or_nil(node, "basic_auth_user_name")}:#{ConfigLoader.load_config_or_nil(node, "basic_auth_password")}@idp.#{node.chef_environment}.login.gov/api/service_provider",
-      idp_sso_url: "https://idp.#{node.chef_environment}.login.gov/api/saml/auth",
-      sp_name: ConfigLoader.load_config_or_nil(node, "basic_auth_user_name"),
-      sp_pass: ConfigLoader.load_config_or_nil(node, "basic_auth_password")
-    })
-    subscribes :run, 'execute[ruby-build install]', :delayed
-  end
+# This should not be happening, and should be fixed as part of
+# https://github.com/18F/identity-devops-private/issues/230
+#
+# Here's the sequence of things that are happening here, from the result
+# backwards:
+# 1. The idp worker is running as a sidekiq task
+# 2. This task is configured to run as the ubuntu user
+# 3. This task also explicitly sources `/home/ubuntu/.bash_profile`
+# 4. This file contains environment variables consumed by the worker
+# 5. This file is an ERB template
+# 6. The variables in this template are passed in by chef
+# 7. These come from ruby variables and string templates
+# 8. The basic_auth_* variables should not be set in staging or prod
+# 9. The configloader downloads these secrets from s3 as individual files
+# 10. These were previously set to zero byte files as placeholders
+# 11. These now don't exist in s3 for staging and prod since they are unused
+# 12. The config loader as sanity checks for file existence in prod
+# 13. So we can't call the configloader on nonexistent files in prod
+# 14. Setting them to the empty string at least replicates the previous behavior
+# 15. Profit?!?
+if node.chef_environment == 'prod'
+  basic_auth_user_name = ""
+  basic_auth_password = ""
+else
+  basic_auth_user_name = ConfigLoader.load_config_or_nil(node, "basic_auth_user_name")
+  basic_auth_password = ConfigLoader.load_config_or_nil(node, "basic_auth_password")
+end
+template '/home/ubuntu/.bash_profile' do
+  owner node['login_dot_gov']['system_user']
+  mode '0644'
+  sensitive true
+  variables({
+    idp_slo_url: "https://idp.#{node.chef_environment}.login.gov/api/saml/logout",
+    idp_sp_url: "https://#{basic_auth_user_name}:#{basic_auth_password}@idp.#{node.chef_environment}.login.gov/api/service_provider",
+    idp_sso_url: "https://idp.#{node.chef_environment}.login.gov/api/saml/auth",
+    sp_name: basic_auth_user_name,
+    sp_pass: basic_auth_password
+  })
+  subscribes :run, 'execute[ruby-build install]', :delayed
 end
 
 # TODO: probably shouldn't be setting anything in /etc/environment
