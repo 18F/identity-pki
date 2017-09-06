cbd0c0f1db994770d054c254a4a24c053b9b7129
Tim Spencer <timothy.spencer@gsa.gov>
2017-09-06 10:19:31 -0700

    Make it so that logstash catches up faster (#592)
    __Why__
    
    * 3 days is a long time to go back to ensure we have all the logs.  Worst case,
      we should be able to relaunch in a few hours, so even 1.5 days is a bit overkill.
      So this change will make it so that we will get up to realtime cloudwatch logs
      much more quickly.
    
    __How__
    
    * Change the sincedb so that we only go back 1.5 days instead of 3

--- kitchen/cookbooks/identity-elk/metadata.rb
+++ kitchen/cookbooks/identity-elk/metadata.rb
@@ -5,7 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures identity-elk'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version          '0.1.23'
+version          '0.1.24'
 
 depends          'java'
 depends          'elasticsearch'
