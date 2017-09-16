658271762e0fc8b88a8a6516d46140f2d5afb5d5
Andy Brody <andrew.brody@gsa.gov>
2017-09-16 11:53:11 -0400

    Don't perform extra sidekiq restart.
    Because this occurs after monit has started sidekiq but before it has
    settled down, the sidekiq restart would cause there to be two sidekiq
    processes running.
    
    Fixes: https://github.com/18F/identity-devops-private/issues/365

--- kitchen/cookbooks/login_dot_gov/recipes/idp_worker.rb
+++ kitchen/cookbooks/login_dot_gov/recipes/idp_worker.rb
@@ -15,10 +15,12 @@ template '/etc/monit/conf.d/sidekiq_idp_production.conf' do
   })
 end
 
-execute 'sleep 1'
-
+# Action nothing means that this won't restart unless it's explicitly notified.
+# We already trigger a monit restart above when sidekiq_idp_production.conf is
+# modified, so keeping both of these restarts was leading to multiple sidekiq
+# processes running. https://github.com/18F/identity-devops-private/issues/365
 service 'sidekiq' do
-  action :restart
+  action :nothing
   restart_command '/usr/bin/monit restart sidekiq_idp_production0'
   start_command '/usr/bin/monit start sidekiq_idp_production0'
   status_command '/usr/bin/monit status'
