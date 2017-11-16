c30ceab1467c8456e122a91145d33a2a3ec4a878
Andy Brody <andrew.brody@gsa.gov>
2017-11-15 20:27:21 -0500

    Fix up app/idp hosts for Ubuntu 16.04/systemd.
    - The service inspec tests seem to be broken on systemd. Add some more
      test functionality that properly checks whether the passenger service
      is installed and enabled.
    - After changing the way nginx is started, the passenger status
      directory now seems to correctly be set to /tmp/.
    - Add a test that the IDP database health check returns healthy.
    
    These tests were all verified to be passing with ami-3ff92947.

--- nodes/app/test/smoke/default/default_test.rb
+++ nodes/app/test/smoke/default/default_test.rb
@@ -11,16 +11,36 @@ describe service('ssh') do
   it { should be_running }
 end
 
-describe service('passenger') do
-  it { should be_installed }
-  it { should be_enabled }
-  it { should be_running }
-end
-
 # make sure we can sudo
 describe command('sudo whoami') do
   its('stdout') { should eq "root\n" }
   its('exit_status') { should eq 0 }
+end
+
+# check that passenger is installed and running
+if os[:release] == '14.04'
+  describe service('passenger') do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+  end
+else
+  # Ubuntu >= 16.04 runs systemd
+
+  describe service('passenger') do
+    it { should be_installed }
+    # enabled check appears broken on systemd with inspec 1.43
+    #it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe command('sudo systemctl show passenger -p SubState') do
+    its('stdout') { should eq "SubState=running\n" }
+  end
+
+  describe command('sudo systemctl is-enabled passenger') do
+    its('exit_status') { should eq 0 }
+  end
 end
 
 describe file('/opt/nginx/logs') do
--- nodes/idp/test/smoke/default/default_test.rb
+++ nodes/idp/test/smoke/default/default_test.rb
@@ -9,10 +9,30 @@ describe service('ssh') do
   it { should be_running }
 end
 
-describe service('passenger') do
-  it { should be_installed }
-  it { should be_enabled }
-  it { should be_running }
+# check taht passenger is installed and running
+if os[:release] == '14.04'
+  describe service('passenger') do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+  end
+else
+  # Ubuntu >= 16.04 runs systemd
+
+  describe service('passenger') do
+    it { should be_installed }
+    # enabled check appears broken on systemd with inspec 1.43
+    #it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe command('sudo systemctl show passenger -p SubState') do
+    its('stdout') { should eq "SubState=running\n" }
+  end
+
+  describe command('sudo systemctl is-enabled passenger') do
+    its('exit_status') { should eq 0 }
+  end
 end
 
 # make sure we can sudo
@@ -24,7 +44,7 @@ end
 # check passenger status
 # TODO: Actually move the instance registry dir to something more reasonable.
 # See: https://stackoverflow.com/questions/31761542/phusion-passenger-status-what-value-for-passenger-instance-registry-dir#31769807
-describe command('sudo env PASSENGER_INSTANCE_REGISTRY_DIR=/var/lib/kitchen/cache/ passenger-status') do
+describe command('sudo env PASSENGER_INSTANCE_REGISTRY_DIR=/tmp/ passenger-status') do
   its('exit_status') { should eq 0 }
   its('stdout') { should include 'General information' }
 end
@@ -57,4 +77,13 @@ describe file('/srv/idp/current/config/database.yml') do
   it { should exist }
   it { should be_file }
   it { should_not be_symlink }
+end
+
+# hit the IDP database health check and ensure it's healthy
+# Ideally we would use the http() inspec resource, but it doesn't seem to work
+describe command('curl -Sfk -i https://localhost/api/health/database') do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should start_with("HTTP/1.1 200 OK") }
+  its('stdout') { should include 'Content-Type: application/json' }
+  its('stdout') { should include '"healthy":true' }
 end
