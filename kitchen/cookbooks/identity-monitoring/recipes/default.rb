24b65bca4205b09cc9ec9205fa1ebc3e1e2ea5d5
Andy Brody <andrew.brody@gsa.gov>
2017-12-01 18:11:53 -0500

    Set some attrs in NR-infra useful for filtering.
    - Add "env" => node.chef_environment
    - Add "domain" => login_dot_gov.domain_name
    - Add "role" => node.roles.first, assuming that the main role will be
      first in the run list.
    
    The attributes will be missing if the run list is overridden, which
    shouldn't normally be a problem. But these might be good candidates for
    moving into a custom Ohai plugin that reads from `/etc/login.gov/info/`,
    or even using the LoginGov::Hostdata gem directly.

--- kitchen/cookbooks/identity-monitoring/recipes/default.rb
+++ kitchen/cookbooks/identity-monitoring/recipes/default.rb
@@ -9,5 +9,11 @@
 
 node.default['newrelic_infra']['config']['license_key'] = ConfigLoader.load_config(node, "newrelic_infra_license_key", common: true)
 
+node.default['newrelic_infra']['config']['custom_attributes'] = {
+  'env' => node.chef_environment,
+  'domain' => node.fetch('login_dot_gov').fetch('domain_name'),
+  'role' => node.roles.first || 'unknown',
+}
+
 include_recipe 'newrelic-infra'
 
