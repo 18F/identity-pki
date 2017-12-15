6ca4b6033767a5a87a39cd5e78677202acacd0b6
Andy Brody <andrew.brody@gsa.gov>
2017-12-15 14:17:28 -0500

    Add lg_ prefixes to all the custom tags.

--- kitchen/cookbooks/identity-monitoring/recipes/default.rb
+++ kitchen/cookbooks/identity-monitoring/recipes/default.rb
@@ -10,9 +10,9 @@
 node.default['newrelic_infra']['config']['license_key'] = ConfigLoader.load_config(node, "newrelic_infra_license_key", common: true)
 
 node.default['newrelic_infra']['config']['custom_attributes'] = {
-  'env' => node.chef_environment,
-  'domain' => node.fetch('login_dot_gov').fetch('domain_name'),
-  'role' => node.roles.first || 'unknown',
+  'lg_env' => node.chef_environment,
+  'lg_domain' => node.fetch('login_dot_gov').fetch('domain_name'),
+  'lg_role' => node.roles.first || 'unknown',
 }
 
 include_recipe 'newrelic-infra'
