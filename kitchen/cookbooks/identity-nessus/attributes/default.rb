5b00dfa5d9893fbce4695d309ec245f933ab8a1f
Andy Brody <andrew.brody@gsa.gov>
2018-01-17 14:17:54 -0500

    Update Nessus to 7.0.1.

--- kitchen/cookbooks/identity-nessus/attributes/default.rb
+++ kitchen/cookbooks/identity-nessus/attributes/default.rb
@@ -2,5 +2,5 @@
 # Cookbook Name:: identity-nessus
 # Attributes:: default
 #
-default['identity-nessus']['deb_name'] = 'NessusAgent-6.10.0-ubuntu1110_amd64.deb'
-default['identity-nessus']['sha256sum'] = '8255430a5911431c7dfad57a3edb7a32928b64e310cc4da94e6a8ac0e8e9d254'
+default['identity-nessus']['deb_name'] = 'NessusAgent-7.0.1-ubuntu1110_amd64.deb'
+default['identity-nessus']['sha256sum'] = '0d02d61ba390ce03ecf7495d0370ef7a31234cf9e825529224f0b4e068a5f757'
