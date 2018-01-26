9374d00a80255a0d528adf09336479b6b63e9377
Andy Brody <andrew.brody@gsa.gov>
2018-01-26 15:18:57 -0500

    Fix monitor.rb to work in prod env, have a CLI.

--- bin/oneoffs/monitor.rb
+++ bin/oneoffs/monitor.rb
@@ -2,9 +2,15 @@
 
 require 'rest-client'
 
-def hit_db_healthcheck(env, domain: 'login.gov')
-  url = "https://idp.#{env}.#{domain}/api/health/database"
+def healthcheck_for_env(env:, domain:)
+  if env == 'prod'
+    "https://secure.#{domain}/api/health"
+  else
+    "https://idp.#{env}.#{domain}/api/health"
+  end
+end
 
+def hit_db_healthcheck(url:)
   begin
     RestClient.get(url)
   rescue RestClient::ExceptionWithResponse => err
@@ -13,9 +19,12 @@ def hit_db_healthcheck(env, domain: 'login.gov')
 end
 
 def monitor(env, interval: 2, domain: 'login.gov')
+  url = healthcheck_for_env(env: env, domain: domain)
+  STDERR.puts "Running health checks every #{interval} sec against #{url}"
+
   results = []
   loop do
-    row = [Time.now, hit_db_healthcheck(env, domain: domain)]
+    row = [Time.now, hit_db_healthcheck(url: url)]
     results << row
 
     puts "#{row.first}: #{row.last.code} #{row.last.net_http_res.msg}"
@@ -23,4 +32,25 @@ def monitor(env, interval: 2, domain: 'login.gov')
   end
 rescue Interrupt
   return results
+end
+
+def main
+  if ARGV.empty?
+    STDERR.puts <<-EOM
+usage: #{File.basename($0)} ENV [DOMAIN]"
+
+Run a healthcheck against https://idp.ENV.DOMAIN/api/health every 2 seconds.
+
+For more complex usage, open this script in a repl, for example:
+    pry -r $0
+    >> monitor('dev', interval: 10, domain: 'identitysandbox.gov')
+    EOM
+    exit 1
+  end
+
+  monitor(ARGV.fetch(0), domain: ARGV.fetch(1, 'login.gov'))
+end
+
+if $0 == __FILE__
+  main
 end
