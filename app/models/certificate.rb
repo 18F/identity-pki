f665e94679efe5cc8426285e4907b7bee1327680
James Smith <James_G_Smith@omb.eop.gov>
2018-04-11 09:45:29 -0400

    Load and store CRLs
    **Why**:
    We want to have at least one way to know if a certificate has been
    revoked. Fetching CRLs is the easiest way to get started, and a good
    fallback if we aren't able to reach an OCSP service.
    
    **How**:
    We have Certificate and CertificateRevocation models that manage
    just enough information to let us know if a particular certificate
    has been revoked and where we can find the CRLs for the signing
    certificates.
    
    We have a rake task that can load the CRLs and update the database.

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -6,12 +6,14 @@ class Certificate < ApplicationRecord
   has_many :certificate_revocations, dependent: :destroy
 
   scope :with_crl_http_url, -> { where.not(crl_http_url: nil) }
+
   ##
   # Fetches the CRL from the stored URL and adds any new serial numbers.
   # Will raise on errors.
   #
   def update_revocations
     serials = CertificateRevocationListService.retrieve_serials_from_url(crl_http_url)
+
     already_revoked = certificate_revocations.pluck(:serial)
     (serials - already_revoked).each do |serial|
       certificate_revocations.create(serial: serial)
