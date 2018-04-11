191179dbbfde19b99c58953639147374a8560212
Jean-Paul Mugizi <jeanpaulcrescent.mugizi@gsa.gov>
2018-04-11 12:43:51 -0400

    Whitelist Jumphost Static IPs for Analytics in Prod and Staging (#824)
    * Whitelist Jumphost Static IPs for Analytics in Prod and Staging
    
    * use the right IPs
    
    * add static IPs for DM and PT as well

--- terraform-analytics/variables.tf
+++ terraform-analytics/variables.tf
@@ -1,25 +1,51 @@
 variable "env_name" {}
+
 variable "jumphost_cidr_block" {
   type = "map"
+
   default = {
-    dev = "52.33.21.52/32"
-    qa = "34.208.151.117/32"
-    int = "52.38.156.57/32"
-    dm = "52.37.18.62/32"
-    staging = "52.26.32.80/32"
-    prod = "34.208.176.143/32"
-    pt = "92.242.140.21/32"
+    dev     = "34.216.215.128/26"
+    qa      = "34.216.215.128/26"
+    int     = "34.216.215.128/26"
+    dm      = "34.216.215.0/26"
+    staging = "34.216.215.0/26"
+    prod    = "34.216.215.64/26"
+    pt      = "34.216.215.128/26"
   }
 }
+
 variable "redshift_master_password" {}
-variable "vpc_cidr_block" { default = "10.1.0.0/16" }
+
+variable "vpc_cidr_block" {
+  default = "10.1.0.0/16"
+}
+
 variable "analytics_version" {}
-variable "region" { default = "us-west-2"}
-variable "kms_key_id" { default = "dc12706b-50ea-40b7-8d0e-206962aaa8f7" }
-variable "num_redshift_nodes" { default = 4 }
-variable "version_info_bucket" { default = "login-dot-gov-analytics-terraform-state" }
-variable "version_info_region" { default = "us-west-2" }
-variable "name" { default = "login" }
+
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "kms_key_id" {
+  default = "dc12706b-50ea-40b7-8d0e-206962aaa8f7"
+}
+
+variable "num_redshift_nodes" {
+  default = 4
+}
+
+variable "version_info_bucket" {
+  default = "login-dot-gov-analytics-terraform-state"
+}
+
+variable "version_info_region" {
+  default = "us-west-2"
+}
+
+variable "name" {
+  default = "login"
+}
+
 variable "wlm_json_configuration" {
   default = "[{\"query_concurrency\": 50}]"
 }
