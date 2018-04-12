2df118a0cdf98a2b20a390136f74ab6d9172c116
Andy Brody <andrew.brody@gsa.gov>
2018-04-12 12:10:41 -0400

    Chef upgrade: bail out on deprecation warnings.

--- kitchen/chef-client.rb
+++ kitchen/chef-client.rb
@@ -30,3 +30,7 @@ cookbook_path [repo_root + '/berks-cookbooks']
 InfoDir = '/etc/login.gov/info'
 environment File.read(InfoDir + '/env').chomp
 json_attribs InfoDir + '/chef-attributes.json'
+
+# Preparation for upgrading from Chef 12 to 13
+treat_deprecation_warnings_as_errors true
+resource_cloning false
