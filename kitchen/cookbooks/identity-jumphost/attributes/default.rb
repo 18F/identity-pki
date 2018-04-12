a903d197936447c1178f565e741191bf5a7653c9
Andy Brody <andrew.brody@gsa.gov>
2018-04-12 15:27:26 -0400

    Remove terraform from jumphost, no longer used.

--- kitchen/cookbooks/identity-jumphost/attributes/default.rb
+++ kitchen/cookbooks/identity-jumphost/attributes/default.rb
@@ -1,7 +1,4 @@
 
-# more modern version of terraform
-default['terraform']['version'] = '0.8.5'
-
 # allow us to connect to anything
 default['squid']['ssl_ports'] = ['1-65535']
 default['squid']['safe_ports'] = ['1-65535']
