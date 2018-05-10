177186d9fe259425d20d5fe3ae5037e4952439ae
Andy Brody <andrew.brody@gsa.gov>
2018-05-10 13:00:32 -0400

    Create a homedir for the appinstall user.
    Ideally we wouldn't need to do this, but bundler does stupid things
    involving writing to predictable paths under /tmp/bundler/home/ if it
    can't write to your home directory, so it probably makes sense to work
    around that by just allowing it to write to an actual home directory.

--- kitchen/cookbooks/login_dot_gov/recipes/system_users.rb
+++ kitchen/cookbooks/login_dot_gov/recipes/system_users.rb
@@ -3,7 +3,8 @@ group node.fetch('login_dot_gov').fetch('system_user') do
   system true
 end
 user node.fetch('login_dot_gov').fetch('system_user') do
-  home '/nonexistent'
+  home '/home/' + node.fetch('login_dot_gov').fetch('system_user')
+  manage_home true
   shell '/usr/sbin/nologin'
   system true
   gid node.fetch('login_dot_gov').fetch('system_user')
