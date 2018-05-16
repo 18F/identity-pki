4c95891ac50d55fd8dfd4cc972672728cd0b89db
Andy Brody <andrew.brody@gsa.gov>
2018-05-16 16:59:16 -0400

    Add TF 0.10 style provider version pins.

--- terraform-app/main.tf
+++ terraform-app/main.tf
@@ -1,6 +1,11 @@
 provider "aws" {
   region = "${var.region}"
+  version = "~> 1.18"
 }
+
+provider "external" { version = "~> 1.0" }
+provider "null"     { version = "~> 1.0" }
+provider "template" { version = "~> 1.0" }
 
 # Stub remote config needed for terraform 0.9.*
 terraform {
