753c03399c876bbcc672bd57e0e7f6c356ce8027
Andy Brody <andrew.brody@gsa.gov>
2018-05-23 20:04:37 -0400

    Move ELB access logs bucket into shared module.
    The access_logs_bucket module can now be found in the public
    identity-terraform repo at:
    https://github.com/18F/identity-terraform/tree/master/elb_access_logs_bucket
    
    Also add lifecycle rules to gradually move logs to cheaper storage and
    delete it after 6 years.

--- terraform-common/sharedbucket.tf
+++ terraform-common/sharedbucket.tf
@@ -47,11 +47,16 @@ resource "aws_s3_bucket" "shared" {
 # The bucket name will be like this:
 #   login-gov.elb-logs.<ACCOUNT_ID>-<AWS_REGION>
 module "elb-logs" {
-  source = "../terraform-modules/access_logs_bucket/"
+  # can't use variable for ref -- see https://github.com/hashicorp/terraform/issues/17994
+  source = "github.com/18F/identity-terraform//elb_access_logs_bucket?ref=9e34866507bc0f0b03b23ee7e71ef31dfe117280"
 
   region = "${var.region}"
   bucket_name_prefix = "login-gov"
   use_prefix_for_permissions = false
+
+  lifecycle_days_standard_ia = 60   # 2 months
+  lifecycle_days_glacier     = 365  # 1 year
+  lifecycle_days_expire      = 2190 # 6 years
 }
 
 output "elb_log_bucket" {
--- terraform-modules/access_logs_bucket/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "bucket_name" {
-    description = "Name of the s3 logs bucket that was created"
-    value = "${aws_s3_bucket.logs.id}"
-}
