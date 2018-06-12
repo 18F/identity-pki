538f47996e4e15bdbd4b07e7c606e9ee34e7da79
Brian Crissup <crissupb@amazon.com>
2018-06-12 13:11:49 -0400

    Configure proxy for aws inspector agent

--- kitchen/cookbooks/login_dot_gov/recipes/configure_awsagent.rb
+++ kitchen/cookbooks/login_dot_gov/recipes/configure_awsagent.rb
@@ -13,3 +13,21 @@ template '/var/awslogs/etc/awslogs.conf' do
     action [:enable, :start]
     supports :restart => true, :status => true, :start => true, :stop => true
  end
+
+ #inspector agent proxy configuration
+ template '/etc/init.d/awsagent.env' do
+  source 'awsagent.env.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  variables ({
+    :proxyUrl => ENV['http_proxy'],
+    :noProxy => '169.254.169.254'
+ })
+  notifies :restart, 'service[awsagent]', :immediate
+end
+
+service 'awsagent' do
+  action [:enable, :start]
+  supports :restart => true, :start => true, :stop => true
+end
