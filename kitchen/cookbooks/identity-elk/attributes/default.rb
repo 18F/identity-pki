6db2e82fd93a470ac72299a7c279fda34421446f
Justin Grevich <justin@grevi.ch>
2018-06-15 00:47:16 -0700

    Pin to latest version v0.1.32

--- kitchen/cookbooks/identity-elk/attributes/default.rb
+++ kitchen/cookbooks/identity-elk/attributes/default.rb
@@ -61,7 +61,7 @@ default['elk']['users'] = [ ]
 # set this so that we listen on 8443
 default['apache']['listen'] = [8443]
 
-default['elk']['elastalert']['version'] = 'v0.1.31'
+default['elk']['elastalert']['version'] = 'v0.1.32'
 # If the list of emails is empty, then do not email.
 default['elk']['elastalert']['emails'] = []
 
