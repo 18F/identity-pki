a368f1a1d27d886c61d872853c8ba3fd1910f470
Justin Grevich <justin@grevi.ch>
2018-07-10 14:06:37 -0700

    Standardize on yml over yaml

--- kitchen/cookbooks/identity-elk/recipes/default.rb
+++ kitchen/cookbooks/identity-elk/recipes/default.rb
@@ -522,7 +522,7 @@ template "#{elastalertdir}/config.yaml" do
   notifies :restart, 'runit_service[elastalert]'
 end
 
-%w{alb429.yaml alb5xx.yaml invaliduser.yaml nologs.yaml failedlogins.yaml unknownip.yaml}.each do |t|
+%w{alb429.yml alb5xx.yml invaliduser.yml nologs.yml failedlogins.yml proxyblock.yml unknownip.yml}.each do |t|
   template "#{elastalertdir}/rules.d/#{t}" do
     source "elastalert_#{t}.erb"
     variables ({
