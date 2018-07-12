a3b89a7592b8d461d1c367982ab022b5cf3f8c1d
Brian Crissup <crissupb@amazon.com>
2018-07-12 08:53:41 -0400

    Create secretsmanager endpoint, sg and set no_proxy

--- terraform-app/vpc_endpoints.tf
+++ terraform-app/vpc_endpoints.tf
@@ -157,6 +157,40 @@ resource "aws_security_group" "logs_endpoint" {
   vpc_id = "${aws_vpc.default.id}"
 }
 
+resource "aws_security_group" "secretsmanager_endpoint" {
+  description = "Allow inbound from all servers"
+
+  # allow outbound to the VPC
+  egress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["${var.vpc_cidr_block}"]
+  }
+
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    security_groups = [
+        "${aws_security_group.idp.id}",
+        "${aws_security_group.app.id}",
+        "${aws_security_group.obproxy.id}",
+        "${aws_security_group.jumphost.id}",
+        "${aws_security_group.elk.id}",
+        "${aws_security_group.pivcac.id}",
+    ]
+  }
+
+  name = "${var.name}-secretsmanager_endpoint-${var.env_name}"
+
+  tags {
+    Name = "${var.name}-secretsmanager_endpoint-${var.env_name}"
+  }
+
+  vpc_id = "${aws_vpc.default.id}"
+}
+
 resource "aws_vpc_endpoint" "kms" {
   vpc_id = "${aws_vpc.default.id}"
   service_name = "com.amazonaws.${var.region}.kms"
@@ -236,6 +270,24 @@ resource "aws_vpc_endpoint" "ec2messages" {
 
   security_group_ids = [
     "${aws_security_group.ec2messages_endpoint.id}",
+    ]
+
+  subnet_ids = [
+    "${aws_subnet.privatesubnet1.id}",
+    "${aws_subnet.privatesubnet2.id}",
+    "${aws_subnet.privatesubnet3.id}",
+  ]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "secretsmanager" {
+  vpc_id = "${aws_vpc.default.id}"
+  service_name = "com.amazonaws.${var.region}.secretsmanager"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    "${aws_security_group.secretsmanager_endpoint.id}",
     ]
 
   subnet_ids = [
