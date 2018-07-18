84f6a20ef08bd41bfcf29a335d2abea2d82bbcc4
Justin Grevich <justin@grevi.ch>
2018-07-18 16:20:30 -0700

    Enable proxyblock alert __Why__
    * I forgot to add this to the list of templates that are added to the host. I
    also forgot to rename the template yml->yaml.

--- kitchen/cookbooks/identity-elk/recipes/default.rb
+++ kitchen/cookbooks/identity-elk/recipes/default.rb
@@ -484,7 +484,7 @@ template "#{elastalertdir}/config.yaml" do
   notifies :restart, 'runit_service[elastalert]'
 end
 
-%w{alb429.yaml alb5xx.yaml invaliduser.yaml nologs.yaml failedlogins.yaml unknownip.yaml}.each do |t|
+%w{alb429.yaml alb5xx.yaml failedlogins.yaml invaliduser.yaml nologs.yaml proxyblock.yaml unknownip.yaml}.each do |t|
   template "#{elastalertdir}/rules.d/#{t}" do
     source "elastalert_#{t}.erb"
     variables ({
