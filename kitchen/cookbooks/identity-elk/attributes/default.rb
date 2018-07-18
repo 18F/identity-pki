d06d071921ccaacb2f6a56430d11611755eaecb1
Justin Grevich <justin@grevi.ch>
2018-07-18 16:50:26 -0700

    Update cloudtrail and kibana plugins for 5.6.x __Why__
    * The previous cloudtrail version worked fine with 5.6 but it can't hurt to
    upgrade to the latest version. The kibana logtrail plugin did need to be
    upgraded for 5.6.x compatibility. I also had to upload a downloadable release
    in our identity-cookbooks repo since the maintainer does not have releases for
    all versions. Boo!

--- kitchen/cookbooks/identity-elk/attributes/default.rb
+++ kitchen/cookbooks/identity-elk/attributes/default.rb
@@ -46,10 +46,9 @@ default['java']['jdk_version'] = '8'
 # remote files
 default['elk']['kibanatarball'] = 'https://artifacts.elastic.co/downloads/kibana/kibana-5.6.9-linux-x86_64.tar.gz'
 default['elk']['logstashdeb'] = 'https://artifacts.elastic.co/downloads/logstash/logstash-5.6.9.deb'
-default['elk']['logstash-input-cloudwatch-logs-version'] = '1.0.1'
+default['elk']['logstash-input-cloudwatch-logs-version'] = '1.0.3'
 default['elk']['logstash-codec-cloudtrail-version'] = '3.0.4'
-default['elk']['kibanalogtrailplugin'] = 'https://github.com/sivasamyk/logtrail/releases/download/0.1.7/logtrail-5.x-0.1.7.zip'
-
+default['elk']['kibanalogtrailplugin'] = 'https://github.com/18F/identity-cookbooks/raw/jjg/logtrail-5.6.9/logtrail/logtrail-5.6.9-0.1.23.zip'
 
 # users to allow into elk
 default['elk']['users'] = [ ]
@@ -103,4 +102,3 @@ default['elk']['elb_logging_bucket'] = "login-gov.elb-logs.#{Chef::Recipe::AwsMe
 
 # use this to turn off analytics logging support
 default['elk']['analytics_logs'] = true
-
