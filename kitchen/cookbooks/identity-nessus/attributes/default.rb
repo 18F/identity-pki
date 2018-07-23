90b47dbda2a96e560b199bf646a1fcdf28ef5cf9
Brian Crissup <crissupb@amazon.com>
2018-07-23 19:09:52 -0400

    Remove nessus agent installation.

--- kitchen/cookbooks/identity-nessus/attributes/default.rb
+++ kitchen/cookbooks/identity-nessus/attributes/default.rb
@@ -2,5 +2,4 @@
 # Cookbook Name:: identity-nessus
 # Attributes:: default
 #
-default['identity-nessus']['deb_name'] = 'NessusAgent-7.0.1-ubuntu1110_amd64.deb'
-default['identity-nessus']['sha256sum'] = '0d02d61ba390ce03ecf7495d0370ef7a31234cf9e825529224f0b4e068a5f757'
+
