c56cbc44e77af9e4f084c8ca9eb81c454ab9e8a2
Andy Brody <andrew.brody@gsa.gov>
2018-09-26 17:46:10 -0400

    Clean up test indendation.

--- nodes/outboundproxy/test/smoke/default/default_test.rb
+++ nodes/outboundproxy/test/smoke/default/default_test.rb
@@ -6,28 +6,27 @@
 # found at http://inspec.io/docs/reference/resources/
 
 describe user('proxy') do
-    it { should exist }
-  end
-  
-  describe service('ssh') do
-    it { should be_installed }
-    it { should be_enabled }
-    it { should be_running }
-  end
-  
-  describe service('squid') do
-    it { should be_installed }
-    it { should be_enabled }
-  end
-  
-  describe port(3128) do
-    it { should be_listening }
-    its('processes') {should include 'squid'}
-  end
-  
-  # make sure we can sudo
-  describe command('sudo whoami') do
-    its('stdout') { should eq "root\n" }
-    its('exit_status') { should eq 0 }
-  end
-  
+  it { should exist }
+end
+
+describe service('ssh') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('squid') do
+  it { should be_installed }
+  it { should be_enabled }
+end
+
+describe port(3128) do
+  it { should be_listening }
+  its('processes') {should include 'squid'}
+end
+
+# make sure we can sudo
+describe command('sudo whoami') do
+  its('stdout') { should eq "root\n" }
+  its('exit_status') { should eq 0 }
+end
