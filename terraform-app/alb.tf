4866bb9e38c0dc281202c615a81c7e251e100554
Andy Brody <andrew.brody@gsa.gov>
2018-11-26 20:18:13 -0500

    Switch to new module for auto creating ACM certs.
    This switches the idp and sample app ALBs to use Terraform's new support
    for ACM to automatically verify and issue TLS certificates.
    
    As a result of this, terraform will automatically create all the
    necessary CNAMEs for domain verification, and we will no longer ever
    have to issue TLS certificates manually.

--- terraform-app/alb.tf
+++ terraform-app/alb.tf
@@ -13,27 +13,35 @@ resource "aws_alb" "idp" {
   enable_deletion_protection = "${var.enable_deletion_protection}"
 }
 
-# This cert should be created by hand with these names:
-# idp.env.login.gov
-# env.login.gov
-#
-# (Or in prod, that's secure.login.gov and idp.env.login.gov)
-#
-# https://us-west-2.console.aws.amazon.com/acm/home?region=us-west-2#/
-#
-# Once the cert has been created by hand, this terraform data resource will
-# discover it so that we can associate the cert with the ALB.
-#
-data "aws_acm_certificate" "idp" {
-    count = "${var.alb_enabled * var.acm_certs_enabled}"
-    # secure.login.gov in prod, idp.$env.login.gov in other environments
-    domain = "${var.env_name == "prod" ? "secure.${var.root_domain}" : "idp.${var.env_name}.${var.root_domain}"}"
-    statuses = ["ISSUED"]
+locals {
+  # In prod, the TLS cert has only "secure.<domain>"
+  # In other environments, the TLS cert has "idp.<env>.<domain>" and "<env>.<domain>"
+  idp_domain_name = "${var.env_name == "prod" ? "secure.${var.root_domain}" : "idp.${var.env_name}.${var.root_domain}"}"
+
+  # until we upgrade to TF 0.12 we can't use lists as the value outputted by a conditional, so split on space
+  idp_subject_alt_names = ["${compact(split(" ", var.env_name == "prod" ? "" : "${var.env_name}.${var.root_domain}"))}"]
+}
+
+# Create a TLS certificate with ACM
+module "acm-cert-idp" {
+  source = "github.com/18F/identity-terraform//acm_certificate?ref=2c43bfd79a8a2377657bc8ed4764c3321c0f8e80"
+  enabled = "${var.apps_enabled * var.acm_certs_enabled}"
+  domain_name = "${local.idp_domain_name}"
+  subject_alternative_names = ["${local.idp_subject_alt_names}"]
+  validation_zone_id = "${var.route53_id}"
+}
+
+# Fake resource to allow depends_on
+# TODO: this can go away in TF 0.12
+# https://github.com/hashicorp/terraform/issues/16983
+resource "null_resource" "acm-idp-issued" {
+  triggers {
+    finished = "${module.acm-cert-idp.finished_id}"
+  }
 }
 
 resource "aws_alb_listener" "idp" {
   count = "${var.alb_enabled * var.alb_http_port_80_enabled}"
-  depends_on = ["aws_alb.idp"]
   load_balancer_arn = "${aws_alb.idp.id}"
   port = "80"
   protocol = "HTTP"
@@ -42,11 +50,15 @@ resource "aws_alb_listener" "idp" {
     target_group_arn = "${aws_alb_target_group.idp.id}"
     type = "forward"
   }
+
+  # TODO TF 0.12 syntax:
+  # depends_on = ["module.acm-cert-idp.finished_id"] # don't use cert until valid
+  depends_on = ["null_resource.acm-idp-issued"] # don't use cert until valid
 }
 
 resource "aws_alb_listener" "idp-ssl" {
   count = "${var.alb_enabled}"
-  certificate_arn = "${data.aws_acm_certificate.idp.arn}"
+  certificate_arn = "${module.acm-cert-idp.cert_arn}"
   load_balancer_arn = "${aws_alb.idp.id}"
   port = "443"
   protocol = "HTTPS"
