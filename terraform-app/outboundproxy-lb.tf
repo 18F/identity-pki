a7bc9c68947f41f599e925696429a483965bcb96
Andy Brody <andrew.brody@gsa.gov>
2019-01-10 11:29:41 -0500

    Fix name clash with existing target group.

--- terraform-app/outboundproxy-lb.tf
+++ terraform-app/outboundproxy-lb.tf
@@ -62,7 +62,7 @@ resource "aws_lb_listener" "obproxy" {
 
 resource "aws_lb_target_group" "obproxy" {
   depends_on = ["aws_lb.obproxy"]
-  name = "${var.env_name}-obproxy-target"
+  name = "${var.env_name}-obproxy2-target"
   port = 3128
   protocol = "TCP"
   vpc_id = "${aws_vpc.default.id}"
