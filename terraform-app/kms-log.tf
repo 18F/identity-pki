8e7888ac298d69fb02edae74de99d6d5b37a3eb4
Brian Crissup <crissupb@amazon.com>
2019-03-05 13:30:36 -0500

    Add variable to enable/disable service

--- terraform-app/kms-log.tf
+++ terraform-app/kms-log.tf
@@ -1,6 +1,7 @@
 module "kms_logging" {
-    #source = "github.com/18F/identity-terraform//kms_log?ref=3caf222d0b191a24200c7e901db57ea8031af821"
-    source = "../../identity-terraform/kms_log"
+    source = "github.com/18F/identity-terraform//kms_log?ref=ebaf7cb3d371fcadc8770406280d40860adf5f7e"
+    #source = "../../identity-terraform/kms_log"
 
     env_name = "${var.env_name}"
+    kmslogging_service_enabled = "${var.kmslogging_enabled}"
 }
