c8f0fe12deba0865a94ed8a047e1826139e4c135
Brian Crissup <crissupb@amazon.com>
2019-03-06 08:37:49 -0500

    Add variable and update gitref

--- terraform-app/kms-log.tf
+++ terraform-app/kms-log.tf
@@ -1,5 +1,5 @@
 module "kms_logging" {
-    source = "github.com/18F/identity-terraform//kms_log?ref=ebaf7cb3d371fcadc8770406280d40860adf5f7e"
+    source = "github.com/18F/identity-terraform//kms_log?ref=13751594b89fa590d6d5f75140f87fdbb1015747"
     #source = "../../identity-terraform/kms_log"
 
     env_name = "${var.env_name}"
