3f8286f7b0959ab0a62b46e55de917f1009ec034
Brian Crissup <crissupb@amazon.com>
2019-03-06 09:40:53 -0500

    Update gitref

--- terraform-app/kms-log.tf
+++ terraform-app/kms-log.tf
@@ -1,5 +1,5 @@
 module "kms_logging" {
-    source = "github.com/18F/identity-terraform//kms_log?ref=13751594b89fa590d6d5f75140f87fdbb1015747"
+    source = "github.com/18F/identity-terraform//kms_log?ref=809753009069f5f850490b510d5361f3ff2a7b08"
     #source = "../../identity-terraform/kms_log"
 
     env_name = "${var.env_name}"
