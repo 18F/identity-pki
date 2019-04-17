df952a82aafa6a2263ae825a7ecf1676b511d7e6
Mark Ryan <34074974+mark-from-usds@users.noreply.github.com>
2019-04-17 11:27:16 -0400

    Use the Slack SNS topic we already defined for KMS dead letters (#1397)

--- terraform-app/kms-log.tf
+++ terraform-app/kms-log.tf
@@ -1,7 +1,8 @@
 module "kms_logging" {
-    source = "github.com/18F/identity-terraform//kms_log?ref=a5069fac498340b276eb1cc5d4a0817138a3015d"
+    source = "github.com/18F/identity-terraform//kms_log?ref=dbb3dde192c7df37fe0424606eaf8650177751f1"
     #source = "../../identity-terraform/kms_log"
 
     env_name = "${var.env_name}"
     kmslogging_service_enabled = "${var.kmslogging_enabled}"
+    sns_topic_dead_letter_arn = "${var.slack_events_sns_hook_arn}"
 }
