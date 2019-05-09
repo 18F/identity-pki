7d2c347d9fa0cd0eff2186f48bd0e6de79ef8c45
Justin Grevich <justin@grevi.ch>
2019-05-09 14:53:29 -0700

    Include new base images with latest changes

--- terraform-app/variables.tf
+++ terraform-app/variables.tf
@@ -266,12 +266,12 @@ variable "bootstrap_private_git_clone_url" {
 # though they will have different IDs. They should be updated here at the same
 # time, and then released to environments in sequence.
 variable "default_ami_id_sandbox" {
-    default = "ami-0898b420cfe498563" # 2019-03-18
+    default = "ami-0cc0b88dba28174d1" # 2019-05-09
     description = "default AMI ID for environments in the sandbox account"
 }
 
 variable "default_ami_id_prod" {
-    default = "ami-06e909b1f11a7e998" # 2019-03-18
+    default = "ami-0137f2fb266ffba6a" # 2019-05-09
     description = "default AMI ID for environments in the prod account"
 }
 
