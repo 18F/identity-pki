7f148099b2cdf3086e384f21a36d238776b70f60
Mark Ryan <34074974+mark-from-usds@users.noreply.github.com>
2019-05-09 12:59:48 -0400

    Update identity-terraform gitref again (#1442)

--- terraform-app/kms-log.tf
+++ terraform-app/kms-log.tf
@@ -1,5 +1,5 @@
 module "kms_logging" {
-    source = "github.com/18F/identity-terraform//kms_log?ref=ce5391322e3f38d8ba4359997b16dc5937330d22"
+    source = "github.com/18F/identity-terraform//kms_log?ref=ce6978c2ebe1e0997a7f88d56fbdd05d1ea687d6"
     #source = "../../identity-terraform/kms_log"
 
     env_name = "${var.env_name}"
