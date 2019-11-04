6cac7c0b90924d95e1d552e8d86c66e36a9a9f96
Andy Brody <andrew.brody@gsa.gov>
2019-11-04 14:25:29 -0500

    Improve monitoring and alerting for Pinpoint.
    Add CloudWatch alarms to provide alerting for several key conditions
    related to sending SMS with AWS Pinpoint. These apply to SMS sending.
    There don't immediately appear to be any metrics for the SMS-Voice API.
    
    Add several spend limit alarms:
    - Alert to Slack at 80% of calendar month cumulative spending limit.
    - Alert to OpsGenie at 90% of calendar month cumulative spending limit.
    - Alert to Slack if daily spending exceeds monthly limit / 31.
    
    These will alert us if we are in danger of exceeding the monthly spend
    limits, which is important because AWS will immediately reject all SMS
    messages if we exceed these limits.
    
    Also add alarms that alert to Slack for all available types of SMS error
    metrics.
    
    Add new metrics to the Cloudwatch Pinpoint dashboard:
    
    - In addition to DirectSendMessageThrottled, add
      DirectSendMessageTemporaryFailure and DirectSendMessagePermanentFailure.
      The temporary failures metric is the one that has been firing in our
      recent error spikes.
    
    - Add cumulative monthly spending and daily spending rate to the
      dashboard. Also add a horizontal annotation for the monthly limit.

--- terraform-sms/prod/main.tf
+++ terraform-sms/prod/main.tf
@@ -30,6 +30,8 @@ module "main" {
   pinpoint_app_name = "login.gov"
   state_lock_table = "terraform_locks"
   opsgenie_devops_high_endpoint = "https://api.opsgenie.com/v1/json/amazonsns?apiKey=1b1a2d80-6260-460a-995a-5200876f7372"
+  sns_topic_arn_slack_events = "arn:aws:sns:us-west-2:472911866628:slack-login-events"
+  pinpoint_spend_limit = 100000 # USD monthly
 }
 
 output "pinpoint_app_id" {
