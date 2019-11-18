c586ed83c512720768aa4929c1ce2ef4f3f80827
Justin Grevich <justin@grevi.ch>
2019-11-18 14:32:28 -0800

    Update version to match the rest of the stack

--- kitchen/cookbooks/identity-elk/recipes/filebeat.rb
+++ kitchen/cookbooks/identity-elk/recipes/filebeat.rb
@@ -1,12 +1,18 @@
 # This cookbook installs filebeat to send stuff to logstash
 
-# If we were running with a chef server, we could use the chef server's node
-# search functionality to find other services.
-#
-# However, we are running chef-zero locally, so we need to rely on some other
-# mechanism.  Our service_discovery cookbook abstracts out this service
-# discovery and has a helper resource to install the certificates of the
-# discovered nodes locally, so we can call that.
+filebeat_install 'default' do
+  version '7.3.1'
+  notifies :enable, 'service[filebeat]'
+end
+
+directory '/etc/systemd/system/filebeat.service.d'
+
+# override default systemd config to log to file (removes -e when loading)
+cookbook_file '/etc/systemd/system/filebeat.service.d/enable_logging.conf' do
+  mode '0755'
+  notifies :restart, 'service[filebeat]'
+end
+
 # TODO: Don't use this suffix, just use the base host certificate
 install_certificates 'Installing ELK certificates to ca-certificates' do
   service_tag_key node['elk']['elk_tag_key']
@@ -18,56 +24,31 @@ end
 
 execute '/usr/sbin/update-ca-certificates' do
   action :nothing
-  notifies :reload, 'service[filebeat]'
+  notifies :restart, 'service[filebeat]'
+end
+
+template "/etc/filebeat/filebeat.yml" do
+  source 'filebeat.yml.erb'
+  variables ({
+    :logstash_hosts => node['filebeat'].fetch('config').fetch('output').fetch('logstash').fetch('hosts'),
+    :logstash_ssl_certificate_authorities => node['filebeat'].fetch('config').fetch('output').fetch('logstash').fetch('tls').fetch('certificate_authorities')
+  })
+  notifies :restart, 'service[filebeat]'
+end
+
+directory '/etc/filebeat/inputs.d'
+
+template "/etc/filebeat/inputs.d/default.yml" do
+  source 'filebeat_inputs_default.yml.erb'
+  variables ({
+    :logfiles => node['filebeat'].fetch('logfiles'),
+  })
+  notifies :restart, 'service[filebeat]'
 end
 
 service 'filebeat' do
-  reload_command 'systemctl force-reload filebeat.service'
+  action :enable
 end
-
-filebeat_install 'default' do
-  version '6.8.3'
-end
-
-filebeat_conf = {
-  'output.logstash.hosts' => ['logstash.login.gov.internal:5044'],
-  'output.logstash.ssl.certificate_authorities' => ["/etc/ssl/certs/ca-certificates.crt"]
-}
-
-monitoring_conf = {
-  'xpack.monitoring.enabled' => true,
-  'xpack.monitoring.elasticsearch.hosts' => ['https://elasticsearch.login.gov.internal:9200'],
-  'xpack.monitoring.elasticsearch.ssl.certificate_authorities' => ["/etc/elasticsearch/root-ca.pem"]
-}
-
-# add monitoring config for instances with elasticsearch and elk roles. This is because ATM those
-# are the only node that can connect directly to the elasticsearch cluster.
-if ['elasticsearch', 'elk'].include?(node.fetch('roles'))
-  filebeat_conf = filebeat_conf.merge(monitoring_conf)
-end
-
-filebeat_config 'default' do
-  config filebeat_conf
-end
-
-node['elk']['filebeat']['logfiles'].each do |logitem|
-  logfile = logitem.fetch('log')
-
-  conf = {
-    'enabled' => true,
-    'harvester_buffer_size' => 16384,
-    'ignore_older' => '24h',
-    'paths' => [logfile],
-    'scan_frequency' => '15s',
-    'type' => 'log'
-  }
-
-  filebeat_prospector logfile.gsub(/[\/\*]/,'_') do
-    config conf
-  end
-end
-
-filebeat_service 'default'
 
 cron 'rerun elk filebeat discovery every 15 minutes' do
   action :create
