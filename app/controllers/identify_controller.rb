af9efa465dda6ce5bf6f6933af88ec610c915e48
Aaron Chapman <aaron.chapman@gsa.gov>
2020-01-03 17:07:17 -0500

    codeclimate

--- app/controllers/identify_controller.rb
+++ app/controllers/identify_controller.rb
@@ -51,7 +51,8 @@ class IdentifyController < ApplicationController
   end
 
   def client_cert
-    cert_pem = request.headers[CERT_HEADER] || request.headers.env['rack.peer_cert']
+    headers = request.headers
+    cert_pem = headers[CERT_HEADER] || headers.env['rack.peer_cert']
     return unless cert_pem
     if Figaro.env.client_cert_escaped == 'true'
       CGI.unescape(cert_pem)
