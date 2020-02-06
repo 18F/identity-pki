6e04e5dbb221afe7c7f30d6262900e6275db1533
Brian Crissup <crissupb@amazon.com>
2020-02-06 14:50:23 -0500

    Fix parameters Remove tag copying

--- bin/copy-image.sh
+++ bin/copy-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set euo pipefail
+set -euo pipefail
 
 run() {
     echo >&2 "+ $*"
@@ -8,91 +8,89 @@ run() {
 
 usage() {
     cat >&2 << EOM
-Usage: ${0} [-a ami_id] [-s sourceAccountId]
-    -a,               ID of AMI to be copied.
-    -s,               Source account ID for image.
-    -r,               Image role to copy (base or rails).
-    -h,               Show this message.
+Usage: ${0} [-a ami_id] [-s sourceAccountId] [-r role]
+Usage: ${0} [--ami ami_id] [--source_account account id for image] [--role role]
+    -a|--ami,               ID of AMI to be copied.
+    -s|--source_account,    Source account ID for image.
+    -r|role,                Image role to copy (base or rails).
+    -h|help,                Show this message.
 
 By default, this will copy the most recently shared base ami from the sandbox account.
 For example:
 To copy the latest shared rails image from the sandbox account
-copy-image.sh -r rails
+${0} --role rails
 
 To copy the latest shared base image from the sandbox account
-copy-image.sh
+${0}
+
+To copy a specific ami from the sandbox account
+${0} --ami ami-38296298
 EOM
 }
 
-while getopts "a:s:r:" opt; do
-    case $opt in
-        h) usage && exit 1
-        ;;
-        a) AMI_ID="$OPTARG"
-        ;;
-        d) SRC_ACCT_ID="$OPTARG"
-        ;;
-        r) AMI_ROLE="$OPTARG"
-        ;;
-        \?) echo "Invalid option -$OPTARG" >&2
-        ;;
-    esac
-done
+#default role to base
+AMI_ROLE="base"
+#default destination to production account
+SRC_ACCT_ID="894947205914"
+AMI_ID=""
+SRC_REGION="us-west-2"
+
+while [[ "$#" -gt 0 ]]; do case $1 in 
+    -r|--role) AMI_ROLE="$2"; shift;;
+    -a|--ami) AMI_ID="$2"; shift;;
+    -d|--source_account) SRC_ACCT_ID="$2"; shift;;
+    -h|--help) usage && exit 1;;
+    *) echo "Unknown parameter passed $1"; exit 1;;
+esac; shift; done
 
 COLOR='\033[1;36m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-SRC_REGION="us-west-2"
-
-if [ "$SRC_ACCT_ID" == "" ]; then
-    #default to sandbox account
-    SRC_ACCT_ID="894947205914"
-fi
-
-if [ "$AMI_ROLE" == "" ]; then
-    #default to base image
-    AMI_ROLE="base"
-fi
+# Get current account
+ACCOUNT_DETAILS=$(aws sts get-caller-identity)
+ACCOUNT_ID=$(echo ${ACCOUNT_DETAILS} | jq -r '.Account')
+echo -e "${COLOR}Destination account ID:${NC}" ${ACCOUNT_ID}
 
 # Checking dependencies
 if ! command -v jq >/dev/null 2>&1 ; then
     echo "jq is required but not installed. Aborting. See https://stedolan.github.io/jq/download/"
+    exit 1
 fi
 
-echo -e "${COLOR}Source account ID:${NC}" ${DST_ACCT_ID}
+echo -e "${COLOR}Source account ID:${NC}" ${SRC_ACCT_ID}
 
-if [ "$AMI_ID" == "" ]; then
+if [ $ACCOUNT_ID == $SRC_ACCT_ID ]; then
+    echo -e "${RED}Destination and source account should be different. Make sure your AWS profile is not identity-dev (sandbox).${NC}"
+    exit 1
+fi
+# Name=tag:Role,Values=$AMI_ROLE 
+if [ -z $AMI_ID ]; then
     # Find latest image
-    AMI_ID=$(aws ec2 describe-images --filters Name=owner-id,Values=$SRC_ACCT_ID --query 'Images[*].[ImageId]' --output text | sort -k2 -r | head -n1)
+    AMI_ID=$(aws ec2 describe-images --owners $SRC_ACCT_ID --filters Name=name,Values=*$AMI_ROLE* --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
 fi
 
-echo -e $(aws ec2 describe-images --image-ids $AMI_ID --query 'Images[*].[ImageId,CreationDate,Name]' --output text)
+echo -e $(aws ec2 describe-images --image-ids $AMI_ID --query 'Images[*].[ImageId,CreationDate,Name]' --output table)
 
 # Describes the source AMI and stores its contents
 AMI_DETAILS=$(aws ec2 describe-images --image-id ${AMI_ID}  --query 'Images[0]')
 
 AMI_NAME=$(echo $AMI_DETAILS | jq -r '.Name')
-AMI_DESCRIPTION=$(echo $AMI_DETAILS | jq -r '.Description')
+#AMI_DESCRIPTION=$(echo $AMI_DETAILS | jq -r '.Description')
 
-NEW_AMI_ID=$(aws ec2 copy-image --source-image-id $AMI_ID --name  --source-region $SRC_REGION --encrypted --name $AMI_NAME --output text)
-echo -e ("New Image Id: " $NEW_AMI_ID)
+NEW_AMI_ID=$(aws ec2 copy-image --source-image-id $AMI_ID --name "$AMI_NAME" --source-region $SRC_REGION --encrypted --output text)
+echo -e "${COLOR}New Image Id: ${NC}" $NEW_AMI_ID
 
-sleep 5
-echo -e ("Copying ")
+sleep 10
+echo -e "Waiting for image to become available (this may take a couple of minutes)"
 while :;do echo -n .;sleep 1;done &
 trap "kill $!" EXIT  #Die with parent if we die prematurely
-IMAGE_STATE=$(aws ec2 describe-images --image-ids ami-0f50ce402f6f41e12 --query 'Images[*].[State]' --output text)
+IMAGE_STATE=$(aws ec2 describe-images --image-ids $NEW_AMI_ID --query 'Images[*].[State]' --output text)
 while [ $IMAGE_STATE == "pending" ] ; do
-    sleep 10
+    sleep 15
+    IMAGE_STATE=$(aws ec2 describe-images --image-ids $NEW_AMI_ID --query 'Images[*].[State]' --output text)
 done
-kill $! && trap " " EX
-echo -e ("Image Status:" $IMAGE_STATE)
 
-# Copy Tags
-AMI_TAGS=$(echo ${AMI_DETAILS} | jq '.Tags')"}"
-if [ "${AMI_TAGS}" != "null}" ]; then
-    NEW_AMI_TAGS="{\"Tags\":"$(echo ${AMI_TAGS} | tr -d ' ')
-    $(aws ec2 create-tags --resources ${NEW_AMI_ID} --cli-input-json ${NEW_AMI_TAGS} 
-    echo -e "${COLOR}Tags added sucessfully${NC}"
-fi
+echo -e "Image Status: " $IMAGE_STATE
+
+kill $! && trap " " EXIT
--- bin/share-image.sh
+++ bin/share-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set euo pipefail
+set -euo pipefail
 
 run() {
     echo >&2 "+ $*"
@@ -8,52 +8,45 @@ run() {
 
 usage() {
     cat >&2 << EOM
-Usage: ${0} [-a ami_id] [-d share to account id]
--a,               ID of AMI to be copied.
--d,               Account Id to share ami with.
--r                Image role to share (base or rails).
--h,               Show this message.
+Usage: ${0} [-a ami_id] [-d share to account id] [-r role]
+Usage: ${0} [--ami ami_id] [-destination_account share to account id] [--role role]
+-a|--ami,               ID of AMI to be copied.
+-d|destination_account, Account Id to share ami with.
+-r|--role               Image role to share (base or rails).
+-h|--help,              Show this message.
 
 By default, will use the latest base image ami and share with the production account.
 
 For example:
 To share the latest rails image with the production account
-share-image.sh -r rails
+${0} --role rails
 
 To share the latest base image with the production account
-share-image.sh
+${0}
+
+To share a specific ami with the production account
+${0} --ami ami-12345676
 
 EOM
 }
 
-while getopts "a:d:r:" opt; do
-    case $opt in
-        h) usage && exit 1
-        ;;
-        a) AMI_ID="$OPTARG"
-        ;;
-        d) DST_ACCT_ID="$OPTARG"
-        ;;
-        r) AMI_ROLE="$OPTARG"
-        ;;
-        \?) echo "Invalid option -$OPTARG" >&2
-        ;;
-    esac
-done
+#default role to base
+AMI_ROLE="base"
+#default destination to production account
+DST_ACCT_ID="555546682965"
+AMI_ID=""
+
+while [[ "$#" -gt 0 ]]; do case $1 in 
+    -r|--role) AMI_ROLE="$2"; shift;;
+    -a|--ami) AMI_ID="$2"; shift;;
+    -d|--destination_account) DST_ACCT_ID="$2"; shift;;
+    -h|--help) usage && exit 1;;
+    *) echo "Unknown parameter passed $1"; exit 1;;
+esac; shift; done
 
 COLOR='\033[1;36m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
-
-if [ "$DST_ACCT_ID" == "" ]; then
-    #default to production account
-    DST_ACCT_ID="555546682965"
-fi
-
-if [ "$AMI_ROLE" == "" ]; then
-    #default to base image
-    AMI_ROLE="base"
-fi
 
 # Checking dependencies
 if ! command -v jq >/dev/null 2>&1 ; then
@@ -66,17 +59,18 @@ echo -e "${COLOR}Destination account ID:${NC}" ${DST_ACCT_ID}
 # Get current account
 ACCOUNT_DETAILS=$(aws sts get-caller-identity)
 ACCOUNT_ID=$(echo ${ACCOUNT_DETAILS} | jq -r '.Account')
-echo -e "${COLOR}Source account ID:${NC}" ${ACCOUNT_ID}ß
-if [ ACCOUNT_ID != "894947205914" ]; then
+echo -e "${COLOR}Source account ID:${NC}" ${ACCOUNT_ID}
+if [ $ACCOUNT_ID != "894947205914" ]; then
     echo -e "${RED}Current account should be identity-dev (sandbox).${NC}"
+    exit 1
 fi
 
-if [ "$AMI_ID" == "" ]; then
+if [ -z $AMI_ID ]; then
     # Find latest image
-    AMI_ID=$(aws ec2 describe-images --filters Name=owner-id,Values=$ACCOUNT_ID,Name=tag:Role,Values=$AMI_ROLE --query 'Images[*].[ImageId]' --output text | sort -k2 -r | head -n1)
+    AMI_ID=$(aws ec2 describe-images --owners $ACCOUNT_ID --filters Name=tag:Role,Values=$AMI_ROLE --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
 fi
 
-echo -e $(aws ec2 describe-images --image-ids $AMI_ID --query 'Images[*].[ImageId,CreationDate,Name]' --output text)
+echo -e $(aws ec2 describe-images --image-ids $AMI_ID --query 'Images[*].[ImageId,CreationDate,Name]' --output table)
 
 # Describes the source AMI and stores its contents
 AMI_DETAILS=$(aws ec2 describe-images --image-id ${AMI_ID} --query 'Images[0]')
