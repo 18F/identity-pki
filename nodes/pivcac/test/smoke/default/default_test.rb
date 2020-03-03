db405e0720b47096af6dad8ef69b4ab7f9e96a64
Justin Grevich <justin@grevi.ch>
2020-03-02 18:16:19 -0800

    Use the file resource for checking filebeat log

--- nodes/pivcac/test/smoke/default/default_test.rb
+++ nodes/pivcac/test/smoke/default/default_test.rb
@@ -68,51 +68,51 @@ end
 
 
 # filebeat is harvesting common logs
-describe command("grep 'Harvester started for file' /var/log/filebeat/filebea* | awk '{print $NF}' | sort | uniq") do
-  its('stdout') { should include '/var/log/alternatives.log' }
-  its('stdout') { should include '/var/log/amazon/ssm/amazon-ssm-agent.log' }
-  its('stdout') { should include '/var/log/amazon/ssm/errors.log' }
-  its('stdout') { should include '/var/log/amazon/ssm/hibernate.log' }
-  its('stdout') { should include '/var/log/apport.log' }
-  its('stdout') { should include '/var/log/apt/history.log' }
-  its('stdout') { should include '/var/log/apt/term.log' }
-  its('stdout') { should include '/var/log/audit/audit.log' }
-  its('stdout') { should include '/var/log/auth.log' }
+describe file('/var/log/filebeat/filebeat') do
+  its('content') { should include '/var/log/alternatives.log' }
+  its('content') { should include '/var/log/amazon/ssm/amazon-ssm-agent.log' }
+  its('content') { should include '/var/log/amazon/ssm/errors.log' }
+  its('content') { should include '/var/log/amazon/ssm/hibernate.log' }
+  its('content') { should include '/var/log/apport.log' }
+  its('content') { should include '/var/log/apt/history.log' }
+  its('content') { should include '/var/log/apt/term.log' }
+  its('content') { should include '/var/log/audit/audit.log' }
+  its('content') { should include '/var/log/auth.log' }
 # TODO: add once we either test the awsagent update process or the build of this instance takes long
 # enough for the awsagent update to occur automatically.
-#  its('stdout') { should include '/var/log/awsagent-update.log' }
-  its('stdout') { should include '/var/log/awslogs-agent-setup.log' }
-  its('stdout') { should include '/var/log/awslogs.log' }
-  its('stdout') { should include '/var/log/clamav/clamav.log' }
+#  its('content') { should include '/var/log/awsagent-update.log' }
+  its('content') { should include '/var/log/awslogs-agent-setup.log' }
+  its('content') { should include '/var/log/awslogs.log' }
+  its('content') { should include '/var/log/clamav/clamav.log' }
 # TODO: add once we have a test that updates the clamav definitions.
-  its('stdout') { should include '/var/log/clamav/freshclam.log' }
-  its('stdout') { should include '/var/log/cloud-init-output.log' }
-  its('stdout') { should include '/var/log/cloud-init.log' }
-  its('stdout') { should include '/var/log/dnsmasq.log' }
-  its('stdout') { should include '/var/log/dpkg.log' }
+  its('content') { should include '/var/log/clamav/freshclam.log' }
+  its('content') { should include '/var/log/cloud-init-output.log' }
+  its('content') { should include '/var/log/cloud-init.log' }
+  its('content') { should include '/var/log/dnsmasq.log' }
+  its('content') { should include '/var/log/dpkg.log' }
 # TODO: perhaps remove this from common since it seems to only be present on ELK instances  
-#  its('stdout') { should include '/var/log/fontconfig.log' }
-  its('stdout') { should include '/var/log/grubfix.log' }
-  its('stdout') { should include '/var/log/kern.log' }
+#  its('content') { should include '/var/log/fontconfig.log' }
+  its('content') { should include '/var/log/grubfix.log' }
+  its('content') { should include '/var/log/kern.log' }
 # NOTE: this does not seem to be used on the jumphost
-#  its('stdout') { should include '/var/log/landscape/sysinfo.log' }
-  its('stdout') { should include '/var/log/mail.log' }
-  its('stdout') { should include '/var/log/messages' }
+#  its('content') { should include '/var/log/landscape/sysinfo.log' }
+  its('content') { should include '/var/log/mail.log' }
+  its('content') { should include '/var/log/messages' }
 # TODO: add once we have a test for proxy and proxy cache.
-#  its('stdout') { should include '/var/log/squid/access.log' }
-#  its('stdout') { should include '/var/log/squid/cache.log' }
-  its('stdout') { should include '/var/log/sysctlfix.log' }
-  its('stdout') { should include '/var/log/syslog' }
-  its('stdout') { should include '/var/log/unattended-upgrades/unattended-upgrades-shutdown.log' }
+#  its('content') { should include '/var/log/squid/access.log' }
+#  its('content') { should include '/var/log/squid/cache.log' }
+  its('content') { should include '/var/log/sysctlfix.log' }
+  its('content') { should include '/var/log/syslog' }
+  its('content') { should include '/var/log/unattended-upgrades/unattended-upgrades-shutdown.log' }
 end
 
 # filebeat is harvesting instance specific logs
-describe command("grep 'Harvester started for file' /var/log/filebeat/filebea* | awk '{print $NF}' | sort | uniq") do
-  its('stdout') { should include '/var/log/nginx/access.log' }
-  its('stdout') { should include '/var/log/nginx/error.log' }
-  its('stdout') { should include '/var/log/nginx/fancy_access.log' }
+describe file('/var/log/filebeat/filebeat') do
+  its('content') { should include '/var/log/nginx/access.log' }
+  its('content') { should include '/var/log/nginx/error.log' }
+  its('content') { should include '/var/log/nginx/fancy_access.log' }
 # TODO: add once we have a test for the piv-cac cron tasks.
-#  its('stdout') { should include '/srv/pki-rails/shared/log/cron.log' }
-  its('stdout') { should include '/srv/pki-rails/shared/log/production.log' }
-  its('stdout') { should include '/srv/pki-rails/shared/log/newrelic_agent.log' }
+#  its('content') { should include '/srv/pki-rails/shared/log/cron.log' }
+  its('content') { should include '/srv/pki-rails/shared/log/production.log' }
+  its('content') { should include '/srv/pki-rails/shared/log/newrelic_agent.log' }
 end
