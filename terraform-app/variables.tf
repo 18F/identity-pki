bfc1641ac091b728b68f5b9d40a980dd7ab21e3f
bleachbyte <jonathan.pirro@gsa.gov>
2020-04-15 12:23:21 -0700

    moving Node.js and Yarn installs to base image

--- terraform-app/variables.tf
+++ terraform-app/variables.tf
@@ -521,22 +521,22 @@ variable "bootstrap_private_git_clone_url" {
 # though they will have different IDs. They should be updated here at the same
 # time, and then released to environments in sequence.
 variable "default_ami_id_sandbox" {
-  default     = "ami-083de24db16bd4fac" # 2020-03-31 Ubuntu 18.04
+  default     = "ami-00ac5137d7b3a7356" # 2020-03-31 Ubuntu 18.04
   description = "default AMI ID for environments in the sandbox account"
 }
 
 variable "default_ami_id_prod" {
-  default     = "ami-09345d225d537894e" # 2020-03-31 Ubuntu 18.04
+  default     = "ami-08ab08affb67e212e" # 2020-03-31 Ubuntu 18.04
   description = "default AMI ID for environments in the prod account"
 }
 
 variable "rails_ami_id_sandbox" {
-  default     = "ami-0f574f86c351b1124" # 2020-03-31 Ubuntu 18.04
+  default     = "ami-098006852ce1145da" # 2020-03-31 Ubuntu 18.04
   description = "AMI ID for Rails (IdP/PIVCAC servers) in the sandbox account"
 }
 
 variable "rails_ami_id_prod" {
-  default     = "ami-082995edbcb0f768e" # 2020-03-31 Ubuntu 18.04
+  default     = "ami-0c71f4019af78ee32" # 2020-03-31 Ubuntu 18.04
   description = "AMI ID for Rails (IdP/PIVCAC servers) in the prod account"
 }
 
