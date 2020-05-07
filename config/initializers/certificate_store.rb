73d810cf39521f930134fc8f32b9f9aa335bb784
Aaron Chapman <aaron.chapman@gsa.gov>
2020-05-07 12:23:18 -0400

    add bundled certs and fix state piv

--- config/initializers/certificate_store.rb
+++ config/initializers/certificate_store.rb
@@ -4,6 +4,7 @@ unless File.basename($PROGRAM_NAME) == 'rake' && ARGV.any? { |arg| arg.start_wit
   # load all of the files in config/certs
   Dir.chdir(Figaro.env.certificate_store_directory) do
     Dir.glob(File.join('**', '*.pem')).each do |file|
+      next if file == 'all_certs_deploy.pem'
       cert_store.add_pem_file(file)
     end
   end
