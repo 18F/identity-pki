a73660eac48e1ed9f898c4c1fb656ea73db07414
bleachbyte <jonathan.pirro@gsa.gov>
2020-05-26 11:39:53 -0700

    moving vars into main.tf

--- terraform/master/global/main.tf
+++ terraform/master/global/main.tf
@@ -3,12 +3,6 @@ provider "aws" {
   allowed_account_ids = ["340731855345"] # require login-master
   profile             = "login-master"
 
-  #assume_role {
-  #  role_arn     = "arn:aws:iam::ACCOUNT_ID:role/ROLE_NAME"
-  #  session_name = "SESSION_NAME"
-  #  external_id  = "EXTERNAL_ID"
-  #}
-
   version = "~> 2.29"
 }
 
@@ -21,14 +15,33 @@ terraform {
 module "main" {
   source = "../module"
 
-  region                          = "us-west-2"
-  #sandbox_account_id              = "894947205914"
-  #production_account_id           = "555546682965"
-  #sandbox_sms_account_id          = "035466892286"
-  #production_sms_account_id       = "472911866628"
-  #production_analytics_account_id = "461353137281"
-  #prod_secops_account_id          = "217680906704"
-  #dev_secops_account_id           = "138431511372"
-  #interviews_account_id           = "034795980528"
+  region            = "us-west-2"
+  master_account_id = "340731855345"
+  
+  prod_aws_account_nums    = [
+    "555546682965", # login-prod
+    "472911866628", # login-sms-prod
+    "461353137281", # login-analytics-prod
+    "217680906704", # login-secops-prod
+  ]
+  nonprod_aws_account_nums = [
+    "894947205914", # login-sandbox
+    "035466892286", # login-sms-sandbox
+    "138431511372", # login-secops-dev
+    "034795980528", # login-interviews
+  ]
+  role_list                = [
+    "FullAdministrator",
+    "PowerUser",
+    "ReadOnly",
+    "BillingReadOnly",
+    "ReportsReadOnly",
+    "KMSAdministrator",
+    "SOCAdministrator",
+  ]
+  auditor_accounts         = {
+    master        = "340731855345" # Include master for testing
+    techportfolio = "133032889584" # TTS Tech Portfolio
+  }
 }
 
--- terraform/master/module/variables.tf
+++ terraform/master/module/variables.tf
@@ -1,50 +1,29 @@
 variable "region" {
-  default = "us-west-2"
+  description = "AWS Region where this account lives."
 }
 
 variable "master_account_id" {
-  default = "340731855345"
+  description = "AWS Account ID for the master account."
 }
 
 variable "prod_aws_account_nums" {
   description = "List of account numbers for 'Prod'-type AWS accounts."
-  default     = [
-    "555546682965", # login-prod
-    "472911866628", # login-sms-prod
-    "461353137281", # login-analytics-prod
-    "217680906704", # login-secops-prod
-  ]
+  default     = []
 }
 
 variable "nonprod_aws_account_nums" {
   description = "List of account numbers for 'NonProd'-type (Sandbox, Dev, etc.) AWS accounts."
-  default     = [
-    "894947205914", # login-sandbox
-    "035466892286", # login-sms-sandbox
-    "138431511372", # login-secops-dev
-    "034795980528", # login-interviews
-  ]
+  default     = []
 }
 
 variable "role_list" {
   description = "List of roles available in the various AWS accounts."
-  default     = [
-    "FullAdministrator",
-    "PowerUser",
-    "ReadOnly",
-    "BillingReadOnly",
-    "ReportsReadOnly",
-    "KMSAdministrator",
-    "SOCAdministrator",
-  ]
+  default     = []
 }
 
 variable "auditor_accounts" {
   description = "Map of non-Login.gov AWS accounts we allow Security Auditor access to"
   # Unlike our master account, these are accounts we do not control!
   type        = map(string)
-  default     = {
-    master        = "340731855345" # Include master for testing
-    techportfolio = "133032889584" # TTS Tech Portfolio
-  }
+  default     = {}
 }
