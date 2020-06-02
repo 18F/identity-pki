b5a4a6f0b07d5cde4c119195b612847e05194e1f
bleachbyte <jonathan.pirro@gsa.gov>
2020-06-02 12:02:56 -0700

    connecting module.iam_groups to module.assume_roles via policy_depends_on

--- terraform/master/module/iam_env_roles.tf
+++ terraform/master/module/iam_env_roles.tf
@@ -1,6 +1,6 @@
 module "assume_roles" {
-  source = "github.com/18F/identity-terraform//iam_masterassume?ref=6e0d3619b3ddf7e59eb2755a267444b526df4c9a"
-  #source = "../../../../identity-terraform/iam_masterassume"
+  #source = "github.com/18F/identity-terraform//iam_masterassume?ref=6e0d3619b3ddf7e59eb2755a267444b526df4c9a"
+  source = "../../../../identity-terraform/iam_masterassume"
 
   role_list         = var.role_list
   aws_account_types = var.aws_account_types
--- terraform/master/module/iam_groups.tf
+++ terraform/master/module/iam_groups.tf
@@ -1,9 +1,10 @@
 # Groups and group policy attachments
 
 module "iam_groups" {
-  source = "github.com/18F/identity-terraform//iam_assumegroup?ref=6e0d3619b3ddf7e59eb2755a267444b526df4c9a"
-  #source = "../../../../identity-terraform/iam_assumegroup"
+  #source = "github.com/18F/identity-terraform//iam_assumegroup?ref=6e0d3619b3ddf7e59eb2755a267444b526df4c9a"
+  source = "../../../../identity-terraform/iam_assumegroup"
 
-  group_role_map = var.group_role_map
+  group_role_map    = var.group_role_map
   master_account_id = var.master_account_id
+  policy_depends_on = module.assume_roles.policy_arns
 }
