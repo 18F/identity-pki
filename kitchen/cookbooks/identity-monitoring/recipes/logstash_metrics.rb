b68172a9e87cbdb3c1545d490cbe502b33bc0683
timothy-spencer <timothy.spencer@gsa.gov>
2020-06-11 15:44:57 -0700

    first step in making sure that we have useful logstash/elasticsearch metrics going that we can alert on

---
+++ kitchen/cookbooks/identity-monitoring/recipes/logstash_metrics.rb
@@ -0,0 +1,49 @@
+# This installs the stuff we need to monitor logstash.
+# Run this recipe on elk nodes.
+
+gem_package "elasticsearch" do
+  gem_binary node.fetch('login_dot_gov').fetch('rbenv_shims_gem')
+end
+
+gem_package "ohai" do
+  gem_binary node['login_dot_gov']['rbenv_shims_gem']
+end
+
+gem_package "sys-proctable" do
+  gem_binary node['login_dot_gov']['rbenv_shims_gem']
+end
+
+logstash_health_path = "/#{Chef::Config['file_cache_path']}/logstash_health"
+
+template logstash_health_path do
+  mode '0755'
+  variables ({
+    ruby: node['login_dot_gov']['rbenv_shims_ruby']
+  })
+end
+
+newrelic_infra_integration 'logstash_health' do
+  integration_name 'logstash_health'
+  remote_url "file://#{logstash_health_path}"
+  install_method 'binary'
+  instances(
+    [
+      {
+        name: 'logstash_node_health',
+        command: "logstash_health",
+        arguments: {
+          check: 'node'
+        },
+        labels: {
+          environment: node.chef_environment
+        }
+      }
+    ]
+  )
+  commands ({
+    logstash_health: []
+  })
+end
+
+include_recipe 'newrelic-infra'
+
