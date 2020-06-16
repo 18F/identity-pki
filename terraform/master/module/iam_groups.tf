f64620575fb68470c8e58fab50a47a84c340323c
bleachbyte <jonathan.pirro@gsa.gov>
2020-06-16 08:12:33 -0700

    updating modules in master with correct SHAs

--- terraform/master/module/iam_groups.tf
+++ terraform/master/module/iam_groups.tf
@@ -1,7 +1,7 @@
 # Groups and group policy attachments
 
 module "iam_groups" {
-  source = "github.com/18F/identity-terraform//iam_assumegroup?ref=0f03f8bd441791bf872f4ea1711a3db07d9b6e1c"
+  source = "github.com/18F/identity-terraform//iam_assumegroup?ref=b77cb8282891a9044cf173baf8e25fc4892a535f"
   #source = "../../../../identity-terraform/iam_assumegroup"
 
   group_role_map    = var.group_role_map
--- terraform/master/module/iam_users.tf
+++ terraform/master/module/iam_users.tf
@@ -1,6 +1,6 @@
 # Users and group memberships
 module "master_users" {
-  source = "github.com/18F/identity-terraform//iam_masterusers?ref=0f03f8bd441791bf872f4ea1711a3db07d9b6e1c"
+  source = "github.com/18F/identity-terraform//iam_masterusers?ref=b77cb8282891a9044cf173baf8e25fc4892a535f"
   #source = "../../../../identity-terraform/iam_masterusers"
 
   user_map         = var.user_map
