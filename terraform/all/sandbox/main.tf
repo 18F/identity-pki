225e308fa6c0017a434fd79a4f810099b421e414
varunirv <varunirv@amazon.com>
2020-06-24 14:24:18 -0400

    configure iamaa

--- terraform/all/sandbox/main.tf
+++ terraform/all/sandbox/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region              = "us-west-2"
   allowed_account_ids = ["894947205914"] # require login-sandbox
   profile             = "identitysandbox.gov"
-  version             = "~> 2.37.0"
+  version             = "~> 2.67.0"
 }
 
 # Stub remote config
