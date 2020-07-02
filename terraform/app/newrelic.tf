06a3b7c723f465d7e0f4e05a2ae5d448948f1ed9
timothy-spencer <timothy.spencer@gsa.gov>
2020-07-02 11:25:27 -0700

    add some more docs to the commented out stuff

--- terraform/app/newrelic.tf
+++ terraform/app/newrelic.tf
@@ -1,5 +1,8 @@
 
 # XXX This is disabled until https://github.com/newrelic/terraform-provider-newrelic/issues/744 is resolved
+# XXX In the meantime, if you really want to deploy this, uncomment this stuff and download
+# XXX the plugin by hand from https://github.com/newrelic/terraform-provider-newrelic/releases/tag/v2.1.2 and
+# XXX put it into terraform/app/.terraform/plugins/darwin_amd64/
 # module "newrelic" {
 #   # once we go to terraform 0.13.x, we will be able to do this
 #   # count = var.newrelic_alerts_enabled
