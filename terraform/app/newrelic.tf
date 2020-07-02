9077e6d884368a8d8c5ec95b5dc237d6974f0a1e
timothy-spencer <timothy.spencer@gsa.gov>
2020-07-02 11:23:35 -0700

    disable newrelic until bug is fixed with plugin downloads

--- terraform/app/newrelic.tf
+++ terraform/app/newrelic.tf
@@ -1,11 +1,12 @@
 
-module "newrelic" {
-  # once we go to terraform 0.13.x, we will be able to do this
-  # count = var.newrelic_alerts_enabled
-  source = "../modules/newrelic/"
+# XXX This is disabled until https://github.com/newrelic/terraform-provider-newrelic/issues/744 is resolved
+# module "newrelic" {
+#   # once we go to terraform 0.13.x, we will be able to do this
+#   # count = var.newrelic_alerts_enabled
+#   source = "../modules/newrelic/"
 
-  enabled     = var.newrelic_alerts_enabled
-  region      = var.region
-  env_name    = var.env_name
-  events_in_last_ten_minutes_threshold = var.events_in_last_ten_minutes_threshold
-}
+#   enabled     = var.newrelic_alerts_enabled
+#   region      = var.region
+#   env_name    = var.env_name
+#   events_in_last_ten_minutes_threshold = var.events_in_last_ten_minutes_threshold
+# }
