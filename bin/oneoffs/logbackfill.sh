2d661258fad7843c6aded764b6c3587dea1c0bf2
Paul Hirsch <paul.hirsch@gsa.gov>
2020-07-07 15:00:02 -0500

    Allow specifying an alternate source bucket and prefix in logbackfill

--- bin/oneoffs/logbackfill.sh
+++ bin/oneoffs/logbackfill.sh
@@ -16,8 +16,10 @@ usage() {
 	echo "examples:  $0 imported-2020-06-19-90days 90"
 	echo "           $0 -d"
 	echo
-	echo "this script must be run on an elk system that has a working cloudtrail logstash"
-	echo "going so that it can copy it's config."
+	echo "This script must be run on an elk system that has a working cloudtrail logstash"
+  echo "going so that it can copy it's config.  To use an alternate bucket to restore"
+  echo "from, set the BUCKET_NAME environment variable.  To use an alternate subfolder in the"
+  echo "bucket (instead of elk/), set the BUCKET_PREFIX environment variable."
 	exit 1
 }
 
@@ -62,12 +64,20 @@ output {
 }
 EOF
 
-BUCKET=$(grep 'bucket => "' /etc/logstash/cloudtraillogstashconf.d/30-s3output.conf | sed 's/.*"\(.*\)"/\1/')
+if [ -z "${BUCKET_NAME}" ]; then
+       BUCKET_NAME=$(grep 'bucket => "' /etc/logstash/cloudtraillogstashconf.d/30-s3output.conf | sed 's/.*"\(.*\)"/\1/')
+fi
+
+if [ -z "${BUCKET_PREFIX}" ]; then
+       BUCKET_PREFIX='elk'
+fi
+
 cat <<EOF >  /etc/logstash/backfilllogstashconf.d/30-backfillin.conf
 input {
    s3 {
      region => "us-west-2"
-     bucket => "$BUCKET"
+     bucket => "${BUCKET_NAME}"
+     prefix => "${BUCKET_PREFIX}"
      proxy_uri => "http://obproxy.login.gov.internal:3128"
      codec => "json_lines"
      sincedb_path => "/usr/share/logstash/.sincedb_backfilllogstash"
