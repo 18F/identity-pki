48824e010e46a15c870500a853533f62007d2ddf
timothy-spencer <timothy.spencer@gsa.gov>
2020-07-09 11:27:15 -0700

    remove debug stuff

--- kitchen/cookbooks/identity-elk/recipes/default.rb
+++ kitchen/cookbooks/identity-elk/recipes/default.rb
@@ -240,8 +240,6 @@ ruby_block 'find_cloudtrail_startfrom' do
       startfrom = Time.now.strftime('%F 00:00:00 +0000')
     end
     ENV['CLOUDTRAIL_SINCEDBDATE'] = startfrom
-    # XXX for debugging
-    File.write("/var/tmp/.ctsincedb", startfrom)
   end
 end
 
