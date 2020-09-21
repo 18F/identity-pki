cc349ddcce5ecad8e76fffd2170707b75074faab
timothy-spencer <timothy.spencer@gsa.gov>
2020-09-21 14:45:26 -0700

    added query for mhenke to work on

--- terraform/modules/newrelic/idp.tf
+++ terraform/modules/newrelic/idp.tf
@@ -217,6 +217,31 @@ resource "newrelic_nrql_alert_condition" "enduser_response_time" {
   }
 }
 
+# # alert created by mhenke, commented out until he refines the query a bit more and makes a runbook
+# resource "newrelic_nrql_alert_condition" "proofing_flow_errors" {
+#   count = var.enabled
+#   policy_id = newrelic_alert_policy.high[0].id
+#   name        = "${var.env_name}: high rate of errors in proofing flow"
+#   enabled     = true
+#   description = "Alerting when errors in proofing flow get above 5% in the past 5 minutes"
+#   value_function = "single_value"
+#   runbook_url = "XXX"
+#   violation_time_limit = "TWELVE_HOURS"
+
+#   nrql {
+#     query = "SELECT percentage(count(*), WHERE error is true and name LIKE 'Controller/idv/%' and appName = '${var.env_name}.${var.root_domain}') 
+# FROM Transaction WHERE name LIKE 'Controller/idv/%' and appName = '${var.env_name}.${var.root_domain}' FACET name"
+#     evaluation_offset = 3
+#   }
+
+#   critical {
+#     operator              = "above"
+#     threshold             = 5
+#     threshold_duration    = 300
+#     threshold_occurrences = "at_least_once"
+#   }
+# }
+
 resource "newrelic_alert_condition" "enduser_error_percentage" {
   count = var.enduser_enabled
   policy_id = newrelic_alert_policy.enduser[0].id
