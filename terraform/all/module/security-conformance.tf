f588050231daeef2ff8216cd2daa8c81d0b2c71d
Brian Crissup <brian@BigMac.local>
2020-10-08 10:06:46 -0400

    Add module for config conformance packs Starting with FedRamp

---
+++ terraform/all/module/security-conformance.tf
@@ -0,0 +1,4 @@
+module "config_fedramp_conformance" {
+  source = "github.com/18F/identity-terraform//config_fedramp_conformance?ref=a24a22ef5640137062047d95ec565a1f4e3c7b9e"
+  #source = "../../../../identity-terraform/config_fedramp_conformance"
+}
