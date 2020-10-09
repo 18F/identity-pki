21845c6bfdbc873d4327113947543fa51284ab05
Brian Crissup <brian@BigMac.local>
2020-10-09 09:30:21 -0400

    Add route53 query logging to vpc

--- terraform/app/cloudwatch-log.tf
+++ terraform/app/cloudwatch-log.tf
@@ -13,3 +13,12 @@ resource "aws_cloudwatch_log_group" "squid_access_log" {
     environment = var.env_name
   }
 }
+
+resource "aws_cloudwatch_log_group" "dns_query_log" {
+  name = "${var.env_name}/dns/query" 
+  retention_in_days = 365
+
+  tags = {
+    environment = var.env_name
+  }
+}
