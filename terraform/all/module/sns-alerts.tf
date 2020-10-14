0ac043b7570172261fba0590607a7c07ad249072
Brian Crissup <brian@BigMac.local>
2020-10-14 11:41:19 -0400

    Update gitref from merged pr in identity-terraform

--- terraform/all/module/sns-alerts.tf
+++ terraform/all/module/sns-alerts.tf
@@ -17,7 +17,7 @@ resource "aws_sns_topic" "slack_events" {
 }
 
 module "slack_login_events" {
-  source = "github.com/18F/identity-terraform//slack_lambda?ref=12a4096cbb2fc5adb600d0312389f500cb6d434b"
+  source = "github.com/18F/identity-terraform//slack_lambda?ref=0bf0813cc5a9fc6447336322288ed3118bad8c91"
   #source = "../../../../identity-terraform/slack_lambda"
   
   lambda_name        = "snstoslack_login_events"
@@ -34,7 +34,7 @@ resource "aws_sns_topic" "slack_otherevents" {
 }
 
 module "slack_login_otherevents" {
-  source = "github.com/18F/identity-terraform//slack_lambda?ref=12a4096cbb2fc5adb600d0312389f500cb6d434b"
+  source = "github.com/18F/identity-terraform//slack_lambda?ref=0bf0813cc5a9fc6447336322288ed3118bad8c91"
   #source = "../../../../identity-terraform/slack_lambda"
   
   lambda_name        = "snstoslack_login_otherevents"
@@ -51,7 +51,7 @@ resource "aws_sns_topic" "slack_soc" {
 }
 
 module "slack_login_soc" {
-  source = "github.com/18F/identity-terraform//slack_lambda?ref=12a4096cbb2fc5adb600d0312389f500cb6d434b"
+  source = "github.com/18F/identity-terraform//slack_lambda?ref=0bf0813cc5a9fc6447336322288ed3118bad8c91"
   #source = "../../../../identity-terraform/slack_lambda"
   
   lambda_name        = "snstoslack_login_soc"
@@ -84,7 +84,7 @@ module "slack_login_events_use1" {
   providers = {
     aws = aws.use1
   }
-  source = "github.com/18F/identity-terraform//slack_lambda?ref=12a4096cbb2fc5adb600d0312389f500cb6d434b"
+  source = "github.com/18F/identity-terraform//slack_lambda?ref=0bf0813cc5a9fc6447336322288ed3118bad8c91"
   #source = "../../../../identity-terraform/slack_lambda"
   
   lambda_name        = "snstoslack_login_events"
@@ -105,7 +105,7 @@ module "slack_login_otherevents_use1" {
   providers = {
     aws = aws.use1
   }
-  source = "github.com/18F/identity-terraform//slack_lambda?ref=12a4096cbb2fc5adb600d0312389f500cb6d434b"
+  source = "github.com/18F/identity-terraform//slack_lambda?ref=0bf0813cc5a9fc6447336322288ed3118bad8c91"
   #source = "../../../../identity-terraform/slack_lambda"
   
   lambda_name        = "snstoslack_login_otherevents"
@@ -126,7 +126,7 @@ module "slack_login_soc_use1" {
   providers = {
     aws = aws.use1
   }
-  source = "github.com/18F/identity-terraform//slack_lambda?ref=12a4096cbb2fc5adb600d0312389f500cb6d434b"
+  source = "github.com/18F/identity-terraform//slack_lambda?ref=0bf0813cc5a9fc6447336322288ed3118bad8c91"
   #source = "../../../../identity-terraform/slack_lambda"
   
   lambda_name        = "snstoslack_login_soc"
