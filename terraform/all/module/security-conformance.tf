e1e07e1e5945c99b73add4777e924563e16098f0
Brian Crissup <brian@BigMac.local>
2020-10-20 09:48:13 -0400

    Update gitref

--- terraform/all/module/security-conformance.tf
+++ terraform/all/module/security-conformance.tf
@@ -1,4 +1,4 @@
 module "config_fedramp_conformance" {
-  source = "github.com/18F/identity-terraform//config_fedramp_conformance?ref=2278bd68ec436b82814ea67a808c3f2a55dea253"
+  source = "github.com/18F/identity-terraform//config_fedramp_conformance?ref=fe39d2b567f8193e161bd59a9d52a111d2102ae2"
   #source = "../../../../identity-terraform/config_fedramp_conformance"
 }
