299bac27041b0febf676985c07b87cc1f7ad7a30
Brian Crissup <brian@BigMac.local>
2020-10-21 08:56:51 -0400

    Add lambda vpc endpoint Remove lambda endpoint from proxy allow list

--- terraform/app/vpc_endpoints.tf
+++ terraform/app/vpc_endpoints.tf
@@ -255,6 +255,35 @@ resource "aws_security_group" "sns_endpoint" {
   vpc_id = aws_vpc.default.id
 }
 
+resource "aws_security_group" "lambda_endpoint" {
+  description = "Allow inbound from idp servers"
+
+  # allow outbound to the VPC
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    security_groups = [
+      aws_security_group.idp.id,
+    ]
+  }
+
+  name = "${var.name}-lambda_endpoint-${var.env_name}"
+
+  tags = {
+    Name = "${var.name}-lambda_endpoint-${var.env_name}"
+  }
+
+  vpc_id = aws_vpc.default.id
+}
+
 resource "aws_vpc_endpoint" "kms" {
   vpc_id            = aws_vpc.default.id
   service_name      = "com.amazonaws.${var.region}.kms"
@@ -406,6 +435,24 @@ resource "aws_vpc_endpoint" "sns" {
 
   security_group_ids = [
     aws_security_group.sns_endpoint.id,
+  ]
+
+  subnet_ids = [
+    aws_subnet.privatesubnet1.id,
+    aws_subnet.privatesubnet2.id,
+    aws_subnet.privatesubnet3.id,
+  ]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "lambda" {
+  vpc_id            = aws_vpc.default.id
+  service_name      = "com.amazonaws.${var.region}.lambda"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.lambda_endpoint.id,
   ]
 
   subnet_ids = [
