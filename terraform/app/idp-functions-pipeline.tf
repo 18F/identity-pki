a907a48b6d5a81d0f83056b5031a227d559277da
Brian Crissup <brian@BigMac.local>
2020-10-27 10:33:46 -0400

    Changes for prod deployment of lambda functions

--- terraform/app/idp-functions-pipeline.tf
+++ terraform/app/idp-functions-pipeline.tf
@@ -1,13 +1,15 @@
 module "idp-lambda-functions" {
-  source = "github.com/18F/identity-terraform//lambda_pipeline?ref=bdd8ad5ff0ea0fcb4be1b8aab7cc206e0dfdded2"
+  source = "github.com/18F/identity-terraform//lambda_pipeline?ref=1604e4f20ebb71e17737685b83b0028ec4504782"
+  #source = "../../../identity-terraform/lambda_pipeline"
 
   region = var.region
   env = var.env_name
   cf_stack_name = "idp-functions"
   project_name = "idp-functions"
   project_description = "Deploy idp functions"
-  project_source_s3_bucket = "login-gov.lambda-functions.894947205914-us-west-2"
-  project_source_object_key = "circleci/identity-idp-functions/master/identity-idp-functions.zip"
+  project_template_s3_bucket = "login-gov.lambda-functions.${data.aws_caller_identity.current.account_id}-us-west-2"
+  project_template_object_key = "circleci/identity-idp-functions/master/identity-idp-functions.zip"
+  project_artifacts_s3_bucket = "login-gov.lambda-functions.894947205914-us-west-2"
   parameter_application_functions = "${var.env_name}/idp/lambda/application-functions"
   vpc_arn = aws_vpc.default.arn
 }
