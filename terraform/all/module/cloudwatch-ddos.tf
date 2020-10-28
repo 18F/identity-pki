39bd0ccf6acb42993d14afd5aa5c3c6181b70a84
bleachbyte <jonathan.pirro@gsa.gov>
2020-10-28 11:41:49 -0500

    moving ddos module to all/

--- terraform/all/module/cloudwatch-ddos.tf
+++ terraform/all/module/cloudwatch-ddos.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_metric_alarm" "ddos_alert" {
-  alarm_name                = "${var.env_name} DDoS Alert"
+  alarm_name                = "DDoS Alert"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "1"
   metric_name               = "MetricName"
@@ -7,8 +7,11 @@ resource "aws_cloudwatch_metric_alarm" "ddos_alert" {
   period                    = "300"
   statistic                 = "Minimum"
   threshold                 = "1"
-  alarm_description         = "DDoS attack detected in ${var.env_name}"
+  alarm_description         = "This Alarm is executed when a DDoS attack is detected"
   insufficient_data_actions = []
-  alarm_actions             = [var.slack_events_sns_hook_arn]
+  alarm_actions             = [
+    aws_sns_topic.slack_events.arn,
+    aws_sns_topic.opsgenie_alert.arn
+  ]
 }
 
