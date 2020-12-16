ba8900b0fa65099a9beea646592a7b2d9adbcc8f
Zach Margolis <zachary.margolis@gsa.gov>
2020-12-16 10:35:54 -0800

    reorganize tests to increase coverage

--- spec/controllers/health/certs_controller_spec.rb
+++ spec/controllers/health/certs_controller_spec.rb
@@ -6,54 +6,63 @@ RSpec.describe Health::CertsController do
     let(:deadline) { nil }
     let(:health_checker) { HealthChecker.new(certificates_store: certificates_store) }
 
-    before do
-      allow(controller).to receive(:health_checker).and_return(health_checker)
+    it 'renders a status as JSON' do
+      action
+
+      expect(response.content_type).to eq('application/json')
+      expect(JSON.parse(response.body, symbolize_names: true)).to include(:healthy)
     end
 
-    let(:expiring_cert) do
-      instance_double(
-        'Certificate',
-        expired?: true,
-        not_after: 15.days.from_now,
-        subject: OpenSSL::X509::Name.new([%w[CN cert1], %w[OU example]]),
-        issuer: OpenSSL::X509::Name.new([%w[CN issuer1], %w[OU example]]),
-        key_id: 'ab:cd:ef:gh:jk'
-      )
-    end
-
-    context 'with expiring certs' do
-      let(:certificates_store) { [expiring_cert] }
-
-      it 'returns a 503' do
-        action
-
-        expect(response.status).to eq(503)
-
-        body = JSON.parse(response.body, symbolize_names: true)
-        expect(body).to include(healthy: false)
-        expect(body[:info][:expiring].first[:key_id]).to eq(expiring_cert.key_id)
-      end
-    end
-
-    context 'with no expiring certs' do
-      let(:certificates_store) { [] }
-
-      it 'returns a 200' do
-        action
-
-        expect(response.status).to eq(200)
-        expect(JSON.parse(response.body, symbolize_names: true)).to include(healthy: true)
+    context 'certs health' do
+      before do
+        allow(controller).to receive(:health_checker).and_return(health_checker)
       end
 
-      context 'with a deadline param' do
-        let(:deadline) { '2020-01-01' }
+      let(:expiring_cert) do
+        instance_double(
+          'Certificate',
+          expired?: true,
+          not_after: 15.days.from_now,
+          subject: OpenSSL::X509::Name.new([%w[CN cert1], %w[OU example]]),
+          issuer: OpenSSL::X509::Name.new([%w[CN issuer1], %w[OU example]]),
+          key_id: 'ab:cd:ef:gh:jk'
+        )
+      end
 
-        it 'checks certs with that deadline' do
-          expect(health_checker).to receive(:check_certs).
-            with(deadline: Time.zone.parse(deadline)).
-            and_call_original
+      context 'with expiring certs' do
+        let(:certificates_store) { [expiring_cert] }
 
+        it 'returns a 503' do
           action
+
+          expect(response.status).to eq(503)
+
+          body = JSON.parse(response.body, symbolize_names: true)
+          expect(body).to include(healthy: false)
+          expect(body[:info][:expiring].first[:key_id]).to eq(expiring_cert.key_id)
+        end
+      end
+
+      context 'with no expiring certs' do
+        let(:certificates_store) { [] }
+
+        it 'returns a 200' do
+          action
+
+          expect(response.status).to eq(200)
+          expect(JSON.parse(response.body, symbolize_names: true)).to include(healthy: true)
+        end
+
+        context 'with a deadline param' do
+          let(:deadline) { '2020-01-01' }
+
+          it 'checks certs with that deadline' do
+            expect(health_checker).to receive(:check_certs).
+              with(deadline: Time.zone.parse(deadline)).
+              and_call_original
+
+            action
+          end
         end
       end
     end
