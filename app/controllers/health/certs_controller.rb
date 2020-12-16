d76683081803ab423cd8ff44f94b2a323ba08be2
Zach Margolis <zachmargolis@users.noreply.github.com>
2020-12-16 10:41:29 -0800

    Add certs health check endpoint (LG-3929) (#181)
    * Run rake certs:remove_invalid

---
+++ app/controllers/health/certs_controller.rb
@@ -0,0 +1,20 @@
+module Health
+  class CertsController < ApplicationController
+    newrelic_ignore_apdex
+
+    def index
+      deadline = params[:deadline].present? ? Time.zone.parse(params[:deadline]) : 30.days.from_now
+
+      result = health_checker.check_certs(deadline: deadline)
+
+      render json: result.as_json,
+             status: result.healthy? ? :ok : :service_unavailable
+    end
+
+    private
+
+    def health_checker
+      @health_checker ||= HealthChecker.new
+    end
+  end
+end
