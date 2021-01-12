a0a6675e27bb573e9a362782e12246c3aee17919
Zach Margolis <zachary.margolis@gsa.gov>
2021-01-12 15:25:41 -0800

    update debug

--- app/services/certificate_chain_service.rb
+++ app/services/certificate_chain_service.rb
@@ -6,11 +6,12 @@ class CertificateChainService
     process_unknown_certs(cert.signing_key_id, cert.ca_issuer_http_url)
   end
 
+  # Gets the chain of Certificates and also prints them out
   # @param [Certificate]
+  # @return [Array<Certificate>]
   def debug(cert)
     process_unknown_certs(cert.signing_key_id, cert.ca_issuer_http_url).
       each_with_index { |cert, step| print_cert(cert.x509_cert, step) }
-    nil
   end
 
   # Finds the missing certs in the chain and writes them to the config/certs repo
