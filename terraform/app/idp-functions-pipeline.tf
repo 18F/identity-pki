26f68ddf8ebb9e2db39b7a3fb1d469748b959ed7
Brian Crissup <crissupb@amazon.com>
2021-01-26 10:37:02 -0500

    Update to main branch

--- terraform/app/idp-functions-pipeline.tf
+++ terraform/app/idp-functions-pipeline.tf
@@ -8,7 +8,7 @@ module "idp-lambda-functions" {
   project_name = "idp-functions"
   project_description = "Deploy idp functions"
   project_template_s3_bucket = "login-gov.lambda-functions.${data.aws_caller_identity.current.account_id}-us-west-2"
-  project_template_object_key = "circleci/identity-idp-functions/master/identity-idp-functions.zip"
+  project_template_object_key = "circleci/identity-idp-functions/main/identity-idp-functions.zip"
   project_artifacts_s3_bucket = "login-gov.lambda-functions.894947205914-us-west-2"
   parameter_application_functions = "${var.env_name}/idp/lambda/application-functions"
   vpc_arn = aws_vpc.default.arn
