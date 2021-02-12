18cf2c637edf15470f37fd876762e889b5c8288e
timothy-spencer <timothy.spencer@gsa.gov>
2021-02-12 09:53:08 -0800

    add in tooling/tooling, forgot auto-apply

--- terraform/tooling/tooling/main.tf
+++ terraform/tooling/tooling/main.tf
@@ -16,17 +16,38 @@ module "main" {
   region = "us-west-2"
 }
 
-# deploy the master/global target to the login-master account
-module "masterglobal" {
+# # deploy the master/global target to the login-master account
+# module "masterglobal" {
+#   region = "us-west-2"
+#   source = "../module-pipeline"
+
+#   # This is the dir under the terraform dir to tf in identity-devops
+#   tf_dir = "master/global"
+#   # This is the gitref to check out in identity-devops
+#   gitref = "master"
+#   # This is the account to deploy tf_dir into
+#   account = "login-master"
+
+#   # pass in global config using module composition (https://www.terraform.io/docs/modules/composition.html)
+#   auto_tf_vpc_id = module.main.auto_tf_vpc_id
+#   auto_tf_subnet_id = module.main.auto_tf_subnet_id
+#   auto_tf_role_arn = module.main.auto_tf_role_arn
+#   auto_tf_sg_id = module.main.auto_tf_sg_id
+#   auto_tf_bucket_id = module.main.auto_tf_bucket_id
+#   auto_tf_pipeline_role_arn = module.main.auto_tf_pipeline_role_arn
+# }
+
+# deploy the all/tooling target to the tooling account on the tspencer/notbrickhouseautomation branch
+module "alltooling" {
   region = "us-west-2"
   source = "../module-pipeline"
 
   # This is the dir under the terraform dir to tf in identity-devops
-  tf_dir = "master/global"
+  tf_dir = "all/tooling"
   # This is the gitref to check out in identity-devops
-  gitref = "master"
+  gitref = "tspencer/notbrickhouseautomation"
   # This is the account to deploy tf_dir into
-  account = "login-master"
+  account = "login-tooling"
 
   # pass in global config using module composition (https://www.terraform.io/docs/modules/composition.html)
   auto_tf_vpc_id = module.main.auto_tf_vpc_id
@@ -37,13 +58,14 @@ module "masterglobal" {
   auto_tf_pipeline_role_arn = module.main.auto_tf_pipeline_role_arn
 }
 
-# XXX temporary module to deploy the all/tooling target to the tooling account on the tspencer/notbrickhouseautomation branch
-module "alltooling" {
+# deploy the tooling/tooling target to the tooling account on the tspencer/notbrickhouseautomation branch
+# XXX not sure, but this might not work super well if a pipeline is reconfigured while running.
+module "toolingtooling" {
   region = "us-west-2"
   source = "../module-pipeline"
 
   # This is the dir under the terraform dir to tf in identity-devops
-  tf_dir = "all/tooling"
+  tf_dir = "tooling/tooling"
   # This is the gitref to check out in identity-devops
   gitref = "tspencer/notbrickhouseautomation"
   # This is the account to deploy tf_dir into
