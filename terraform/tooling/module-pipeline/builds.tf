3f905474ba73c12ec81109e293e9295a1e25a07d
timothy-spencer <timothy.spencer@gsa.gov>
2021-02-18 11:08:20 -0800

    getting the right execution id

--- terraform/tooling/module-pipeline/builds.tf
+++ terraform/tooling/module-pipeline/builds.tf
@@ -74,8 +74,8 @@ phases:
           export AWS_ACCESS_KEY_ID=$ORIG_AWS_ACCESS_KEY_ID
           export AWS_SECRET_ACCESS_KEY=$ORIG_AWS_SECRET_ACCESS_KEY
           export AWS_SESSION_TOKEN=$ORIG_AWS_SESSION_TOKEN
-          env | cat -n
-          EXE_ID=$(echo $CODEBUILD_BUILD_ID | awk -F: '{print $2}')
+          EXE_ID=$(aws codepipeline get-pipeline-state --region ${var.region} --name $CODEBUILD_INITIATOR --query 'stageStates[?actionStates[?latestExecution.externalExecutionId==`'$CODEBUILD_BUILD_ID'`]].latestExecution.pipelineExecutionId' --output text
+)
           aws codepipeline stop-pipeline-execution --pipeline-name auto_terraform_${local.clean_tf_dir}_plan --pipeline-execution-id "$EXE_ID" --abandon --reason "no changes"
         elif [ "$EXITCODE" -eq "1" ] ; then
           echo "================================ Error: fail the build"
