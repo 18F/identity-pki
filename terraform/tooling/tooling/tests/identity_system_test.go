d659c998c661b8ce92b65d717fab6fec420545f9
timothy-spencer <timothy.spencer@gsa.gov>
2021-02-19 14:48:45 -0800

    refactor tests to be DRY

--- terraform/tooling/tooling/tests/identity_system_test.go
+++ terraform/tooling/tooling/tests/identity_system_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"os"
 	"testing"
+	"time"
 
 	aws_sdk "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/codepipeline"
@@ -23,21 +24,43 @@ func TestToolingtoolingPipelineExists(t *testing.T) {
 	require.NoError(t, err)
 }
 
-func TestToolingtoolingPipelineCanGetSource(t *testing.T) {
+func FindStatusForPipeline(t *testing.T, pipeline string, stage string) string {
 	svc := codepipeline.New(aws_session)
 
 	input := &codepipeline.GetPipelineStateInput{
-		Name: aws_sdk.String("auto_terraform_tooling_tooling_pipeline"),
+		Name: aws_sdk.String(pipeline),
 	}
-	pstate, err := svc.GetPipelineState(input)
-	require.NoError(t, err)
-
 	sourceStageStatus := []string{}
-	for _, i := range pstate.StageStates {
-		if *i.StageName == "Source" {
-			sourceStageStatus = append(sourceStageStatus, *i.LatestExecution.Status)
+
+	// If the pipeline is running that stage, loop until we get a real answer
+	for {
+		pstate, err := svc.GetPipelineState(input)
+		require.NoError(t, err)
+
+		sourceStageStatus = []string{}
+		for _, i := range pstate.StageStates {
+			if *i.StageName == stage {
+				sourceStageStatus = append(sourceStageStatus, *i.LatestExecution.Status)
+			}
+		}
+		assert.Equal(t, len(sourceStageStatus), 1)
+
+		if sourceStageStatus[0] == "InProgress" {
+			time.Sleep(2)
+		} else {
+			break
 		}
 	}
-	assert.Equal(t, len(sourceStageStatus), 1)
-	assert.Equal(t, sourceStageStatus[0], "Succeeded")
+
+	return (sourceStageStatus[0])
+}
+
+func TestToolingtoolingPipelineCanGetSource(t *testing.T) {
+	status := FindStatusForPipeline(t, "auto_terraform_tooling_tooling_pipeline", "Source")
+	assert.Equal(t, "Succeeded", status)
+}
+
+func TestToolingtoolingPipelineCanPlan(t *testing.T) {
+	status := FindStatusForPipeline(t, "auto_terraform_tooling_tooling_pipeline", "Plan")
+	assert.Equal(t, "Succeeded", status)
 }
