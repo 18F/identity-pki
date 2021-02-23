3bad8133da7cbc752f51fbbbb5958b4b2548cb08
timothy-spencer <timothy.spencer@gsa.gov>
2021-02-23 14:48:16 -0800

    try to deploy tspencer env

--- terraform/tooling/tooling/main.tf
+++ terraform/tooling/tooling/main.tf
@@ -81,25 +81,25 @@ module "toolingtooling" {
   auto_tf_pipeline_role_arn = module.main.auto_tf_pipeline_role_arn
 }
 
-# # deploy the tspencer environment to the sandbox account on the tspencer/notbrickhouseautomation branch
-# module "toolingtooling" {
-#   region = "us-west-2"
-#   source = "../module-pipeline"
+# deploy the tspencer environment to the sandbox account on the tspencer/notbrickhouseautomation branch
+module "toolingtooling" {
+  region = "us-west-2"
+  source = "../module-pipeline"
 
-#   # This is the dir under the terraform dir to tf in identity-devops
-#   tf_dir = "app"
-#   # This is the environment to deploy to
-#   env_name = "tspencer"
-#   # This is the gitref to check out in identity-devops
-#   gitref = "tspencer/notbrickhouseautomation"
-#   # This is the account to deploy tf_dir into
-#   account = "894947205914"
+  # This is the dir under the terraform dir to tf in identity-devops
+  tf_dir = "app"
+  # This is the environment to deploy to
+  env_name = "tspencer"
+  # This is the gitref to check out in identity-devops
+  gitref = "tspencer/notbrickhouseautomation"
+  # This is the account to deploy tf_dir into
+  account = "894947205914"
 
-#   # pass in global config using module composition (https://www.terraform.io/docs/modules/composition.html)
-#   auto_tf_vpc_id = module.main.auto_tf_vpc_id
-#   auto_tf_subnet_id = module.main.auto_tf_subnet_id
-#   auto_tf_role_arn = module.main.auto_tf_role_arn
-#   auto_tf_sg_id = module.main.auto_tf_sg_id
-#   auto_tf_bucket_id = module.main.auto_tf_bucket_id
-#   auto_tf_pipeline_role_arn = module.main.auto_tf_pipeline_role_arn
-# }
+  # pass in global config using module composition (https://www.terraform.io/docs/modules/composition.html)
+  auto_tf_vpc_id = module.main.auto_tf_vpc_id
+  auto_tf_subnet_id = module.main.auto_tf_subnet_id
+  auto_tf_role_arn = module.main.auto_tf_role_arn
+  auto_tf_sg_id = module.main.auto_tf_sg_id
+  auto_tf_bucket_id = module.main.auto_tf_bucket_id
+  auto_tf_pipeline_role_arn = module.main.auto_tf_pipeline_role_arn
+}
