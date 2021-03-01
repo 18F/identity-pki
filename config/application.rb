e826e65f37ad6e3a315efb5fd9e3feb9b68fff51
Zach Margolis <zachary.margolis@gsa.gov>
2021-03-01 10:07:46 -0800

    Expand out rails/all

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,16 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'action_mailer/railtie'
+require 'active_job/railtie'
+require 'action_cable/engine'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
 require 'identity/logging/railtie'
 
 # Require the gems listed in Gemfile, including any gems
