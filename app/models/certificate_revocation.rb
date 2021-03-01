1de52086cb96d9891239dc1b2fea68b0757106a6
Zach Margolis <zachary.margolis@gsa.gov>
2021-03-01 11:45:52 -0800

    Revert "Revert Rails 6.1 (#211)"
    This reverts commit e5fba3e034999a4097c5f8100d375e4c58d1b02a.

--- app/models/certificate_revocation.rb
+++ app/models/certificate_revocation.rb
@@ -1,6 +1,6 @@
 class CertificateRevocation < ApplicationRecord
-  validates :serial, presence: true, case_sensitive: false,
-                     uniqueness: { scope: :certificate_authority_id },
+  validates :serial, presence: true,
+                     uniqueness: { case_sensitive: false, scope: :certificate_authority_id },
                      format: { with: /\A\d+\Z/, message: 'must be a positive integer' }
   validates :certificate_authority, presence: true
 
