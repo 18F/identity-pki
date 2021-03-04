9d718b08ab5aad01fda66253ae326c713ce481cf
timothy-spencer <timothy.spencer@gsa.gov>
2021-03-03 16:46:30 -0800

    add encryption to artifacts bucket

--- terraform/tooling/module/codebuild.tf
+++ terraform/tooling/module/codebuild.tf
@@ -110,6 +110,14 @@ POLICY
 resource "aws_s3_bucket" "codepipeline_bucket" {
   bucket = "auto-tf-bucket-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
 }
 
 resource "aws_iam_role" "codepipeline_role" {
