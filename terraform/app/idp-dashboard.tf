4fe646f3219ae2e8ee4e23b1309a542879676c64
Mitchell Henke <mitchell.henke@gsa.gov>
2021-03-15 16:55:07 -0500

    add external services dashboard

--- terraform/app/idp-dashboard.tf
+++ terraform/app/idp-dashboard.tf
@@ -3,6 +3,7 @@ locals {
   ialx_metric_namespace = "${var.env_name}/idp-ialx"
   external_service_namespace = "${var.env_name}/idp-external-service"
   dashboard_name        = "${var.env_name}-idp-workload"
+  external_service_dashboard_name = "${var.env_name}-idp-external-service"
 }
 
 variable "idp_events_auth_filters" {
@@ -755,6 +756,75 @@ resource "aws_cloudwatch_dashboard" "idp_workload" {
                         "showUnits": false,
                         "label": "Requests"
                     }
+                }
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_dashboard" "idp_external_service" {
+  dashboard_name = local.external_service_dashboard_name
+  dashboard_body = <<EOF
+{
+    "widgets": [
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 0,
+            "width": 24,
+            "height": 12,
+            "properties": {
+                "metrics": [
+                    [ "mhenke/idp-external-service", "aws-kms-decrypt-response-time", { "label": "KMS Decrypt" } ],
+                    [ ".", "aws-kms-encrypt-response-time", { "label": "KMS Encrypt" } ],
+                    [ ".", "aws-pinpoint-phone-number-validate-response-time", { "label": "Pinpoint Validate Phone" } ],
+                    [ ".", "aws-pinpoint-send-messages-response-time", { "label": "Pinpoint Send SMS" } ],
+                    [ ".", "aws-pinpoint-voice-send-voice-message-response-time", { "label": "Pinpoint Send Voice" } ],
+                    [ ".", "aws-s3-put-object-response-time", { "label": "S3 Put Object" } ],
+                    [ ".", "aws-ses-send-raw-email-response-time", { "label": "SES Send Email" } ],
+                    [ ".", "aws-sts-assume-role-response-time", { "label": "STS Assume Role" } ]
+                ],
+                "view": "timeSeries",
+                "stacked": false,
+                "region": "us-west-2",
+                "period": 300,
+                "stat": "p99",
+                "title": "AWS 99th Percentile Response Times"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 12,
+            "width": 24,
+            "height": 12,
+            "properties": {
+                "metrics": [
+                    [ "mhenke/idp-external-service", "aws-kms-decrypt-response-time", { "label": "KMS Decrypt", "yAxis": "right" } ],
+                    [ ".", "aws-kms-encrypt-response-time", { "label": "KMS Encrypt", "yAxis": "right" } ],
+                    [ ".", "aws-pinpoint-phone-number-validate-response-time", { "label": "Pinpoint Validate Phone" } ],
+                    [ ".", "aws-pinpoint-send-messages-response-time", { "label": "Pinpoint Send SMS" } ],
+                    [ ".", "aws-pinpoint-voice-send-voice-message-response-time", { "label": "Pinpoint Send Voice" } ],
+                    [ ".", "aws-s3-put-object-response-time", { "label": "S3 Put Object" } ],
+                    [ ".", "aws-ses-send-raw-email-response-time", { "label": "SES Send Email" } ],
+                    [ ".", "aws-sts-assume-role-response-time", { "label": "STS Assume Role" } ]
+                ],
+                "view": "timeSeries",
+                "stacked": false,
+                "region": "us-west-2",
+                "period": 60,
+                "stat": "SampleCount",
+                "title": "AWS Requests",
+                "setPeriodToTimeRange": true,
+                "yAxis": {
+                    "left": {
+                        "showUnits": true
+                    }
+                },
+                "legend": {
+                    "position": "bottom"
                 }
             }
         }
