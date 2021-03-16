4716ae1fc2eb1f355f71c2ea6e2a0414021b36f2
timothy-spencer <timothy.spencer@gsa.gov>
2021-03-16 15:01:55 -0700

    add sms/sandbox to auto-tf

--- terraform/tooling/tooling/pipelines.tf
+++ terraform/tooling/tooling/pipelines.tf
@@ -153,6 +153,27 @@ module "allsms-sandbox" {
   auto_tf_pipeline_role_arn = module.main.auto_tf_pipeline_role_arn
 }
 
+# deploy the sms/sandbox target to the sms-sandbox account on the main branch
+module "smssandbox" {
+  region = "us-west-2"
+  source = "../module-pipeline"
+
+  # This is the dir under the terraform dir to tf in identity-devops
+  tf_dir = "sms/sandbox"
+  # This is the gitref to check out in identity-devops
+  gitref = "main"
+  # This is the account to deploy tf_dir into
+  account = "035466892286"
+
+  # pass in global config using module composition (https://www.terraform.io/docs/modules/composition.html)
+  auto_tf_vpc_id            = module.main.auto_tf_vpc_id
+  auto_tf_subnet_id         = module.main.auto_tf_subnet_id
+  auto_tf_role_arn          = module.main.auto_tf_role_arn
+  auto_tf_sg_id             = module.main.auto_tf_sg_id
+  auto_tf_bucket_id         = module.main.auto_tf_bucket_id
+  auto_tf_pipeline_role_arn = module.main.auto_tf_pipeline_role_arn
+}
+
 # deploy the tspencer environment to the sandbox account on the stages/tspencer branch!
 module "tspencer" {
   region = "us-west-2"
