de7478eb52e08448fec73fce7a0ea3c4a6765fb0
timothy-spencer <timothy.spencer@gsa.gov>
2021-04-02 16:56:49 -0700

    give migration instance another tuneable to make sure it has time to run fully

--- terraform/app/recycle.sh
+++ terraform/app/recycle.sh
@@ -11,8 +11,10 @@ fi
 
 ENV_NAME="$1"
 
-# How many minutes to wait for a migration to complete before doing all the recycles
+# How many minutes to give migrations a head start before doing all the recycles
 MIGRATIONDELAY=5
+# How many minutes to wait for a migration to get done and thus be torn down
+MIGRATIONDURATION=10
 
 # healthy percentage required during an instance refresh
 # Chose 50 because in prod, we seem to have our systems running at around 25% CPU at peak
@@ -30,10 +32,10 @@ CURRENTSIZE=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-
 DESIREDSIZE=$(expr $CURRENTSIZE + 1)
 if [ "$(uname -s)" = "Darwin" ] ; then
 	NOW=$(date -v +15S +%Y-%m-%dT%H:%M:%SZ)
-	THEFUTURE=$(date -v +"$MIGRATIONDELAY"M +%Y-%m-%dT%H:%M:%SZ)
+	THEFUTURE=$(date -v +"$MIGRATIONDURATION"M +%Y-%m-%dT%H:%M:%SZ)
 else
 	NOW=$(date -d "15 seconds" +%Y-%m-%dT%H:%M:%SZ)
-	THEFUTURE=$(date -d "$MIGRATIONDELAY minutes" +%Y-%m-%dT%H:%M:%SZ)
+	THEFUTURE=$(date -d "$MIGRATIONDURATION minutes" +%Y-%m-%dT%H:%M:%SZ)
 fi
 echo "============= Scheduling migration host launch and teardown"
 aws autoscaling put-scheduled-update-group-action --scheduled-action-name "migrate-$ENV_NAME" --auto-scaling-group-name "${ENV_NAME}-migration" --start-time "$NOW" --desired-capacity "$DESIREDSIZE"
