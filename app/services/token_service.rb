1b65e95ad7adf2e2d4d1e7faa04e1428778ef70b
Mitchell Henke <mitchell.henke@gsa.gov>
2021-04-14 13:24:04 -0500

    refactor key rotation

--- app/services/token_service.rb
+++ app/services/token_service.rb
@@ -3,12 +3,13 @@ class TokenService
   RANDOM_BYTES = 8
 
   class << self
-    def box(data)
+    def box(data, encryptor = nil)
+      encryptor = message_encryptor if encryptor.nil?
       # The RANDOM_BYTES are there to introduce some entropy since we aren't putting a lot
       # of information into the token. Ruby hashes serialize in the order the keys are
       # introduced, so the data is sandwiched between two random strings, though that is
       # sandwiched in non-random strings -- namely, `{` and `}` to open/close the JSON object.
-      message_encryptor.encrypt_and_sign(
+      encryptor.encrypt_and_sign(
         { r1: SecureRandom.base64(RANDOM_BYTES) }.
         merge(data).
         merge(r2: SecureRandom.base64(RANDOM_BYTES)).to_json,
@@ -78,21 +79,30 @@ class TokenService
     end
 
     def message_encryptor
-      @message_encryptor ||= begin
-        encryptor = ActiveSupport::MessageEncryptor.new(current_key, cipher: 'aes-256-gcm')
-        prior_keys.each { |key| encryptor.rotate key }
-        encryptor
+      return @message_encryptor if @message_encryptor
+
+      encryptor = ActiveSupport::MessageEncryptor.new(current_key, cipher: 'aes-256-gcm')
+      old_key = prior_key
+
+      if old_key
+        encryptor.rotate prior_key
       end
+
+      @message_encryptor = encryptor
     end
 
     def current_key
-      salt, pepper = key_salt_and_pepper
+      salt = IdentityConfig.store.token_encryption_key_salt
+      pepper = IdentityConfig.store.token_encryption_key_pepper
+
       truncate_key(ActiveSupport::KeyGenerator.new(pepper).generate_key(salt))
     end
 
-    def prior_keys
-      prior_key_signifiers.map do |ending|
-        old_salt, old_pepper = key_salt_and_pepper(ending)
+    def prior_key
+      old_salt = IdentityConfig.store.token_encryption_key_salt_old
+      old_pepper = IdentityConfig.store.token_encryption_key_pepper_old
+
+      if old_salt.present? && old_pepper.present?
         truncate_key(ActiveSupport::KeyGenerator.new(old_pepper).generate_key(old_salt))
       end
     end
@@ -102,31 +112,6 @@ class TokenService
     # that we manually truncate to 32 bytes
     def truncate_key(key)
       key[0...32]
-    end
-
-    def key_salt_and_pepper(ordinal = nil)
-      env = IdentityConfig.store
-      if ordinal
-        [env.send(:"token_encryption_key_salt_#{ordinal}!"),
-         env.send(:"token_encryption_key_pepper_#{ordinal}!")]
-      else
-        [env.token_encryption_key_salt,
-         env.token_encryption_key_pepper]
-      end
-    end
-
-    def prior_key_signifiers
-      salt_endings = gather_env_key_endings('token_encryption_key_salt_')
-      pepper_endings = gather_env_key_endings('token_encryption_key_pepper_')
-      (salt_endings & pepper_endings).sort_by(&:to_i)
-    end
-
-    def gather_env_key_endings(prefix)
-      range = prefix.length..-1
-      ENV.
-        keys.
-        select { |name| name.start_with?(prefix) }.
-        map { |name| name[range] }
     end
   end
 end
