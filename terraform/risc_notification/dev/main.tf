b36365f965562319521c5e35410898a32f199667
Brian Crissup <crissupb@amazon.com>
2021-04-19 15:31:33 -0400

    Add dashboard destination for dev environment

--- terraform/risc_notification/dev/main.tf
+++ terraform/risc_notification/dev/main.tf
@@ -13,5 +13,18 @@ terraform {
 module "main" {
   source = "../module"
 
-  env           = "dev"
+  env_name = "dev"
+  
+  risc_notifications = {  
+    dashboard = {
+      partner_name            = "dashboard"
+      notification_url        = "https://dashboard.dev.identitysandbox.gov/api/security_events"
+      notification_rate_limit = 10
+      notification_source     = "urn:gov:gsa:openidconnect.profiles:sp:sso:gsa:dashboard"
+      notification_state      = "ENABLED"
+      basic_auth_user_name    = "na"
+      api_key_name            = "x-api-key"
+      authentication_type     = "API_KEY"
+    }
+  }
 }
