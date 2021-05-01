cf7c56baa9bd8c863eeded46e39f06d7061de26e
Alex Kritikos <alex.kritikos@gsa.gov>
2021-04-30 20:09:59 -0700

    Change auto-converted hcl into real hcl

--- terraform/all/module/macie.tf
+++ terraform/all/module/macie.tf
@@ -2,7 +2,7 @@ resource "aws_kms_key" "awsmacietrail_dataevent" {
   description             = "Macie v2"
   deletion_window_in_days = 7
   enable_key_rotation     = true
-  policy = data.aws_iam_policy_document.awsmacietrail_dataevent.json
+  policy = data.aws_iam_policy_document.kms_awsmacietrail_dataevent.json
 }
 
 resource "aws_kms_alias" "awsmacietrail_dataevent" {
@@ -16,7 +16,7 @@ resource "aws_s3_bucket" "awsmacietrail_dataevent" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-	kms_master_key_id = aws_kms_key.kms_awsmacietrail_dataevent.arn
+	kms_master_key_id = aws_kms_key.awsmacietrail_dataevent.arn
         sse_algorithm = "aws:kms"
       }
     }
@@ -63,60 +63,67 @@ data "aws_iam_policy_document" "s3_awsmacietrail_dataevent" {
   statement {
     sid = "Deny non-HTTPS access"
     effect = "Deny"
-    principal = "*"
-    action = "s3:*"
-    resource = "arn:aws:s3:::917793222841-awsmacietrail-dataevent/*"
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
+    actions = ["s3:*"]
+    resources = ["arn:aws:s3:::917793222841-awsmacietrail-dataevent/*"]
     condition {
       test = "Bool"
       variable = "aws:SecureTransport"
-      value = "false"
+      values = ["false"]
     }
   }
   statement {
     sid = "Deny incorrect encryption header. This is optional"
     effect = "Deny"
-    principal = {
-      service = "macie.amazonaws.com"
+    principals {
+      type = "Service"
+      identifiers = ["macie.amazonaws.com"]
     }
-    action = "s3:PutObject"
-    resource = "arn:aws:s3:::917793222841-awsmacietrail-dataevent/*"
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::917793222841-awsmacietrail-dataevent/*"]
     condition {
       test = "StringNotEquals"
       variable = "s3:x-amz-server-side-encryption-aws-kms-key-id"
-      value = "arn:aws:kms:us-west-2:917793222841:key/82338288-a8af-4a2c-96d5-98df8bed932e"
+      values = ["arn:aws:kms:us-west-2:917793222841:key/82338288-a8af-4a2c-96d5-98df8bed932e"]
     }
   }
   statement {
     sid = "Deny unencrypted object uploads. This is optional"
     effect = "Deny"
-    principal = {
-      service = "macie.amazonaws.com"
+    principals {
+      type = "Service"
+      identifiers = ["macie.amazonaws.com"]
     }
-    action = "s3:PutObject"
-    resource = "arn:aws:s3:::917793222841-awsmacietrail-dataevent/*"
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::917793222841-awsmacietrail-dataevent/*"]
     condition {
       test = "StringNotEquals"
       variable = "s3:x-amz-server-side-encryption"
-      value = "aws:kms"
+      values = ["aws:kms"]
     }
   }
   statement {
     sid = "Allow Macie to upload objects to the bucket"
     effect = "Allow"
-    principal = {
-      service = "macie.amazonaws.com"
+    principals {
+      type = "Service"
+      identifiers = ["macie.amazonaws.com"]
     }
-    action = "s3:PutObject"
-    resource = "arn:aws:s3:::917793222841-awsmacietrail-dataevent/*"
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::917793222841-awsmacietrail-dataevent/*"]
   }
   statement {
     sid = "Allow Macie to use the getBucketLocation operation"
     effect = "Allow"
-    principal = {
-      service = "macie.amazonaws.com"
+    principals {
+      type = "Service"
+      identifiers = ["macie.amazonaws.com"]
     }
-    action = "s3:GetBucketLocation"
-    resource = "arn:aws:s3:::917793222841-awsmacietrail-dataevent"
+    actions = ["s3:GetBucketLocation"]
+    resources = ["arn:aws:s3:::917793222841-awsmacietrail-dataevent"]
   }
   
   statement {
