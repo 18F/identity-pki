882b758c89a18a91cef337103769ded3e9824599
Brian Crissup <crissupb@amazon.com>
2021-05-13 16:26:11 -0400

    Add PT WAF

---
+++ terraform/waf/pt/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region              = "us-west-2"
+  allowed_account_ids = [""] # require login-sandbox
+  profile             = "identitysandbox.gov"
+}
+
+# Stub remote config
+terraform {
+  backend "s3" {
+  }
+}
+
+module "main" {
+  source = "../module"
+
+  env           = "pt"
+  region        = "us-west-2"
+  enforce       = true
+  ip_block_list = ["34.216.215.141", "34.216.215.131", "34.216.215.136"]
+}
