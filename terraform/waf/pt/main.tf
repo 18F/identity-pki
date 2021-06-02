0cb8adc71c9ab1aaf8df5d0cd3b7d149b2964c81
Justin Grevich <justin@grevi.ch>
2021-06-02 12:08:16 -0700

    'til the next wargames ;)

--- terraform/waf/pt/main.tf
+++ terraform/waf/pt/main.tf
@@ -15,6 +15,5 @@ module "main" {
 
   env           = "pt"
   region        = "us-west-2"
-  enforce       = true
-  ip_block_list = ["34.216.215.141/32", "34.216.215.131/32", "34.216.215.136/32", "97.126.2.14/32"]
+  enforce       = false
 }
