9ab3dc3f1e62d516382d99acbcc82f83ed937ab8
Paul Hirsch <paul.hirsch@gsa.gov>
2021-07-15 09:44:06 -0500

    Add LB log bucket to tooling

---
+++ terraform/tooling/module/elb_log_bucket.tf
@@ -0,0 +1,19 @@
+# Create a common bucket for storing ELB/ALB access logs
+# The bucket name will be like this:
+#   login-gov.elb-logs.<ACCOUNT_ID>-<AWS_REGION>
+module "elb-logs" {
+  # can't use variable for ref -- see https://github.com/hashicorp/terraform/issues/17994
+  source = "github.com/18F/identity-terraform//elb_access_logs_bucket?ref=21a2ce16cf1dbf85822c9005d72f8d17cb9dbe4b"
+
+  region                     = var.region
+  bucket_name_prefix         = "login-gov"
+  use_prefix_for_permissions = false
+  inventory_bucket_arn       = "arn:aws:s3:::login-gov.s3-inventory.${data.aws_caller_identity.current.account_id}-${var.region}"
+  lifecycle_days_standard_ia = 60   # 2 months
+  lifecycle_days_glacier     = 365  # 1 year
+  lifecycle_days_expire      = 2190 # 6 years
+}
+
+output "elb_log_bucket" {
+  value = module.elb-logs.bucket_name
+}
