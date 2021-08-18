99b1edde150a90e8f7b25784d3ed13d9d1f94308
timothy-spencer <timothy.spencer@gsa.gov>
2021-08-18 16:04:15 -0500

    is it newrelic related at all?

--- kitchen/cookbooks/identity-monitoring/recipes/default.rb
+++ kitchen/cookbooks/identity-monitoring/recipes/default.rb
@@ -43,6 +43,6 @@ cookbook_file '/etc/systemd/system/newrelic-infra.service' do
 end
 
 execute 'reload_systemd' do
-	command "chown -R newrelic_infra: /tmp/nr-integrations /var/db/newrelic-infra /var/run/newrelic-infra ; echo systemctl reload newrelic-infra ; systemctl restart newrelic-infra"
+	command "chown -R newrelic_infra: /tmp/nr-integrations /var/db/newrelic-infra /var/run/newrelic-infra ; echo systemctl reload newrelic-infra ; echo systemctl restart newrelic-infra"
 	action :nothing
 end
