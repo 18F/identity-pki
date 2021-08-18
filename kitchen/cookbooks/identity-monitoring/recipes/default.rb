4a8f264267fcd01690e07d63152980b7032a8b33
timothy-spencer <timothy.spencer@gsa.gov>
2021-08-18 16:21:23 -0500

    should probably use systemd unit, not service

--- kitchen/cookbooks/identity-monitoring/recipes/default.rb
+++ kitchen/cookbooks/identity-monitoring/recipes/default.rb
@@ -46,5 +46,5 @@ execute 'reload_systemd' do
 	command "chown -R newrelic_infra: /tmp/nr-integrations /var/db/newrelic-infra /var/run/newrelic-infra"
 	action :nothing
   notifies :reload, 'systemd_unit[newrelic-infra]', :delayed
-  notifies :restart, 'service[newrelic-infra]', :delayed
+  notifies :restart, 'systemd_unit[newrelic-infra]', :delayed
 end
