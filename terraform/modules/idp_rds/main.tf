d817d2d7ed7eb218fc62aa70995be2affda01d31
bleachbyte <jonathan.pirro@gsa.gov>
2021-09-02 17:15:47 -0500

    fix for provider aliases with idp_rds module

--- terraform/modules/idp_rds/main.tf
+++ terraform/modules/idp_rds/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.52.0"
+    }
+  }
+  required_version = ">= 0.13.7"
+}
+
 data "aws_caller_identity" "current" {}
 
 locals {
