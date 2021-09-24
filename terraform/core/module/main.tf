74fc4adf4c302cf27dd328fe9e33c422919920b8
Mitchell Henke <mitchell.henke@gsa.gov>
2021-09-24 09:58:42 -0500

    Fix terraform format (#3800)

--- terraform/core/module/main.tf
+++ terraform/core/module/main.tf
@@ -1,6 +1,6 @@
 locals {
-  bucket_name_prefix       = "login-gov"
-  app_secrets_bucket_type  = "app-secrets"
-  cert_secrets_bucket_type = "internal-certs"
+  bucket_name_prefix        = "login-gov"
+  app_secrets_bucket_type   = "app-secrets"
+  cert_secrets_bucket_type  = "internal-certs"
   app_artifacts_bucket_type = "app-artifacts"
 }
