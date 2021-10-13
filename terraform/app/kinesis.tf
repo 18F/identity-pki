e3b1936337976b21a5efc9256bb0bd0225fb2eb5
Vivek Chugh <vivek.chugh@gsa.gov>
2021-10-13 09:34:28 -0400

    fixing terraform fmt issues

--- terraform/app/kinesis.tf
+++ terraform/app/kinesis.tf
@@ -5,7 +5,7 @@ module "kinesis-firehose" {
   kinesis_firehose_stream_backup_prefix = "backup/"
   bucket_name                           = "login-gov-idp-events-${var.env_name}.${data.aws_caller_identity.current.account_id}-${var.region}"
   cloudwatch_subscription_filter_name   = "cw-kinesis-s3-idp-events"
-  cloudwatch_log_group_name             = ["${var.env_name}_/srv/idp/shared/log/events.log","${var.env_name}_/srv/idp/shared/log/workers.log"]
+  cloudwatch_log_group_name             = ["${var.env_name}_/srv/idp/shared/log/events.log", "${var.env_name}_/srv/idp/shared/log/workers.log"]
   cloudwatch_filter_pattern             = " "
 }
 
