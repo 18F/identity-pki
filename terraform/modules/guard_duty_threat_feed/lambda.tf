160062d0bb5107c27cfc452dda6795e29a50c245
Vladimir Toussaint <toussaiv@amazon.com>
2021-10-27 11:13:28 -0400

    GuardDuty Threat Feed - Terraform module

---
+++ terraform/modules/guard_duty_threat_feed/lambda.tf
@@ -0,0 +1,30 @@
+resource "aws_lambda_function" "guard_duty_threat_feed_lambda" {
+  filename      = "${var.guard_duty_threat_feed_code}"
+  function_name = "${var.guard_duty_threat_feed_name}-function"
+  role          = "${aws_iam_role.guard_duty_threat_feed_lambda_role.arn}"
+  description   = "GuardDuty Threat Feed Function"
+  handler       = "lambda_function.lambda_handler"
+
+  source_code_hash = filebase64sha256("${var.guard_duty_threat_feed_code}")
+  memory_size      = "3008"
+  runtime          = "python3.9"
+  timeout          = "300"
+
+  environment {
+    variables = {
+      LOG_LEVEL      = "INFO",
+      DAYS_REQUESTED = "${var.days_requested}",
+      PUBLIC_KEY     = "${aws_ssm_parameter.guard_duty_threat_feed_public_key.arn}",
+      PRIVATE_KEY    = "${aws_ssm_parameter.guard_duty_threat_feed_private_key.arn}",
+      OUTPUT_BUCKET  = "${aws_s3_bucket.guard_duty_threat_feed_s3_bucket.id}"
+    }
+  }
+}
+
+resource "aws_lambda_permission" "guard_duty_threat_feed_lambda_permission" {
+  statement_id  = "${var.guard_duty_threat_feed_name}-lambda-permission"
+  function_name = "${aws_lambda_function.guard_duty_threat_feed_lambda.function_name}"
+  action        = "lambda:InvokeFunction"
+  principal     = "events.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_event_rule.guard_duty_threat_feed_rule.arn}"
+}
---
+++ terraform/modules/guard_duty_threat_feed/variables.tf
@@ -0,0 +1,36 @@
+variable "guard_duty_threat_feed_name" {
+    type = string
+}
+
+variable "account_id" {
+    type = string
+}
+
+variable "aws_region" {
+    type = string
+}
+
+variable "days_requested" {
+    type    = number
+    default = 7
+}
+
+variable "frequency" {
+    type    = number
+    default = 6
+}
+
+variable "guard_duty_threat_feed_public_key" {
+    type        = string
+    description = "Enter the public key value contents (This will be stored in a secured parameter store)"
+}
+
+variable "guard_duty_threat_feed_private_key" {
+    type        = string
+    description = "Enter the private key value contents (This will be stored in a secured parameter store)"
+}
+
+variable "guard_duty_threat_feed_code" {
+    type        = string
+    description = "Enter the path of the compressed lambda source code. e.g: (../guard/src/guard-duty-threat-feed.zip)"
+}
