3dae659fe7fea5a39d2fb0a0ae2d3c6c83dea776
Alex Kritikos <alex.kritikos@gsa.gov>
2021-11-01 12:47:28 -0500

    Refactor sync.go to separate I/O operations and logic, for easier testing (#3925)
    * Don't allow users to create groups
    
    * Create/Delete groups
    
    * Add new group memberships
    
    * Delete unused memberships; Include devopsnonprod
    
    * Fix typo
    
    * Add dry-run support to groups and group memberships
    
    * Refactor for easier testing
    
    Separate funcs used to get data, manipulate data, and make API calls based on data
    
    * Fix typo
    
    * Remove unnecessary Membership type
    
    * Update tests to match refactored sync.go
    
    * Add group tests
    
    * Add membership tests
    
    * Test parsing
    
    * gofmt

--- bin/users/sync.go
+++ bin/users/sync.go
@@ -17,10 +17,12 @@ var fqdn string
 var dryrun bool
 var userYaml string
 var apiToken string // env var
+var check bool
 
 var allowedRoles = map[string]bool{
-	"devops": true,
-	"appdev": true,
+	"devops":        true,
+	"appdev":        true,
+	"devopsnonprod": true,
 }
 
 var ignoredUsers = []string{
@@ -28,19 +30,24 @@ var ignoredUsers = []string{
 	"alert",
 }
 
-type AuthorizedUsers struct {
-	Users map[string][]string
+type AuthUser struct {
+	Aws_groups    []string
+	Gitlab_groups []string
 }
 
-type UserState struct {
-	User   *gitlab.User
-	Enable bool
+type AuthorizedUsers struct {
+	Users map[string]*AuthUser
 }
+
+// A mapping of user and group names to IDs, because our configs work with
+// usernames, but GitLab often wants the ID. Populate as we find more mappings.
+var idCache = map[string]int{}
 
 func init() {
 	flag.StringVar(&fqdn, "fqdn", "", "Required. Fully qualified domain name for the GitLab instance.")
 	flag.StringVar(&userYaml, "file", "../../terraform/master/global/users.yaml", "Input YAML.")
 	flag.BoolVar(&dryrun, "dryrun", false, "Whether to run in dryrun mode. Defaults to false.")
+	flag.BoolVar(&check, "check", false, "Only check whether a change would be made, and error if so.")
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "sync.go: Syncs GitLab users with a YAML source of truth. Requires GITLAB_API_TOKEN to be set in the environment. Usage:\n")
 		flag.PrintDefaults()
@@ -60,7 +67,7 @@ func main() {
 	}
 
 	// Set up GitLab connection
-	gitlabClient, err := gitlab.NewClient(
+	gitc, err := gitlab.NewClient(
 		apiToken,
 		gitlab.WithBaseURL(fmt.Sprintf("https://%s", fqdn)),
 	)
@@ -68,8 +75,12 @@ func main() {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
+	//
+	// Sync Users
+	//
+
 	// Get existing GitLab users
-	existingUsers, err := getExistingUsers(gitlabClient)
+	existingUsers, err := getExistingUsers(gitc)
 	if err != nil {
 		log.Fatalf("Failed to list users: %v", err)
 	}
@@ -79,94 +90,126 @@ func main() {
 		log.Fatalf("Error reading user YAML: %s", err)
 	}
 
-	newUsers := make(map[string]string)
-	markUsers(existingUsers, newUsers, authorizedUsers)
+	usersToBlock, usersToUnblock, usersToCreate := resolveUsers(existingUsers, authorizedUsers)
+	err = blockUsers(gitc, usersToBlock)
+	if err != nil {
+		log.Fatalf("Unable to block users: %v", err)
+	}
+	err = unblockUsers(gitc, usersToUnblock)
+	if err != nil {
+		log.Fatalf("Unable to unblock users: %v", err)
+	}
+	err = createUsers(gitc, usersToCreate)
+	if err != nil {
+		log.Fatalf("Unable to create users: %v", err)
+	}
 
-	// For each user in GitLab, create/keep/disable as marked
-	ignoreMap := make(map[string]bool)
-	for _, v := range ignoredUsers {
-		ignoreMap[fmt.Sprintf("%v@%v", v, fqdn)] = true
+	//
+	// Sync Groups
+	//
+
+	// Get existing groups
+	gitlabGroups, err := getExistingGroups(gitc)
+	if err != nil {
+		log.Fatalf("Could not get GitLab groups: %v", err)
 	}
-	
-	for _, u := range existingUsers {
-		if _, ok := ignoreMap[u.User.Email]; ok {
-			continue
-		}
-		if u.Enable {
-			err = unblockUser(gitlabClient, u.User)
-			if err != nil {
-				log.Fatalf("Could not unblock %v: %v", u.User.Email, err)
+	log.Printf("Found groups: %v", gitlabGroups)
+
+	authGroups := getAuthorizedGroups(authorizedUsers)
+	log.Printf("Authorized groups: %v", authGroups)
+
+	groupsToCreate, groupsToDelete := resolveGroups(gitlabGroups, authGroups)
+	err = createGroups(gitc, groupsToCreate)
+	if err != nil {
+		log.Fatalf("Unable to create groups: %v", err)
+	}
+	err = deleteGroups(gitc, groupsToDelete)
+	if err != nil {
+		log.Fatalf("Unable to delete groups: %v", err)
+	}
+
+	//
+	// Sync Members
+	//
+
+	groupsWithMembers, err := getExistingMembers(gitc)
+	if err != nil {
+		log.Fatalf("Failed to list memberships: %v", err)
+	}
+	authGroups = getAuthorizedGroups(authorizedUsers)
+	membersToCreate, membersToDelete := resolveMembers(groupsWithMembers, authGroups)
+	err = createMemberships(gitc, membersToCreate)
+	if err != nil {
+		log.Fatalf("Failed to create members: %v", err)
+	}
+	err = deleteMemberships(gitc, membersToDelete)
+	if err != nil {
+		log.Fatalf("Failed to delete members: %v", err)
+	}
+}
+
+func getAuthorizedGroups(authUsers *AuthorizedUsers) map[string]map[string]bool {
+	groups := map[string]map[string]bool{}
+	for username, au := range authUsers.Users {
+		for _, g := range au.Gitlab_groups {
+			if _, ok := groups[g]; !ok {
+				groups[g] = make(map[string]bool)
 			}
-		} else if !u.Enable {
-			err = blockUser(gitlabClient, u.User)
-			if err != nil {
-				log.Fatalf("Could not block %v: %v", u.User.Email, err)
-			}
+			groups[g][username] = true
 		}
 	}
-	for email, username := range newUsers {
-		err = createUserFromEmail(gitlabClient, email, username)
+
+	return groups
+}
+
+func getExistingGroups(gitc *gitlab.Client) (map[string]*gitlab.Group, error) {
+	groups, _, err := gitc.Groups.ListGroups(
+		&gitlab.ListGroupsOptions{
+			ListOptions: gitlab.ListOptions{
+				PerPage: 100,
+			},
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	groupMap := make(map[string]*gitlab.Group)
+	for _, g := range groups {
+		groupMap[g.Name] = g
+		idCache[fmt.Sprintf("group:%v", g.Name)] = g.ID
+	}
+
+	return groupMap, nil
+}
+
+func getExistingMembers(gitc *gitlab.Client) (map[string]map[string]bool, error) {
+	memberships := map[string]map[string]bool{}
+
+	gitlabGroups, err := getExistingGroups(gitc)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Found groups: %v", gitlabGroups)
+
+	for gname, g := range gitlabGroups {
+		members, _, err := gitc.Groups.ListGroupMembers(
+			g.ID,
+			&gitlab.ListGroupMembersOptions{
+				ListOptions: gitlab.ListOptions{
+					PerPage: 100,
+				},
+			},
+		)
 		if err != nil {
-			log.Fatalf("Could not create %v: %v", email, err)
+			return nil, err
+		}
+		memberships[gname] = make(map[string]bool)
+		for _, m := range members {
+			memberships[gname][m.Username] = true
 		}
 	}
-}
-
-func createUserFromEmail(gitc *gitlab.Client, email string, username string) error {
-	log.Printf("Create %#v", email)
-	options := &gitlab.CreateUserOptions{
-		Email:               gitlab.String(fmt.Sprintf(email)),
-		ForceRandomPassword: gitlab.Bool(true),
-		Username:            gitlab.String(username),
-		Name:                gitlab.String(username),
-		SkipConfirmation:    gitlab.Bool(true),
-	}
-
-	if dryrun {
-		return nil
-	}
-
-	_, _, err := gitc.Users.CreateUser(options)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func blockUser(gitc *gitlab.Client, user *gitlab.User) error {
-	log.Printf("Block %v (ID %v)", user.Email, user.ID)
-
-	if dryrun {
-		return nil
-	}
-
-	if user.State == "blocked" {
-		return nil
-	}
-
-	err := gitc.Users.BlockUser(user.ID)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func unblockUser(gitc *gitlab.Client, user *gitlab.User) error {
-	log.Printf("Unblock %v (ID %v)", user.Email, user.ID)
-
-	if dryrun {
-		return nil
-	}
-
-	if user.State == "active" {
-		return nil
-	}
-
-	err := gitc.Users.UnblockUser(user.ID)
-	if err != nil {
-		return err
-	}
-	return nil
+	return memberships, nil
 }
 
 func getAuthorizedUsers(f string) (*AuthorizedUsers, error) {
@@ -176,14 +219,14 @@ func getAuthorizedUsers(f string) (*AuthorizedUsers, error) {
 	if err != nil {
 		return nil, err
 	}
-	err = yaml.Unmarshal(userFile, &authorizedUsers)
+	err = yaml.UnmarshalStrict(userFile, &authorizedUsers)
 	if err != nil {
 		return nil, err
 	}
 	return &authorizedUsers, nil
 }
 
-func getExistingUsers(gitc *gitlab.Client) (map[string]*UserState, error) {
+func getExistingUsers(gitc *gitlab.Client) (map[string]*gitlab.User, error) {
 	gitUsers, _, err := gitc.Users.ListUsers(
 		&gitlab.ListUsersOptions{
 			ListOptions: gitlab.ListOptions{
@@ -195,30 +238,254 @@ func getExistingUsers(gitc *gitlab.Client) (map[string]*UserState, error) {
 		return nil, err
 	}
 
-	existingUsers := make(map[string]*UserState)
+	existingUsers := make(map[string]*gitlab.User)
 	for _, u := range gitUsers {
-		existingUsers[u.Email] = &UserState{
-			User:   u,
-			Enable: false,
-		}
+		existingUsers[u.Username] = u
 		log.Printf("Found existing GitLab user: %v (%v)", u.Email, u.State)
+		idCache[fmt.Sprintf("user:%v", u.Username)] = u.ID
 	}
 
 	return existingUsers, nil
 }
 
-func markUsers(existingUsers map[string]*UserState, newUsers map[string]string, authorizedUsers *AuthorizedUsers) {
+// Given sets of existing and authorized users, returns sets of users to block, unblock and create
+func resolveUsers(
+	existingUsers map[string]*gitlab.User,
+	authorizedUsers *AuthorizedUsers,
+) (map[string]*gitlab.User, map[string]*gitlab.User, map[string]bool) {
+
+	usersToBlock := map[string]*gitlab.User{}
+	usersToUnblock := map[string]*gitlab.User{}
+	usersToCreate := map[string]bool{}
+
+	// Copy and clean existingUsers so we don't mangle it unexpectedly
+	ignoreMap := make(map[string]bool)
+	for _, v := range ignoredUsers {
+		ignoreMap[fmt.Sprintf("%v@%v", v, fqdn)] = true
+	}
+	for username, user := range existingUsers {
+		if _, ok := ignoreMap[user.Email]; !ok {
+			usersToBlock[username] = user
+		}
+	}
+
 	// For each user in YAML,  mark to keep it
-	for username, userRoles := range authorizedUsers.Users {
-		for _, role := range userRoles {
-			if _, ok := allowedRoles[role]; ok {
-				email := fmt.Sprintf("%s@gsa.gov", username)
-				if _, ok := existingUsers[email]; ok {
-					existingUsers[email].Enable = true
-				} else {
-					newUsers[email] = username
-				}
+	for username, userAttrs := range authorizedUsers.Users {
+		for _, role := range userAttrs.Aws_groups {
+			if _, ok := allowedRoles[role]; !ok {
+				// skip to the next role
+				continue
+			}
+			if u, ok := existingUsers[username]; ok {
+				usersToUnblock[username] = u
+				delete(usersToBlock, username)
+			} else {
+				usersToCreate[username] = true
 			}
 		}
 	}
+
+	return usersToBlock, usersToUnblock, usersToCreate
+}
+
+// Returns sets of groups to create and delete
+func resolveGroups(
+	gitlabGroups map[string]*gitlab.Group,
+	authGroups map[string]map[string]bool,
+) (map[string]bool, map[string]*gitlab.Group) {
+
+	groupsToCreate := map[string]bool{}
+	groupsToDelete := map[string]*gitlab.Group{}
+
+	// Copy input so we don't confuse callers
+	for k, v := range gitlabGroups {
+		groupsToDelete[k] = v
+	}
+
+	for ag, _ := range authGroups {
+		if _, ok := gitlabGroups[ag]; ok {
+			delete(groupsToDelete, ag)
+			continue
+		}
+		groupsToCreate[ag] = true
+	}
+
+	return groupsToCreate, groupsToDelete
+}
+
+func resolveMembers(
+	memberships map[string]map[string]bool,
+	authGroups map[string]map[string]bool,
+) (map[string]map[string]bool, map[string]map[string]bool) {
+
+	membersToCreate := map[string]map[string]bool{}
+
+	for gname, members := range memberships {
+		membersToCreate[gname] = make(map[string]bool)
+		for username, _ := range authGroups[gname] {
+			// Remove authorized members from maybeDelete
+			if _, ok := members[username]; ok {
+				delete(members, username)
+				continue
+			}
+			// Create new memberships if required
+			membersToCreate[gname][username] = true
+		}
+	}
+	return membersToCreate, memberships
+}
+
+func blockUsers(gitc *gitlab.Client, usersToBlock map[string]*gitlab.User) error {
+	for username, user := range usersToBlock {
+		if user.State == "blocked" {
+			continue
+		}
+		if check {
+			log.Fatalf("User %v should be blocked, but isn't.", username)
+		}
+
+		log.Printf("Blocking %v (ID %v)", user.Email, user.ID)
+		if dryrun {
+			continue
+		}
+
+		err := gitc.Users.BlockUser(user.ID)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func unblockUsers(gitc *gitlab.Client, usersToUnblock map[string]*gitlab.User) error {
+	for username, user := range usersToUnblock {
+		if user.State == "active" {
+			continue
+		}
+		if check {
+			log.Fatalf("User %v should be unblocked, but isn't.", username)
+		}
+
+		log.Printf("Unblocking %v (ID %v)", user.Email, user.ID)
+		if dryrun {
+			continue
+		}
+
+		err := gitc.Users.UnblockUser(user.ID)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func createUsers(gitc *gitlab.Client, usersToCreate map[string]bool) error {
+	for username, _ := range usersToCreate {
+		if check {
+			log.Fatalf("User %v should exist, but doesn't.", username)
+		}
+
+		log.Printf("Creating %v", username)
+		if dryrun {
+			return nil
+		}
+		options := &gitlab.CreateUserOptions{
+			Email:               gitlab.String(fmt.Sprintf("%v@gsa.gov", username)),
+			ForceRandomPassword: gitlab.Bool(true),
+			Username:            gitlab.String(username),
+			Name:                gitlab.String(username),
+			SkipConfirmation:    gitlab.Bool(true),
+			CanCreateGroup:      gitlab.Bool(false),
+		}
+
+		newUser, _, err := gitc.Users.CreateUser(options)
+		if err != nil {
+			return err
+		}
+		idCache[fmt.Sprintf("user:%v", newUser.Username)] = newUser.ID
+	}
+	return nil
+}
+
+func createGroups(gitc *gitlab.Client, groupsToCreate map[string]bool) error {
+	for gname, _ := range groupsToCreate {
+		if check {
+			log.Fatalf("Group %v should exist, but doesn't.", gname)
+		}
+		log.Printf("Creating %v", gname)
+		if dryrun {
+			return nil
+		}
+		options := &gitlab.CreateGroupOptions{
+			Name: gitlab.String(gname),
+			Path: gitlab.String(gname),
+		}
+		_, _, err := gitc.Groups.CreateGroup(options)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func deleteGroups(gitc *gitlab.Client, groupsToDelete map[string]*gitlab.Group) error {
+	for gname, group := range groupsToDelete {
+		if check {
+			log.Fatalf("Group %v shouldn't exist, but does.", gname)
+		}
+		log.Printf("Deleting group %v", gname)
+		if dryrun {
+			continue
+		}
+		_, err := gitc.Groups.DeleteGroup(group.ID)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func createMemberships(gitc *gitlab.Client, membersToCreate map[string]map[string]bool) error {
+	for groupName, members := range membersToCreate {
+		for memberName, _ := range members {
+			if check {
+				log.Fatalf("Member %v should exist in %v, but doesn't.", memberName, groupName)
+			}
+			log.Printf("Adding %v to %v", memberName, groupName)
+			if dryrun {
+				continue
+			}
+			groupID := idCache[fmt.Sprintf("group:%v", groupName)]
+			memberOpts := &gitlab.AddGroupMemberOptions{
+				UserID:      gitlab.Int(idCache[fmt.Sprintf("user:%v", memberName)]),
+				AccessLevel: gitlab.AccessLevel(gitlab.DeveloperPermissions),
+			}
+			_, _, err := gitc.GroupMembers.AddGroupMember(groupID, memberOpts)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func deleteMemberships(gitc *gitlab.Client, membersToDelete map[string]map[string]bool) error {
+	for groupName, members := range membersToDelete {
+		for memberName, _ := range members {
+			if check {
+				log.Fatalf("Member %v shouldn't exist in %v, but does.", memberName, groupName)
+			}
+			log.Printf("Removing %v from %v", memberName, groupName)
+			if dryrun {
+				continue
+			}
+			groupID := idCache[fmt.Sprintf("group:%v", groupName)]
+			userID :=  idCache[fmt.Sprintf("user:%v", memberName)]
+			_, err := gitc.GroupMembers.RemoveGroupMember(groupID, userID)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
--- bin/users/sync_test.go
+++ bin/users/sync_test.go
@@ -1,101 +1,173 @@
 package main
 
 import (
+	"github.com/xanzy/go-gitlab"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
-var testData = []struct {
-	Name              string
-	ExistingUsers     map[string]*UserState
-	AuthorizedUsers   *AuthorizedUsers
-	WantExistingUsers map[string]*UserState
-	WantNewUsers      map[string]string
+var testResolveUsersData = []struct {
+	Name            string
+	ExistingUsers   map[string]*gitlab.User
+	AuthorizedUsers *AuthorizedUsers
+	WantToBlock     map[string]*gitlab.User
+	WantToUnblock   map[string]*gitlab.User
+	WantToCreate    map[string]bool
 }{
 	{
-		Name: "Block Nonexistent User",
-		ExistingUsers: map[string]*UserState{
-			"just.testing@gsa.gov": {
-				Enable: false,
+		Name: "Add/Block/Unblock Users",
+		ExistingUsers: map[string]*gitlab.User{
+			"just.testing": {
+				Email: "just.testing@gsa.gov",
 			},
-		},
-		AuthorizedUsers: &AuthorizedUsers{},
-		WantExistingUsers: map[string]*UserState{
-			"just.testing@gsa.gov": {
-				Enable: false,
+			"john.doe": {
+				Email: "john.doe@gsa.gov",
 			},
-		},
-		WantNewUsers: map[string]string{},
-	},
-	{
-		Name: "Block Unauthorized User",
-		ExistingUsers: map[string]*UserState{
-			"john.doe@gsa.gov": {
-				Enable: false,
+			"alexandra.thegreat": {
+				Email: "alexandra.thegreat@gsa.gov",
 			},
 		},
 		AuthorizedUsers: &AuthorizedUsers{
-			Users: map[string][]string{
-				"john.doe": []string{"notadev", "atall"},
+			Users: map[string]*AuthUser{
+				"john.doe": &AuthUser{
+					Aws_groups: []string{"notadev", "atall"},
+				},
+				"alexandra.thegreat": &AuthUser{
+					Aws_groups: []string{"luthier", "devops"},
+				},
+				"new.engineer": &AuthUser{
+					Aws_groups: []string{"appdev"},
+				},
 			},
 		},
-		WantExistingUsers: map[string]*UserState{
-			"john.doe@gsa.gov": {
-				Enable: false,
+		WantToBlock: map[string]*gitlab.User{
+			"just.testing": {
+				Email: "just.testing@gsa.gov",
+			},
+			"john.doe": {
+				Email: "john.doe@gsa.gov",
 			},
 		},
-		WantNewUsers: map[string]string{},
-	},
-	{
-		Name: "Add User",
-		ExistingUsers: map[string]*UserState{
-			"just.testing@gsa.gov": {
-				Enable: false,
-			},
-			"john.doe@gsa.gov": {
-				Enable: false,
-			},
-			"alexandra.thegreat@gsa.gov": {
-				Enable: false,
+		WantToUnblock: map[string]*gitlab.User{
+			"alexandra.thegreat": {
+				Email: "alexandra.thegreat@gsa.gov",
 			},
 		},
-		AuthorizedUsers: &AuthorizedUsers{
-			Users: map[string][]string{
-				"john.doe":           []string{"notadev", "atall"},
-				"alexandra.thegreat": []string{"luthier", "devops"},
-				"new.engineer":       []string{"appdev"},
-			},
-		},
-		WantExistingUsers: map[string]*UserState{
-			"just.testing@gsa.gov": {
-				Enable: false,
-			},
-			"john.doe@gsa.gov": {
-				Enable: false,
-			},
-			"alexandra.thegreat@gsa.gov": {
-				Enable: true,
-			},
-		},
-		WantNewUsers: map[string]string{
-			"new.engineer@gsa.gov": "new.engineer",
+		WantToCreate: map[string]bool{
+			"new.engineer": true,
 		},
 	},
 }
 
-func TestMarkUsers(t *testing.T) {
-	for _, td := range testData {
-		existingUsers := td.ExistingUsers
-		authorizedUsers := td.AuthorizedUsers
-		newUsers := map[string]string{}
-		markUsers(existingUsers, newUsers, authorizedUsers)
+func TestResolveUsers(t *testing.T) {
+	for _, td := range testResolveUsersData {
+		toBlock, toUnblock, toCreate := resolveUsers(td.ExistingUsers, td.AuthorizedUsers)
 
-		if !cmp.Equal(existingUsers, td.WantExistingUsers) {
-			t.Errorf("%v failed: %s", td.Name, cmp.Diff(existingUsers, td.WantExistingUsers))
-		}
-		if !cmp.Equal(newUsers, td.WantNewUsers) {
-			t.Errorf("%v failed: %s", td.Name, cmp.Diff(newUsers, td.WantNewUsers))
-		}
+		assertEqual(t, td.Name, toBlock, td.WantToBlock)
+		assertEqual(t, td.Name, toUnblock, td.WantToUnblock)
+		assertEqual(t, td.Name, toCreate, td.WantToCreate)
+	}
+}
+
+var testResolveGroupsData = []struct {
+	Name         string
+	GitlabGroups map[string]*gitlab.Group
+	AuthGroups   map[string]map[string]bool
+	WantToCreate map[string]bool
+	WantToDelete map[string]*gitlab.Group
+}{
+	{
+		Name: "Create/Delete Groups",
+		GitlabGroups: map[string]*gitlab.Group{
+			"lg":        &gitlab.Group{},
+			"vestigial": &gitlab.Group{},
+		},
+		AuthGroups: map[string]map[string]bool{
+			"lg": map[string]bool{
+				"user1": true,
+			},
+			"new_admin_group": map[string]bool{
+				"user2": true,
+			},
+		},
+		WantToCreate: map[string]bool{
+			"new_admin_group": true,
+		},
+		WantToDelete: map[string]*gitlab.Group{
+			"vestigial": &gitlab.Group{},
+		},
+	},
+}
+
+func TestResolveGroups(t *testing.T) {
+	for _, td := range testResolveGroupsData {
+		toCreate, toDelete := resolveGroups(td.GitlabGroups, td.AuthGroups)
+
+		assertEqual(t, td.Name, toCreate, td.WantToCreate)
+		assertEqual(t, td.Name, toDelete, td.WantToDelete)
+	}
+}
+
+var testResolveMembersData = []struct {
+	Name         string
+	Memberships  map[string]map[string]bool
+	AuthGroups   map[string]map[string]bool
+	WantToCreate map[string]map[string]bool
+	WantToDelete map[string]map[string]bool
+}{
+	{
+		Name: "Create/Delete Members",
+		Memberships: map[string]map[string]bool{
+			"lg": map[string]bool{
+				"lg_dev": true,
+				"ex_dev": true,
+			},
+		},
+		AuthGroups: map[string]map[string]bool{
+			"lg": map[string]bool{
+				"lg_dev":  true,
+				"new_dev": true,
+			},
+		},
+		WantToCreate: map[string]map[string]bool{
+			"lg": map[string]bool{
+				"new_dev": true,
+			},
+		},
+		WantToDelete: map[string]map[string]bool{
+			"lg": map[string]bool{
+				"ex_dev": true,
+			},
+		},
+	},
+}
+
+func TestResolveMembers(t *testing.T) {
+	for _, td := range testResolveMembersData {
+		toCreate, toDelete := resolveMembers(td.Memberships, td.AuthGroups)
+
+		assertEqual(t, td.Name, toCreate, td.WantToCreate)
+		assertEqual(t, td.Name, toDelete, td.WantToDelete)
+	}
+}
+
+func TestGetAuthorizedGroups(t *testing.T) {
+	want := map[string]map[string]bool{
+		"lg": map[string]bool{
+			"gitlab.and.group.please": true,
+		},
+	}
+	authUsers, err := getAuthorizedUsers("test_users.yaml")
+	if err != nil {
+		t.Errorf("Failed: %v", err)
+	}
+	authGroups := getAuthorizedGroups(authUsers)
+	assertEqual(t, "Parse groups", authGroups, want)
+}
+
+func assertEqual(t *testing.T, testName string, got interface{}, want interface{}) {
+	if !cmp.Equal(got, want) {
+		t.Errorf("%v failed: %s", testName, cmp.Diff(got, want))
 	}
 }
