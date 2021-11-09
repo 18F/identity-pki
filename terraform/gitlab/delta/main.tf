14758613fd43286b3415f0706b43444d5cfc9aa5
James Tranovich <james.tranovich@gsa.gov>
2021-11-09 10:41:33 -0800

    typo

--- terraform/gitlab/delta/main.tf
+++ terraform/gitlab/delta/main.tf
@@ -14,7 +14,7 @@ module "main" {
   source = "../module"
 
   bootstrap_main_git_ref_default = "stages/gitlabdelta"
-  env_name                       = "bravo"
+  env_name                       = "delta"
   region                         = "us-west-2"
   slack_events_sns_hook_arn      = "arn:aws:sns:us-west-2:034795980528:slack-otherevents"
   #enforce                       = true
