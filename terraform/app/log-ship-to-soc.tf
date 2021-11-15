c75348d7fdb6342326a694308fe3739cc3b0eab9
Vivek Chugh <vivek.chugh@gsa.gov>
2021-11-15 10:55:31 -0500

    Updated code with environment

--- terraform/app/log-ship-to-soc.tf
+++ terraform/app/log-ship-to-soc.tf
@@ -1,20 +1,21 @@
 locals {
-  count = data.aws_caller_identity.current.account_id == "894947205914" ? 1 : 0
+  environment = var.env_name == "int" || var.env_name == "staging" || var.env_name == "prod" ? 1 : 0
+
 }
-module "log-ship-s3-soc" {
-  count                               = local.count
+module "log-ship-s3-soc-nonprod" {
+  count                               = local.environment
   source                              = "../modules/log_ship_s3_soc"
   region                              = "us-west-2"
   cloudwatch_subscription_filter_name = "log-ship"
-  cloudwatch_log_group_name           = {
-                                        "${var.env_name}_flow_log_group" = " "
-                                        "${var.env_name}/dns/query" = " "
-                                        "${var.env_name}_/var/log/audit/audit.log" = " "
-                                        "${var.env_name}_/var/log/auth.log" = " "
-                                        "${var.env_name}_/var/log/kern.log" = " "
-                                        "${var.env_name}_/var/log/messages" = " "
-                                        "${var.env_name}_/var/log/syslog" = " "
-                                      }   
-  env_name                            = var.env_name
-  soc_destination_arn                 = var.soc_destination_arn
+  cloudwatch_log_group_name = {
+    "${var.env_name}_flow_log_group"           = " "
+    "${var.env_name}/dns/query"                = " "
+    "${var.env_name}_/var/log/auth.log"        = " "
+    "${var.env_name}_/var/log/audit/audit.log" = " "
+    "${var.env_name}_/var/log/kern.log"        = " "
+    "${var.env_name}_/var/log/messages"        = " "
+    "${var.env_name}_/var/log/syslog"          = " "
+  }
+  env_name            = var.env_name
+  soc_destination_arn = var.soc_destination_arn
 }
