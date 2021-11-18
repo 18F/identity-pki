674f2cd3e02c39e595d5cf029efd0f978a363972
Vladimir Toussaint <toussaiv@amazon.com>
2021-11-18 17:40:23 -0500

    Terraform FMT - Config Access Key Rotation

--- terraform/core/module/config-access-key-rotation.tf
+++ terraform/core/module/config-access-key-rotation.tf
@@ -3,6 +3,7 @@ module "config_access_key_rotation" {
 
   config_access_key_rotation_name        = var.config_access_key_rotation_name
   region                                 = var.region
+  slack_events_sns_topic                 = var.slack_events_sns_topic
   config_access_key_rotation_frequency   = var.config_access_key_rotation_frequency
   config_access_key_rotation_max_key_age = var.config_access_key_rotation_max_key_age
   config_access_key_rotation_code        = "../../modules/config_access_key_rotation/${var.config_access_key_rotation_code}"
