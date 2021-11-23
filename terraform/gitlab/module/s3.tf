7d99953db85f7880750652e4a7c30d865be100f2
Ryan Brown <ryan.d.brown@gsa.gov>
2021-11-22 16:27:08 -0800

    fix other s3 buckets

--- terraform/gitlab/module/s3.tf
+++ terraform/gitlab/module/s3.tf
@@ -34,6 +34,14 @@ resource "aws_s3_bucket" "backups" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "config_access_block" {
+  bucket = aws_s3_bucket.backups.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket" "config" {
   bucket                  = "gitlab-${var.env_name}-config"
   acl                     = "private"
@@ -54,6 +62,14 @@ resource "aws_s3_bucket" "config" {
     Name        = "gitlab-${var.env_name}-config"
     Environment = "${var.env_name}"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "config_access_block" {
+  bucket = aws_s3_bucket.config.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 locals {
