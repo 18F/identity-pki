6e08cc7bc659e2b46a3b00bbc7afad63c4ceee3e
Ryan Brown <ryan.d.brown@gsa.gov>
2021-12-08 15:23:30 -0800

    temp disable identity-soc

--- kitchen/cookbooks/identity-soc/recipes/default.rb
+++ kitchen/cookbooks/identity-soc/recipes/default.rb
@@ -1,7 +1,7 @@
-unless node.chef_environment == 'prod'
-   excluding until we have publicly accessible endpoints and a config that does
-   not perform patching and software deployments.
-   include_recipe 'identity-soc::bigfix'
-   include_recipe 'identity-soc::endgame'
-   include_recipe 'identity-soc::fireeye'
-end
+#unless node.chef_environment == 'prod'
+#   excluding until we have publicly accessible endpoints and a config that does
+#   not perform patching and software deployments.
+#   include_recipe 'identity-soc::bigfix'
+#   include_recipe 'identity-soc::endgame'
+#   include_recipe 'identity-soc::fireeye'
+#end
