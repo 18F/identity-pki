af91bb243e70bbe3c5332faecb1605ba0407a6df
Vivek Chugh <vivek.chugh@gsa.gov>
2021-12-09 09:45:54 -0500

    Need to still maintain S3 buckets

--- terraform/waf/module/logging.tf
+++ terraform/waf/module/logging.tf
@@ -1,6 +1,63 @@
+locals {
+  
+  s3_inventory_bucket_arn = "arn:${data.aws_partition.current.partition}:s3:::login-gov.s3-inventory.${data.aws_caller_identity.current.account_id}-${var.region}"
+}
+
+resource "aws_s3_bucket" "waf_logs" {
+  bucket = "login-gov.${local.web_acl_name}-logs.${data.aws_caller_identity.current.account_id}-${var.region}"
+  acl    = "private"
+
+  logging {
+    target_bucket = "login-gov.s3-access-logs.${data.aws_caller_identity.current.account_id}-${var.region}"
+    target_prefix = "login-gov.${local.web_acl_name}-logs.${data.aws_caller_identity.current.account_id}-${var.region}/"
+  }
+
+  tags = {
+    environment = var.env
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    id      = "expire"
+    prefix  = "/"
+    enabled = true
+
+    transition {
+      storage_class = "INTELLIGENT_TIERING"
+    }
+    noncurrent_version_transition {
+      storage_class = "INTELLIGENT_TIERING"
+    }
+    expiration {
+      days = 2190
+    }
+    noncurrent_version_expiration {
+      days = 2190
+    }
+  }
+}
+
+module "waf_log_bucket_config" {
+  source = "github.com/18F/identity-terraform//s3_config?ref=7e11ebe24e3a9cbc34d1413cf4d20b3d71390d5b"
+
+  bucket_name_override = aws_s3_bucket.waf_logs.id
+  inventory_bucket_arn = local.s3_inventory_bucket_arn
+}
+
 
 resource "aws_cloudwatch_log_group" "cw_waf_logs" {
-  name              = "aws-waf-logs-${local.web_acl_name}"
+  name              = "aws-waf-logs-${local.web_acl_name}"   #stream name must start with "aws-waf-logs"
   retention_in_days = 365
 }
 
