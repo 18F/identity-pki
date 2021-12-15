b513284b3a8eacd53a8bdb59f2247eab226e918b
Vivek Chugh <vivek.chugh@gsa.gov>
2021-12-15 14:39:19 -0500

    Initial Commit

---
+++ terraform/all/module/prismacloudrole.tf
@@ -0,0 +1,123 @@
+locals {
+  externalId    = "978CE893-7812-4790-86CC-C3FF275BD7FB"
+  accountNumber = ""
+}
+
+resource "aws_iam_role" "PrismaCloud-connect-role" {
+  name               = "PrismaCloudConnectRole"
+  assume_role_policy = data.aws_iam_policy_document.PrismaCloud-trust.json
+}
+resource "aws_iam_policy" "PrismaCloud-connect-policy" {
+  name        = "PrismaCloud-connect"
+  description = "PrismaCloud Connection Policy"
+  policy      = data.aws_iam_policy_document.prismacloud-policy.json
+}
+
+resource "aws_iam_policy_attachment" "PrismaCloud-connect-policy-attach" {
+  name       = "PrismaCloud-ro-policy-attach"
+  role       = aws_iam_role.PrismaCloud-connect-role.name
+  policy_arn = aws_iam_policy.PrismaCloud-connect-policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "PrismaCloud-connect-policy-attach-2" {
+  role       = aws_iam_role.PrismaCloud-connect-role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
+}
+
+data "aws_iam_policy_document" "PrismaCloud-trust" {
+  statement {
+    sid     = "trust"
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam:${local.accountNumber}:root"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "sts:ExternalId"
+      values   = [local.externalId]
+    }
+
+  }
+}
+
+data "aws_iam_policy_attachment" "prismacloud-policy" {
+  statement {
+    sid    = "prismacloudpolicy"
+    effect = "Allow"
+    actions = [
+      "apigateway:GET",
+      "backup:ListBackupVaults",
+      "backup:ListTags",
+      "backup:GetBackupVaultAccessPolicy",
+      "cloudwatch:ListTagsForResource",
+      "cognito-identity:ListTagsForResource",
+      "cognito-idp:ListTagsForResource",
+      "ds:ListTagsForResource",
+      "dynamodb:ListTagsOfResource",
+      "ec2:SearchTransitGatewayRoutes",
+      "ec2:GetEbsEncryptionByDefault",
+      "ecr:DescribeImages",
+      "ecr:GetLifecyclePolicy",
+      "ecr:ListTagsForResource",
+      "eks:ListFargateProfiles",
+      "eks:DescribeFargateProfile",
+      "eks:ListTagsForResource",
+      "elasticbeanstalk:ListTagsForResource",
+      "elasticfilesystem:DescribeFileSystemPolicy",
+      "elasticfilesystem:DescribeTags",
+      "elasticache:ListTagsForResource",
+      "es:ListTags",
+      "glacier:GetVaultLock",
+      "glacier:ListTagsForVault",
+      "glue:GetConnections",
+      "glue:GetSecurityConfigurations",
+      "kafka:ListClusters",
+      "logs:GetLogEvents",
+      "mq:listBrokers",
+      "mq:describeBroker",
+      "ram:GetResourceShares",
+      "sns:ListTagsForResource",
+      "sns:ListPlatformApplications",
+      "ssm:GetDocument",
+      "ssm:GetParameters",
+      "ssm:ListTagsForResource",
+      "sqs:SendMessage",
+      "elasticmapreduce:ListSecurityConfigurations",
+      "elasticmapreduce:GetBlockPublicAccessConfiguration",
+      "sns:listSubscriptions",
+      "wafv2:ListResourcesForWebACL",
+      "wafv2:ListWebACLs",
+      "wafv2:ListTagsForResource",
+      "wafv2:GetWebACL",
+      "wafv2:GetLoggingConfiguration",
+      "waf:GetWebACL",
+      "waf:ListTagsForResource",
+      "waf-regional:GetLoggingConfiguration",
+      "waf:GetLoggingConfiguration",
+      "waf-regional:ListResourcesForWebACL",
+      "waf-regional:ListTagsForResource",
+      "codebuild:BatchGetProjects",
+      "s3:DescribeJob",
+      "s3:ListJobs",
+      "s3:GetJobTagging",
+      "ssm:GetInventory",
+      "shield:GetSubscriptionState",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:GetAuthorizationToken",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetLifecyclePolicyPreview",
+      "kms:Decrypt",
+      "secretsmanager:GetSecretValue",
+      "lambda:GetLayerVersion",
+      "ssm:GetParameter",
+      "securityhub:BatchImportFindings",
+      "lambda:GetFunction"
+    ]
+    resources = ["*"]
+  }
+}
+
