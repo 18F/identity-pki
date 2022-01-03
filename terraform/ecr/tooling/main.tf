bcaf69a088318796e4890e441b9ddc97072cc6fa
Ryan Brown <ryan.d.brown@gsa.gov>
2022-01-03 09:15:20 -0800

    tagging prototype

--- terraform/ecr/tooling/main.tf
+++ terraform/ecr/tooling/main.tf
@@ -9,3 +9,36 @@ terraform {
   backend "s3" {
   }
 }
+
+locals {
+  repos = {
+    alpine-internal = {
+      prod = "write"
+      tooling = "write"
+      
+    }
+
+    nginx-internal = {
+      secondAChoice = "foobar"
+      secondBChoice = "barfoo"
+    }
+  }
+}
+
+
+module "settings" {
+  source = "../module-ecr-global-settings"
+
+  env_name                       = "tooling"
+  region                         = "us-west-2"
+  continuous_scan_filter         = "*"
+  scan_on_push_filter            = "*"
+}
+
+module "repos" {
+  source = "../module-ecr-repo"
+
+  env_name                       = "tooling"
+  region                         = "us-west-2"
+
+}
