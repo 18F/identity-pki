658942360dec5816165b4fd7c76ec03a0a63ff43
Paul Hirsch <paul.hirsch@gsa.gov>
2022-01-14 01:16:31 -0600

    Draft common queries

--- terraform/app/cloudwatch-log-queries.tf
+++ terraform/app/cloudwatch-log-queries.tf
@@ -1,35 +1,8 @@
-yamldecode(templatefile("cloudwatch-log-queries.yml", {"env" = "frank"}))
-{
-  "waf/blocked-by-path" = {
-    "logs" = [
-      "aws-waf-logs-frank-idp-waf",
-    ]
-    "query" = <<-EOT
-    filter action != "ALLOW"
-    | stats count() as count by httpRequest.httpMethod,httpRequest.uri
-    | sort count desc
-
-    EOT
-  }
-  "waf/blocked-by-rule-and-ip" = {
-    "logs" = [
-      "aws-waf-logs-frank-idp-waf",
-    ]
-    "query" = <<-EOT
-    filter action != "ALLOW"
-    | stats count() as count by terminatingRuleId,httpRequest.clientIp
-    | sort count desc
-
-    EOT
-  }
-}
-
-
 resource "aws_cloudwatch_query_definition" "default" {
-    for_each = var.queries
+  for_each = yamldecode(templatefile("cloudwatch-log-queries.yml", { "env" = var.env_name }))
 
-    name = "${env}/${key}"
-    log_group_names = value.logs
-    query_string = value.query
+  name            = "${var.env_name}/${each.key}"
+  log_group_names = each.value.logs
+  query_string    = each.value.query
 }
 
