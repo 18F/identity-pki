f180fd18e9482d731a60b8d3ccf44f70c93bf22d
Paul Hirsch <paul.hirsch@gsa.gov>
2022-01-18 08:45:08 -0600

    Allow users that have no aws_groups memberships

--- terraform/master/module/iam_users.tf
+++ terraform/master/module/iam_users.tf
@@ -1,7 +1,7 @@
 # Users and group memberships
 module "master_users" {
-  source = "github.com/18F/identity-terraform//iam_masterusers?ref=3485c49ef8c1cdeb50bd101b7926dca8d26d8ded"
-  # source = "../../../../identity-terraform/iam_masterusers"
+  #source = "github.com/18F/identity-terraform//iam_masterusers?ref=3485c49ef8c1cdeb50bd101b7926dca8d26d8ded"
+  source = "../../../../identity-terraform/iam_masterusers"
 
   user_map         = var.user_map
   group_depends_on = module.iam_groups.group_names
