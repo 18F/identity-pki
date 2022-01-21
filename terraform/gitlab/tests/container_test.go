5ab292c50757de364712f55a46fd889a92bd8983
Alex Kritikos <alex.kritikos@gsa.gov>
2022-01-21 07:54:46 -0800

    Test and document s5.14: container restart policy <= 5 (#4190)
    * Add test for s5.14, refactor and parallelize
    
    * Add doc for s5.14 and fix doc status for s5.13

--- terraform/gitlab/tests/container_test.go
+++ terraform/gitlab/tests/container_test.go
@@ -9,13 +9,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func _TestMemory(t *testing.T) {
+func RunOnRunners(t *testing.T, cmd string) []string {
+	t.Parallel()
 	asgName := env_name + "-gitlab_runner"
 	instances := aws.GetInstanceIdsForAsg(t, asgName, region)
-	cmd := "docker ps --quiet --all | xargs docker inspect --format '{{.Name}}: {{ .HostConfig.Memory }}'"
 	result := RunCommandOnInstances(t, instances, cmd)
 	t.Log(*result.StandardOutputContent)
-	for _, s := range strings.Split(*result.StandardOutputContent, "\n") {
+	return strings.Split(*result.StandardErrorContent, "\n")
+}
+
+func _TestMemory(t *testing.T) {
+	cmd := "docker ps --quiet --all | xargs docker inspect --format '{{.Name}}: {{ .HostConfig.Memory }}'"
+	for _, s := range RunOnRunners(t, cmd) {
 		// TODO: Remove conditional once https://gitlab.com/gitlab-org/gitlab-runner/-/merge_requests/1834 is merged
 		if !regexp.MustCompile("-postgres-|-redis-").MatchString(s) {
 			assert.NotRegexp(t, ": 0$", s)
@@ -24,31 +29,29 @@ func _TestMemory(t *testing.T) {
 }
 
 func _TestCPUShares(t *testing.T) {
-	asgName := env_name + "-gitlab_runner"
-	instances := aws.GetInstanceIdsForAsg(t, asgName, region)
 	cmd := "docker ps --quiet --all | xargs docker inspect --format '{{ .Name }}: {{ .HostConfig.CpuShares }}'"
-
-	result := RunCommandOnInstances(t, instances, cmd)
-	t.Log(*result.StandardOutputContent)
-	for _, s := range strings.Split(*result.StandardOutputContent, "\n") {
+	for _, s := range RunOnRunners(t, cmd) {
 		// TODO: Remove conditional once https://gitlab.com/gitlab-org/gitlab-runner/-/merge_requests/1834 is merged
 		if !regexp.MustCompile("-postgres-|-redis-").MatchString(s) {
 			assert.NotRegexp(t, ": 0$", s)
 		}
 	}
-
 }
 
 func _TestBoundHostInterface(t *testing.T) {
 	cmd := "docker ps --quiet | xargs docker inspect --format '{{ .Name }}: {{ .NetworkSettings.Ports }}'"
-
-	asgName := env_name + "-gitlab_runner"
-	instances := aws.GetInstanceIdsForAsg(t, asgName, region)
-		result := RunCommandOnInstances(t, instances, cmd)
-	t.Log(*result.StandardOutputContent)
-
-	for _, s := range strings.Split(*result.StandardOutputContent, "\n") {
+	for _, s := range RunOnRunners(t, cmd) {
 		assert.NotRegexp(t, "0\\.0\\.0\\.0|::", s)
+	}
+}
+
+func _TestContainerRestartPolicy(t *testing.T) {
+	cmd := "docker ps --quiet --all | xargs docker inspect --format '{{ .Name }}: RestartPolicyName={{ .HostConfig.RestartPolicy.Name }} MaximumRetryCount={{ .HostConfig.RestartPolicy.MaximumRetryCount }}'"
+	for _, s := range RunOnRunners(t, cmd) {
+		assert.NotRegexp(t, "RestartPolicyName=always", s)
+		if regexp.MustCompile("RestartPolicyName=on-failure").MatchString(s) {
+			assert.Regexp(t, "MaximumRetryCount=[0-5]$", s)
+		}
 	}
 }
 
@@ -58,6 +61,7 @@ func TestJobContainers(t *testing.T) {
 	t.Run("Require memory arg", _TestMemory)
 	t.Run("Require cpu_shares arg", _TestCPUShares)
 	t.Run("Require bound interfaces", _TestBoundHostInterface)
+	t.Run("Ensure on-failure restart policy <= 5", _TestContainerRestartPolicy)
 
 	// TODO: teardown: cancel long-running job
 }
