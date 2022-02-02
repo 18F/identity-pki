9ceafde8f99978a475884949758c6cb0db461f5a
Tim Spencer <timothy.spencer@gsa.gov>
2022-02-02 09:55:27 -0800

    fixed up alarm to point at default runner group in the module (#4262)

--- terraform/gitlab/module/alarms.tf
+++ terraform/gitlab/module/alarms.tf
@@ -1,7 +1,7 @@
 module "gitlab_insufficent_instances_alerts" {
   source = "../../modules/asg_insufficent_instances_alerts"
 
-  asg_name = aws_autoscaling_group.gitlab.name
+  asg_name = module.gitlab_build_runner_pool.runner_asg_name
 
   alarm_actions = [var.slack_events_sns_hook_arn]
 }
@@ -9,7 +9,7 @@ module "gitlab_insufficent_instances_alerts" {
 module "gitlab_runner_insufficent_instances_alerts" {
   source = "../../modules/asg_insufficent_instances_alerts"
 
-  asg_name = aws_autoscaling_group.gitlab_runner.name
+  asg_name = module.gitlab_build_runner_pool.runner_asg_name
 
   alarm_actions = [var.slack_events_sns_hook_arn]
 }
--- terraform/modules/gitlab_runner_pool/output.tf
+++ terraform/modules/gitlab_runner_pool/output.tf
@@ -5,3 +5,7 @@ output "proxy_security_group_id" {
 output "proxy_asg_id" {
   value = aws_autoscaling_group.gitlab_runner.id
 }
+
+output "runner_asg_name" {
+  value = aws_autoscaling_group.gitlab_runner.name
+}
