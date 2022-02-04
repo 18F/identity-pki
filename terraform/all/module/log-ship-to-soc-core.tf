6c5663d03a3cc00128fc09ea6b5fbebca16376fc
Paul Hirsch <59626817+pauldoomgov@users.noreply.github.com>
2022-02-04 09:54:52 -0600

    Add SOC log shipping notes and make code region portable (#4275)

--- terraform/all/module/log-ship-to-soc-core.tf
+++ terraform/all/module/log-ship-to-soc-core.tf
@@ -1,10 +1,13 @@
+# For new AWS accounts you MUST contact the SOCaaS team to allow this account
+# permission to the destination ARN.
+# See: https://github.com/18F/identity-devops/wiki/Runbook:-GSA-SOC-as-a-Service-(SOCaaS)#cloudwatch-shipping-important-note
 module "log-ship-to-soc-cloudtrail-logs" {
   source                              = "../../modules/log_ship_to_soc"
-  region                              = "us-west-2"
+  region                              = var.region
   cloudwatch_subscription_filter_name = "log-ship-to-soc"
   cloudwatch_log_group_name = {
     "CloudTrail/DefaultLogGroup" = ""
   }
   env_name            = "account"
-  soc_destination_arn = "arn:aws:logs:us-west-2:752281881774:destination:elp-cloudtrail-lg"
+  soc_destination_arn = "arn:aws:logs:${var.region}:752281881774:destination:elp-cloudtrail-lg"
 }
--- terraform/app/log-ship-to-soc.tf
+++ terraform/app/log-ship-to-soc.tf
@@ -1,7 +1,10 @@
+# For new AWS accounts you MUST contact the SOCaaS team to allow this account
+# permission to the destination ARN.
+# See: https://github.com/18F/identity-devops/wiki/Runbook:-GSA-SOC-as-a-Service-(SOCaaS)#cloudwatch-shipping-important-note
 module "log-ship-to-soc-os-logs" {
   count                               = var.send_cw_to_soc
   source                              = "../modules/log_ship_to_soc"
-  region                              = "us-west-2"
+  region                              = var.region
   cloudwatch_subscription_filter_name = "log-ship-to-soc"
   cloudwatch_log_group_name = {
     "${var.env_name}_/var/log/auth.log"        = ""
@@ -18,27 +21,25 @@ module "log-ship-to-soc-os-logs" {
 module "log-ship-to-soc-vpc-flow-log" {
   count                               = var.send_cw_to_soc
   source                              = "../modules/log_ship_to_soc"
-  region                              = "us-west-2"
+  region                              = var.region
   cloudwatch_subscription_filter_name = "log-ship-to-soc"
   cloudwatch_log_group_name = {
     "${var.env_name}_flow_log_group" = ""
   }
   env_name            = "${var.env_name}-vpc"
-  soc_destination_arn = "arn:aws:logs:us-west-2:752281881774:destination:elp-flow-lg"
+  soc_destination_arn = "arn:aws:logs:${var.region}:752281881774:destination:elp-flow-lg"
 }
 
 ### sending dns query logs to the dns query log endpoint
 module "log-ship-to-soc-dns-query-log" {
   count                               = var.send_cw_to_soc
   source                              = "../modules/log_ship_to_soc"
-  region                              = "us-west-2"
+  region                              = var.region
   cloudwatch_subscription_filter_name = "log-ship-to-soc"
   cloudwatch_log_group_name = {
     "${var.env_name}/dns/query" = ""
   }
   env_name            = "${var.env_name}-dns"
-  soc_destination_arn = "arn:aws:logs:us-west-2:752281881774:destination:elp-dns-lg"
+  soc_destination_arn = "arn:aws:logs:${var.region}:752281881774:destination:elp-dns-lg"
 }
-
-
 
