cbaad1f1b2774d23cddc9057ff35300eb4f9b1c3
Mitchell Henke <mitchell.henke@gsa.gov>
2022-03-03 12:49:10 -0600

    add acuant create document alert (#4364)

--- terraform/app/cloudwatch-alarms-external-service.tf
+++ terraform/app/cloudwatch-alarms-external-service.tf
@@ -39,3 +39,24 @@ resource "aws_cloudwatch_metric_alarm" "idp_lexis_nexis_phone_finder_response_ti
   insufficient_data_actions = []
   alarm_actions             = local.low_priority_alarm_actions
 }
+
+resource "aws_cloudwatch_metric_alarm" "idp_acuant_create_document_response_time_high_alarm" {
+  count = var.idp_external_service_alarms_enabled
+
+  alarm_name          = "${var.env_name}-acuant-create-document-response-time-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "3"
+  datapoints_to_alarm = "3"
+  metric_name         = "faraday-response-time"
+  namespace           = "${var.env_name}/idp-external-service"
+  period              = "60"
+  extended_statistic  = "p50"
+  dimensions = {
+    Service = "acuant_doc_auth_create_document"
+  }
+  threshold                 = "5"
+  alarm_description         = "Acuant Create Document 50th pecentile response time in ${var.env_name} has been greater than 5 seconds for 3 minutes"
+  treat_missing_data        = "ignore"
+  insufficient_data_actions = []
+  alarm_actions             = local.low_priority_alarm_actions
+}
