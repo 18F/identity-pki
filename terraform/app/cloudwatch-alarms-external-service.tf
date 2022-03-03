c9cb9a3518b330be1dbf549eed7b237424d494f0
Mitchell Henke <mitchell.henke@gsa.gov>
2022-03-03 13:04:36 -0600

    add vendor outage runbooks (#4365)

--- terraform/app/cloudwatch-alarms-external-service.tf
+++ terraform/app/cloudwatch-alarms-external-service.tf
@@ -13,7 +13,11 @@ resource "aws_cloudwatch_metric_alarm" "idp_lexis_nexis_instant_verify_response_
     Service = "lexis_nexis_instant_verify"
   }
   threshold                 = "10"
-  alarm_description         = "LN Instant Verify 90th pecentile response time in ${var.env_name} has been greater than 10 seconds for 3 minutes"
+  alarm_description         = <<EOM
+LN Instant Verify 90th pecentile response time in ${var.env_name} has been greater than 10 seconds for 3 minutes
+
+Runbook: https://handbook.login.gov/articles/vendor-outage-response-process.html
+EOM
   treat_missing_data        = "ignore"
   insufficient_data_actions = []
   alarm_actions             = local.low_priority_alarm_actions
@@ -34,7 +38,11 @@ resource "aws_cloudwatch_metric_alarm" "idp_lexis_nexis_phone_finder_response_ti
     Service = "lexis_nexis_phone_finder"
   }
   threshold                 = "10"
-  alarm_description         = "LN Phone Finder 90th pecentile response time in ${var.env_name} has been greater than 10 seconds for 3 minutes"
+  alarm_description         = <<EOM
+LN Phone Finder 90th pecentile response time in ${var.env_name} has been greater than 10 seconds for 3 minutes
+
+Runbook: https://handbook.login.gov/articles/vendor-outage-response-process.html
+EOM
   treat_missing_data        = "ignore"
   insufficient_data_actions = []
   alarm_actions             = local.low_priority_alarm_actions
@@ -55,7 +63,11 @@ resource "aws_cloudwatch_metric_alarm" "idp_acuant_create_document_response_time
     Service = "acuant_doc_auth_create_document"
   }
   threshold                 = "5"
-  alarm_description         = "Acuant Create Document 50th pecentile response time in ${var.env_name} has been greater than 5 seconds for 3 minutes"
+  alarm_description         = <<EOM
+Acuant Create Document 50th pecentile response time in ${var.env_name} has been greater than 5 seconds for 3 minutes
+
+Runbook: https://handbook.login.gov/articles/vendor-outage-response-process.html
+EOM
   treat_missing_data        = "ignore"
   insufficient_data_actions = []
   alarm_actions             = local.low_priority_alarm_actions
