5e2d36286abde077f13499b1603cbc3c23794580
Zach Margolis <zachmargolis@users.noreply.github.com>
2022-03-09 13:46:19 -0800

    Update slack_lambda reference to get prettier formatting (#4391)
    - See 18F/identity-terraform#119

--- terraform/all/module/sns-alerts.tf
+++ terraform/all/module/sns-alerts.tf
@@ -83,7 +83,7 @@ resource "aws_ssm_parameter" "account_alarm_slack_usw2" {
 
 module "slack_lambda_usw2" {
   for_each = local.slack_channel_map
-  source   = "github.com/18F/identity-terraform//slack_lambda?ref=ee7140bb9277354871bff9e2400d17b6e1171c65"
+  source   = "github.com/18F/identity-terraform//slack_lambda?ref=0d757c8d00e1b8d39fcb0b201a5b9e7c7caecbe0"
   #source = "../../../../identity-terraform/slack_lambda"
 
   lambda_name                 = "snstoslack_login_${each.key}"
@@ -151,7 +151,7 @@ resource "aws_ssm_parameter" "account_alarm_slack_use1" {
 
 module "slack_lambda_use1" {
   for_each = local.slack_channel_map
-  source   = "github.com/18F/identity-terraform//slack_lambda?ref=ee7140bb9277354871bff9e2400d17b6e1171c65"
+  source   = "github.com/18F/identity-terraform//slack_lambda?ref=0d757c8d00e1b8d39fcb0b201a5b9e7c7caecbe0"
   #source = "../../../../identity-terraform/slack_lambda"
   providers = {
     aws = aws.use1
