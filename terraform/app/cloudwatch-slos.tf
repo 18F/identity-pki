05b1591b6fc197c25d74c82d640a59e765f09ff9
Alex Kritikos <alex.kritikos@gsa.gov>
2022-03-10 09:36:43 -0800

    Update SLO lambda sha (#4393)
    Update SLO lambda sha

--- terraform/app/cloudwatch-slos.tf
+++ terraform/app/cloudwatch-slos.tf
@@ -1,6 +1,6 @@
 module "cloudwatch_sli" {
-  source = "github.com/18F/identity-terraform//slo_lambda?ref=ba96a78a026a5ac77deecd7e6e6c615970a389f2"
-  #source = "../../../identity-terraform/slo_lambda"
+  source = "github.com/18F/identity-terraform//slo_lambda?ref=e088c86308b757e37b608ae54b6e7b5f3e7075a3"
+  # source = "../../../identity-terraform/slo_lambda"
 
   env_name          = var.env_name
   load_balancer_arn = aws_alb.idp.arn
