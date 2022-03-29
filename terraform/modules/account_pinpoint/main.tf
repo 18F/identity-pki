55e87f8084630a6b7b9eed6fa7670bc3f71158a0
Jonathan Pirro <jonathan.pirro@gsa.gov>
2022-03-29 12:33:31 -0500

    fix superfluous/overlapping items (password policy, S3 blocks) (BREAKING) (#4458)
    * remove superfluous/overlapping items (password policy, S3 blocks) from non-all/ directories
    
    * re-add account_pinpoint module but remove unused "admin" role

--- terraform/modules/account_pinpoint/main.tf
+++ terraform/modules/account_pinpoint/main.tf
@@ -1,17 +1,46 @@
-# Account wide resources - These should only be applied by one region's Terraform
+# Role assumed by idp role in main account for delivering SMS messages and
+# voice calls.
+resource "aws_iam_role" "idp-pinpoint" {
+  name = "idp-pinpoint"
 
-resource "aws_iam_account_password_policy" "strict" {
-  minimum_password_length        = 30
-  max_password_age               = 365
-  require_lowercase_characters   = true
-  require_numbers                = true
-  require_uppercase_characters   = true
-  require_symbols                = true
-  allow_users_to_change_password = true
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {"AWS": "arn:aws:iam::${var.main_account_id}:root"}
+    }
+  ]
+}
+EOF
+
 }
 
-# Block creation of public S3 buckets, account-wide
-resource "aws_s3_account_public_access_block" "acct-policy" {
-  block_public_acls   = true
-  block_public_policy = true
+# Allow sending SMS/Voice messages with Pinpoint
+# Allow managing opt-in/opt-out of phone numbers
+resource "aws_iam_role_policy" "idp-pinpoint-send" {
+  name   = "idp-pinpoint-send"
+  role   = aws_iam_role.idp-pinpoint.id
+  policy = <<EOM
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "mobiletargeting:PhoneNumberValidate",
+                "mobiletargeting:SendMessages",
+                "mobiletargeting:SendUsersMessages",
+                "sms-voice:SendVoiceMessage",
+                "sns:CheckIfPhoneNumberIsOptedOut",
+                "sns:ListPhoneNumbersOptedOut",
+                "sns:OptInPhoneNumber"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOM
 }
