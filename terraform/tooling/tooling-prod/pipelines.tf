5cc7b00de2270c59795ecd4a45667162102b54b4
Tim Spencer <timothy.spencer@gsa.gov>
2022-03-30 13:03:22 -0700

    add tooling/tooling-prod to auto-tf (#4472)
    * add tooling/tooling-prod to auto-tf
    
    * update comment

--- terraform/tooling/tooling-prod/pipelines.tf
+++ terraform/tooling/tooling-prod/pipelines.tf
@@ -82,3 +82,27 @@ module "imagebuildtoolingprod" {
   auto_tf_pipeline_role_arn = module.main.auto_tf_pipeline_role_arn
   enable_autotf_alarms      = true
 }
+
+# deploy the tooling/tooling-prod target to the tooling-prod account on the main branch
+module "toolingtoolingprod" {
+  region = "us-west-2"
+  source = "../module-pipeline"
+
+  # This is the dir under the terraform dir to tf in identity-devops
+  tf_dir = "tooling/tooling-prod"
+  # This is the gitref to check out in identity-devops
+  gitref = "main"
+  # This is the account to deploy tf_dir into
+  account = "217680906704"
+  # This is needed so that we don't have name collisions
+  recycletest_env_name = "toolingtoolingprod"
+
+  # pass in global config using module composition (https://www.terraform.io/docs/modules/composition.html)
+  auto_tf_vpc_id            = module.main.auto_tf_vpc_id
+  auto_tf_subnet_id         = module.main.auto_tf_subnet_id
+  auto_tf_subnet_arn        = module.main.auto_tf_subnet_arn
+  auto_tf_role_arn          = module.main.auto_tf_role_arn
+  auto_tf_sg_id             = module.main.auto_tf_sg_id
+  auto_tf_bucket_id         = module.main.auto_tf_bucket_id
+  auto_tf_pipeline_role_arn = module.main.auto_tf_pipeline_role_arn
+}
