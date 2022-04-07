907e2fabcd58979864cb13ed5586c79a76647c7b
Vivek Chugh <vivek.chugh@gsa.gov>
2022-04-07 13:06:20 -0400

    Initial Commit - Pinpoint log ship (#4499)

--- terraform/sms/module/main.tf
+++ terraform/sms/module/main.tf
@@ -1,1 +1,12 @@
 data "aws_caller_identity" "current" {}
+
+module "log-ship-to-soc-pinpoint-logs" {
+  source                              = "../../modules/log_ship_to_soc"
+  region                              = data.aws_region.current.name
+  cloudwatch_subscription_filter_name = "log-ship-to-soc"
+  cloudwatch_log_group_name = {
+    "${var.env}-pinpoint-logs" = ""
+  }
+  env_name            = "sms-${var.env}-${data.aws_region.current.name}"
+  soc_destination_arn = "arn:aws:logs:${data.aws_region.current.name}:752281881774:destination:elp-sms-lg-${data.aws_region.current.name}"
+}
