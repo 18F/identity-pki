7d70757b21893a53b2a8ba8265b41a50771ccfa7
Justin Grevich <justin@grevi.ch>
2022-04-19 12:46:58 -0700

    Temporarily turning off ATF in `pt` (#4552)

--- terraform/tooling/tooling/pipelines.tf
+++ terraform/tooling/tooling/pipelines.tf
@@ -242,30 +242,6 @@ module "tspencer" {
   auto_tf_pipeline_role_arn = module.main.auto_tf_pipeline_role_arn
 }
 
-# deploy the pt environment to the sandbox account on the stages/pt branch!
-module "pt" {
-  region = "us-west-2"
-  source = "../module-pipeline"
-
-  # This is the dir under the terraform dir to tf in identity-devops
-  tf_dir = "app"
-  # This is the environment to deploy to
-  env_name = "pt"
-  # This is the gitref to check out in identity-devops
-  gitref = "stages/pt"
-  # This is the account to deploy tf_dir into
-  account = "894947205914"
-
-  # pass in global config using module composition (https://www.terraform.io/docs/modules/composition.html)
-  auto_tf_vpc_id            = module.main.auto_tf_vpc_id
-  auto_tf_subnet_id         = module.main.auto_tf_subnet_id
-  auto_tf_subnet_arn        = module.main.auto_tf_subnet_arn
-  auto_tf_role_arn          = module.main.auto_tf_role_arn
-  auto_tf_sg_id             = module.main.auto_tf_sg_id
-  auto_tf_bucket_id         = module.main.auto_tf_bucket_id
-  auto_tf_pipeline_role_arn = module.main.auto_tf_pipeline_role_arn
-}
-
 # deploy the pt2 environment to the sandbox account on the stages/pt2 branch!
 module "pt2" {
   region = "us-west-2"
