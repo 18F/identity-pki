2b616cb9332178259ba55480d03636ec27b89039
Mitchell Henke <mitchell.henke@gsa.gov>
2022-05-02 12:10:36 -0500

    Add low disk space alert for GitLab (#4599)

--- terraform/modules/newrelic/gitlab.tf
+++ terraform/modules/newrelic/gitlab.tf
@@ -17,3 +17,31 @@ resource "newrelic_synthetics_alert_condition" "gitlab_health" {
   name       = "${var.env_name} gitlab health failure"
   monitor_id = newrelic_synthetics_monitor.gitlab_health[0].id
 }
+
+resource "newrelic_nrql_alert_condition" "disk_space_alert" {
+  count                        = (var.enabled + var.gitlab_enabled) >= 2 ? 1 : 0
+  policy_id                    = newrelic_alert_policy.low[0].id
+  name                         = "${var.env_name}: GitLab LOW Disk Space Alert"
+  violation_time_limit_seconds = 43200
+  aggregation_window           = 60
+  aggregation_method           = "cadence"
+  aggregation_delay            = 120
+
+  nrql {
+    query = "from StorageSample SELECT min(diskFreePercent) where fullHostname like '%${var.env_name}.gitlab%'"
+  }
+
+  critical {
+    operator              = "below"
+    threshold             = 10
+    threshold_duration    = 300
+    threshold_occurrences = "ALL"
+  }
+
+  warning {
+    operator              = "below"
+    threshold             = 15
+    threshold_duration    = 300
+    threshold_occurrences = "ALL"
+  }
+}
