ff0c39381e0d5151264abc7efcd6640322c0ddf5
Tim Spencer <timothy.spencer@gsa.gov>
2022-05-03 10:14:49 -0700

    Get gitlab endpoints working in sandboxes again (#4620)
    * allow gitlab runners to register
    
    * add private dns name to endpoint and verify
    
    * verification needs to be done manually
    
    * terraform fmt strikes again

--- terraform/gitlab/module/privatelink.tf
+++ terraform/gitlab/module/privatelink.tf
@@ -12,8 +12,22 @@ resource "aws_vpc_endpoint_service" "gitlab" {
   acceptance_required        = false
   allowed_principals         = local.principals
   network_load_balancer_arns = [aws_lb.gitlab.arn]
+  private_dns_name           = var.production ? var.root_domain : "gitlab.${var.env_name}.${var.root_domain}"
 
   tags = {
     Name = var.production ? var.root_domain : "gitlab.${var.env_name}.${var.root_domain}"
   }
 }
+
+# XXX we really should do this, but unfortunately, the verification needs to be in
+# the domain level, so for gitlab.login.gov, it needs to live in login.gov, which is
+# unable to be edited from the tooling accounts.
+#
+# resource "aws_route53_record" "gitlab-endpoint-validation" {
+#   allow_overwrite = true
+#   name            = aws_vpc_endpoint_service.gitlab.private_dns_name_configuration[0].name
+#   ttl             = 60
+#   records         = [aws_vpc_endpoint_service.gitlab.private_dns_name_configuration[0].value]
+#   type            = aws_vpc_endpoint_service.gitlab.private_dns_name_configuration[0].type
+#   zone_id         = data.aws_route53_zone.gitlab.zone_id
+# }
