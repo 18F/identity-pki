75258ff6cc6a87d9e13c2fa5c3c95d47ec70b9e8
Paul Hirsch <59626817+pauldoomgov@users.noreply.github.com>
2022-08-01 13:09:17 -0500

    Adjust SMS searches slightly (#5018)

--- terraform/sms/module/cloudwatch-log-queries.tf
+++ terraform/sms/module/cloudwatch-log-queries.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_query_definition" "default" {
   for_each = yamldecode(templatefile("${path.module}/cloudwatch-log-queries.yml", { "env" = var.env }))
 
-  name            = "${var.env}/${each.key}"
+  name            = "login-sms-${var.env}/${each.key}"
   log_group_names = each.value.logs
   query_string    = each.value.query
 }
