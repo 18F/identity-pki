64f877fe18496df44724e1d0c83682e162b4eb83
Paul Hirsch <59626817+pauldoomgov@users.noreply.github.com>
2022-09-23 17:19:19 -0500

    Disable the header regex in dev WAF (#5282)

--- terraform/waf/dev/main.tf
+++ terraform/waf/dev/main.tf
@@ -21,10 +21,10 @@ module "main" {
   region  = "us-west-2"
   enforce = true
 
-  header_block_regex = yamldecode(file("header_block_regex.yml"))
-
   # commenting this out to free up one of our 10(!) available
   # per-account per-region regex pattern sets
+  #header_block_regex = yamldecode(file("header_block_regex.yml"))
+
   #query_block_regex  = ["ExampleStringToBlock"]
 
   waf_alert_actions = ["arn:aws:sns:us-west-2:894947205914:slack-otherevents"]
