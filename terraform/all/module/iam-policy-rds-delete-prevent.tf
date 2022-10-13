7fa4fe733949b3fae1fd4dfc3df6c2dcb0f35ba6
Jonathan Pirro <jonathan.pirro@gsa.gov>
2022-10-13 09:25:07 -0500

    add DB/GlobalCluster statements to rds_delete_prevent (#5359)

--- terraform/all/module/iam-policy-rds-delete-prevent.tf
+++ terraform/all/module/iam-policy-rds-delete-prevent.tf
@@ -12,6 +12,30 @@ data "aws_iam_policy_document" "rds_delete_prevent" {
       "arn:aws:rds:*:*:db:*prod*",
     ]
   }
+  statement {
+    sid    = "AuroraClusterDeletionPrevent"
+    effect = "Deny"
+    actions = [
+      "rds:DeleteDBCluster",
+    ]
+    resources = [
+      "arn:aws:rds:*:*:cluster:*int*",
+      "arn:aws:rds:*:*:cluster:*staging*",
+      "arn:aws:rds:*:*:cluster:*prod*",
+    ]
+  }
+  statement {
+    sid    = "GlobalClusterDeletionPrevent"
+    effect = "Deny"
+    actions = [
+      "rds:DeleteGlobalCluster",
+    ]
+    resources = [
+      "arn:aws:rds:*:*:global-cluster:*int*",
+      "arn:aws:rds:*:*:global-cluster:*staging*",
+      "arn:aws:rds:*:*:global-cluster:*prod*",
+    ]
+  }
 }
 
 resource "aws_iam_policy" "rds_delete_prevent" {
