7935adcbd9d7c58bafdba7cb4dfa471a06aebd62
Peter Chen <116311760+p3ch3n@users.noreply.github.com>
2022-11-02 10:12:23 -0400

    5385/enable redis logs (#5423)
    * Update idp-elasticache.tf
    
    enable redis server-side soft logs
    
    * Update cloudwatch-log.tf
    
    add cloudwatch log group for soft logs
    
    * updated with fmt
    
    Co-authored-by: PeterChen <peterchen@fcoh2j-g1arlvdf.mynetworksettings.com>

--- terraform/app/idp-elasticache.tf
+++ terraform/app/idp-elasticache.tf
@@ -34,6 +34,13 @@ resource "aws_elasticache_replication_group" "idp" {
     log_format       = "text"
     log_type         = "engine-log"
   }
+
+  log_delivery_configuration {
+    destination      = aws_cloudwatch_log_group.elasticache_redis_soft_log.name
+    destination_type = "cloudwatch-logs"
+    log_format       = "text"
+    log_type         = "slow-log"
+  }
 }
 
 ###Multi-AZ redis cluster, used for Attempt API Related Storage. Added 09-05-22
@@ -63,5 +70,12 @@ resource "aws_elasticache_replication_group" "idp_attempts" {
     destination_type = "cloudwatch-logs"
     log_format       = "text"
     log_type         = "engine-log"
+  }
+
+  log_delivery_configuration {
+    destination      = aws_cloudwatch_log_group.elasticache_redis_soft_log.name
+    destination_type = "cloudwatch-logs"
+    log_format       = "text"
+    log_type         = "slow-log"
   }
 }
