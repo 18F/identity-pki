414b7ea6436b51c284b2d06171e16c1f474df157
Peter Chen <116311760+p3ch3n@users.noreply.github.com>
2022-12-05 10:41:35 -0500

    5385/Add redis-reachability tests (#5498)
    * add access tests
    
    * updated with clusterlookup func
    
    * formatted code

---
+++ tests/app_access_to_redis_cluster_test.go
@@ -0,0 +1,70 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	aws_sdk "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/elasticache"
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var redis_cluster_identifier = env_name + "-idp-001"
+
+func GetCluster(t *testing.T, cluster_name string) *elasticache.DescribeCacheClustersOutput {
+	cluster := elasticache.New(session.New())
+	input := &elasticache.DescribeCacheClustersInput{
+		CacheClusterId:    aws_sdk.String(cluster_name),
+		ShowCacheNodeInfo: aws_sdk.Bool(true),
+	}
+
+	result, err := cluster.DescribeCacheClusters(input)
+	require.NoError(t, err)
+
+	return result
+}
+
+// Ensure IDP instance can still access the Redis cluster endpoint
+func TestRedisAccessIdpInstances(t *testing.T) {
+	// Get an instance from the ASG
+	instances := aws.GetInstanceIdsForAsg(t, idp_asg, region)
+	require.NotEmpty(t, instances)
+	firstinstance := instances[0]
+
+	// Get cluster endpoint
+	response := GetCluster(t, redis_cluster_identifier)
+	endpoint := (*response.CacheClusters[0].CacheNodes[0].Endpoint.Address)
+
+	// Attempt to establish connection
+	fmt.Println("Attempting to connect to Redis endpoint " + endpoint + " from IDP instance")
+	cmd := "timeout --preserve-status 5 nc -v -z " + endpoint + " 6379"
+	result := RunCommandOnInstance(t, firstinstance, cmd)
+
+	// Command should return success code
+	fmt.Println(*result.StandardErrorContent)
+	assert.Equal(t, "Success", *result.Status)
+}
+
+// Ensure app instance cannot access the Redis cluster endpoint
+func TestRedisAccessAppInstances(t *testing.T) {
+	// Get an instance from the ASG
+	instances := aws.GetInstanceIdsForAsg(t, app_asg, region)
+	require.NotEmpty(t, instances)
+	firstinstance := instances[0]
+
+	// Get cluster endpoint
+	response := GetCluster(t, redis_cluster_identifier)
+	endpoint := (*response.CacheClusters[0].CacheNodes[0].Endpoint.Address)
+
+	// Attempt to establish connection. Netcat will hang indefinitely, so a 5s timeout is implemented to kill the nc command if a connection isn't established within that timeframe
+	fmt.Println("Attempting to connect to Redis endpoint " + endpoint + " from App instance")
+	cmd := "timeout --preserve-status 5 nc -v -z " + endpoint + " 6379"
+	result := RunCommandOnInstance(t, firstinstance, cmd)
+
+	// Command should fail and return failure status
+	fmt.Println("Unable to establish connection between app instance and Redis endpoint")
+	assert.Equal(t, "Failed", *result.Status)
+}
