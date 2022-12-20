5ae11980263c64df41941a9c86897eca9160079d
Jonathan Pirro <jonathan.pirro@gsa.gov>
2022-12-20 17:24:12 -0600

    AMI update, 2022-12-20 (#5604)

--- terraform/gitlab/foxtrot/main.tf
+++ terraform/gitlab/foxtrot/main.tf
@@ -18,7 +18,7 @@ module "main" {
   region                         = "us-west-2"
   slack_events_sns_hook_arn      = "arn:aws:sns:us-west-2:034795980528:slack-otherevents"
   dr_region                      = "us-east-2"
-  default_ami_id_tooling         = "ami-026d121fc28ba3ad1" # 2022-12-13 Ubuntu 18.04
+  default_ami_id_tooling         = "ami-0a135bfb842ce19d2" # 2022-12-20 Ubuntu 18.04
   route53_id                     = "Z096400532ZFM348WWIAA"
   destination_artifact_accounts  = ["894947205914"] # login-sandbox
   # These are the account IDs who can access this cluster's gitlab service.
--- terraform/gitlab/production/main.tf
+++ terraform/gitlab/production/main.tf
@@ -19,7 +19,7 @@ module "main" {
   region                          = "us-west-2"
   slack_events_sns_hook_arn       = "arn:aws:sns:us-west-2:034795980528:slack-events"
   root_domain                     = "gitlab.login.gov"
-  default_ami_id_tooling          = "ami-09aa67cd03890b2af" # 2022-12-13 Ubuntu 18.04
+  default_ami_id_tooling          = "ami-0f1018968245a966e" # 2022-12-20 Ubuntu 18.04
   route53_id                      = "Z07730471OKZ5T4V8NB2M"
   asg_gitlab_test_runner_desired  = 38
   asg_gitlab_build_runner_desired = 4
