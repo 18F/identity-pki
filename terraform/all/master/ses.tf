538c1d6302015da38a5bd384fc229c1fca870c3c
Sujana Gurung <100624903+sujana09@users.noreply.github.com>
2023-01-05 17:37:36 -0500

    Revert "Create SES identify for Master account and complete verification"

--- terraform/all/master/ses.tf
+++
@@ -1,8 +0,0 @@
-### SES identity for master account###
-module "master_ses" {
-  source = "github.com/18F/identity-terraform//ses_dkim_r53?ref=main"
-
-  domain           = "humans.login.gov"
-  zone_id          = "Z2DA4DCW3GKJVW"
-  ttl_dkim_records = "1800"
-}
