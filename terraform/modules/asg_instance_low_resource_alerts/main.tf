35ff415fbc4355b027b85d24e9b27ff68f51a1cc
Alex Kritikos <alex.kritikos@gsa.gov>
2023-01-24 12:49:41 -0600

    Add dashboard and runbook for asg instance resource use alerts (#5722)
    * Add dashboard for asg instance resource use alerts
    
    * Add runbook link

--- terraform/modules/asg_instance_low_resource_alerts/main.tf
+++ terraform/modules/asg_instance_low_resource_alerts/main.tf
@@ -47,7 +47,7 @@ resource "aws_cloudwatch_metric_alarm" "memory_used" {
   threshold           = var.high_mem_threshold
   statistic           = "Maximum"
   alarm_description   = <<EOM
-${var.alert_handle} An instance in ASG ${var.asg_name} has exceeded ${var.high_mem_threshold}% memory utilization for over 60 seconds. This is a critical alert.
+${var.alert_handle} An instance in ASG ${var.asg_name} has exceeded ${var.high_mem_threshold}% memory utilization for over 60 seconds. This is a critical alert. Dashboard: https://${var.region}.console.aws.amazon.com/cloudwatch/home?region=${var.region}#dashboards:name=${var.asg_name}-instance-resource-use Runbook: https://github.com/18F/identity-devops/wiki/Runbook:-High-Memory-and-Disk-Usage
 EOM
   alarm_actions       = var.alarm_actions
 
@@ -68,7 +68,7 @@ resource "aws_cloudwatch_metric_alarm" "disk_used" {
   threshold           = var.high_disk_threshold
   statistic           = "Maximum"
   alarm_description   = <<EOM
-${var.alert_handle} An instance in ASG ${var.asg_name} has exceeded ${var.high_disk_threshold}% disk space utilization for over 60 seconds. This is a critical alert.
+${var.alert_handle} An instance in ASG ${var.asg_name} has exceeded ${var.high_disk_threshold}% disk space utilization for over 60 seconds. This is a critical alert. Dashboard: https://${var.region}.console.aws.amazon.com/cloudwatch/home?region=${var.region}#dashboards:name=${var.asg_name}-instance-resource-use Runbook: https://github.com/18F/identity-devops/wiki/Runbook:-High-Memory-and-Disk-Usage
 EOM
   alarm_actions       = var.alarm_actions
 
@@ -76,4 +76,62 @@ EOM
     AutoScalingGroupName = var.asg_name
     path                 = each.key
   }
+}
+
+resource "aws_cloudwatch_dashboard" "instance_resource_use" {
+  dashboard_name = "${var.asg_name}-instance-resource-use"
+  dashboard_body = <<EOF
+{
+    "widgets": [
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 0,
+            "width": 10,
+            "height": 14,
+            "properties": {
+                "metrics": [
+                    [ { "expression": "SEARCH('{${var.env_name}/EC2,AutoScalingGroupName,InstanceId,InstanceType,fstype,path} AutoScalingGroupName=\"${var.asg_name}\" MetricName=\"disk_used_percent\"', 'Average', 60)", "label": "", "id": "q1" } ]
+                ],
+                "view": "timeSeries",
+                "stacked": false,
+                "region": "us-west-2",
+                "stat": "Average",
+                "period": 300,
+                "start": "-PT72H",
+                "end": "P0D",
+                "title": "Percentage of disk space used",
+                "yAxis": {
+                    "left": {
+                        "showUnits": false
+                    }
+                }
+            }
+        },
+        {
+            "type": "metric",
+            "x": 10,
+            "y": 0,
+            "width": 10,
+            "height": 14,
+            "properties": {
+                "metrics": [
+                    [ { "expression": "SEARCH('{${var.env_name}/EC2,AutoScalingGroupName,InstanceId,InstanceType} AutoScalingGroupName=\"${var.asg_name}\" MetricName=\"mem_used_percent\"', 'Average', 300)", "id": "e2", "period": 300, "region": "us-west-2" } ]
+                ],
+                "view": "timeSeries",
+                "stacked": false,
+                "region": "us-west-2",
+                "stat": "Average",
+                "period": 300,
+                "title": "Percentage of memory used",
+                "yAxis": {
+                    "left": {
+                        "showUnits": false
+                    }
+                }
+            }
+        }
+    ]
+}
+EOF
 }
