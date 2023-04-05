8dc81b3da30f8a51bd8b8dd61727444c921d417a
Connor McCarthy <Connor.McCarthy@gsa.gov>
2023-04-05 15:24:26 -0400

    Fix error in kms_toggle.rb

--- kitchen/cookbooks/login_dot_gov/recipes/kms_toggle.rb
+++ kitchen/cookbooks/login_dot_gov/recipes/kms_toggle.rb
@@ -1,14 +1,14 @@
 # Add the enable and disable Cloudwatch KMS scripts
 
-file '/usr/local/bin/id-disable-kms' do
-  source 'id-disable-kms'
+cookbook_file '/usr/local/bin/id-disable-kms' do
+  source 'id-cloudwatch-disable-kms'
   owner 'root'
   group 'root'
   mode '0755'
 end
 
-file '/usr/local/bin/id-enable-kms' do
-  source 'id-enable-kms'
+cookbook_file '/usr/local/bin/id-enable-kms' do
+  source 'id-cloudwatch-enable-kms'
   owner 'root'
   group 'root'
   mode '0755'
