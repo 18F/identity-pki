6a8e2fac4aedaa7a73602a47fe972763cfe96bdb
Stephen Shelton <stephen.shelton@gsa.gov>
2023-04-24 12:35:39 -0400

    Adding in validation in code, add data block for hosted zone (#6115)
    * Adding in validation in code, add data block for hosted zone

--- terraform/review-app-eks/acm_wildcard.tf
+++ terraform/review-app-eks/acm_wildcard.tf
@@ -1,3 +1,9 @@
+# Grab the hosted zone ID for our domain
+data "aws_route53_zone" "selected" {
+  name = var.dnszone
+}
+
+# Create our wildcard certificate
 resource "aws_acm_certificate" "wildcard" {
   domain_name       = "*.${var.dnszone}"
   validation_method = "DNS"
@@ -6,4 +12,21 @@ resource "aws_acm_certificate" "wildcard" {
     Name      = "${var.cluster_name}-wildcard"
     Terraform = "true"
   }
+}
+
+# Validate our wildcard certificate
+resource "aws_route53_record" "validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.wildcard.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
+  zone_id = data.aws_route53_zone.selected.zone_id
+  records = [each.value.record]
+  ttl     = 60
 }
