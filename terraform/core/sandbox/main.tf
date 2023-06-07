e6924ed3eb50db5de45db55ab2099667a3ca58ac
Tim Bradley <90272033+NavaTim@users.noreply.github.com>
2023-06-07 10:03:13 -0700

    LG-9287: Enable dev email address for USPS IPPaaS notifications (#6282)
    * LG-9287: Enable dev email address for USPS IPPaaS notifications
    
    * Lint fix

--- terraform/core/sandbox/main.tf
+++ terraform/core/sandbox/main.tf
@@ -51,7 +51,7 @@ module "main" {
   mx_provider                   = "amazon-ses-inbound.us-west-2"
   sandbox_ses_inbound_enabled   = 1
   sandbox_ses_email_users       = ["smoketest-dev", "smoketest-int", "smoketest-staging", "smoketest-prod"]
-  sandbox_ses_usps_enabled_envs = ["joy"]
+  sandbox_ses_usps_enabled_envs = ["joy", "dev"]
   mta_sts_report_mailboxes      = ["tls.reports@gsa.gov", "tls-reports@login.gov"]
   mta_sts_mode                  = "enforce"
 
