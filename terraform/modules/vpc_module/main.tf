b1ad15e1163a7b04f9e8da94c66f43629048bf3e
Sujana Gurung <100624903+sujana09@users.noreply.github.com>
2023-06-15 10:17:38 -0400

    Update terraform/modules/vpc_module/main.tf
    Co-authored-by: Jonathan Pirro <jonathan.pirro@gsa.gov>

--- terraform/modules/vpc_module/main.tf
+++ terraform/modules/vpc_module/main.tf
@@ -106,8 +106,10 @@ resource "aws_route_table" "app" {
 }
 
 resource "aws_route_table_association" "app" {
+  count = var.enable_app ? 1 : 0
+
   for_each       = aws_subnet.app
-  route_table_id = aws_route_table.app[0].id
+  route_table_id = aws_route_table.app[count.index].id
   subnet_id      = each.value.id
 }
 
