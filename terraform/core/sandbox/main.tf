9999bae5f77d75eb679d0637b67a415913bedb71
Paul Hirsch <59626817+pauldoomgov@users.noreply.github.com>
2023-06-30 15:53:28 -0500

    Offboard Paul Hirsch (#6370)
    * ight imma head out
    
    * Do not leave me hanging

--- terraform/core/sandbox/main.tf
+++ terraform/core/sandbox/main.tf
@@ -102,13 +102,11 @@ module "main" {
   cross_account_archive_bucket_access = {
     "arn:aws:iam::217680906704:role/production-build-pool_gitlab_runner_role" = [
       "mhenke",
-      "pauldoom",
       "int",
       "dev"
     ],
     "arn:aws:iam::034795980528:role/alpha-build-pool_gitlab_runner_role" = [
-      "mhenke",
-      "pauldoom"
+      "mhenke"
     ]
   }
 }
