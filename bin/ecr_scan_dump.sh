6d0d070a801d88172a8dd1fecc74fe594d6fa376
Tim Spencer <timothy.spencer@gsa.gov>
2023-07-13 10:01:39 -0700

    remove unneeded make target for tests, update documentation for scan dump script

--- bin/ecr_scan_dump.sh
+++ bin/ecr_scan_dump.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#
+# This script dumps a bunch of scan results of the images that we build/use that
+# can be uploaded to the FedRAMP scan artifacts folder.  They appear in the
+# current directory and are named IMAGENAME-scan.txt.
+#
 
 IMAGES=$(aws s3 cp s3://login-gov.secrets.217680906704-us-west-2/common/gitlab_env_runner_allowed_images - | grep -v 000000000000.dkr.ecr)
 
@@ -7,10 +12,10 @@ for i in $IMAGES ; do
 	REPO=$(echo "$IMAGE" | awk -F@ '{print $1}')
 	SHA=$(echo "$IMAGE" | awk -F@ '{print $2}')
 	IMAGENAME=$(echo "$REPO" | awk -F/ '{print $2}')
-	rm -rf "/tmp/$IMAGENAME-scan.txt"
+	rm -rf "$IMAGENAME-scan.txt"
 	aws ecr describe-image-scan-findings \
 		--repository-name "$REPO" \
 		--image-id imageDigest="$SHA" \
-		--output table > "/tmp/$IMAGENAME-scan.txt"
+		--output table > "$IMAGENAME-scan.txt"
 done
 
