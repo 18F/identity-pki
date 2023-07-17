f3e44f638c5f46736f78e139f03b620de7355b05
Tim Spencer <timothy.spencer@gsa.gov>
2023-07-17 16:41:16 -0700

    add code to update aide db after the chef run is done

---
+++ kitchen/cookbooks/login_dot_gov/recipes/update_aide.rb
@@ -0,0 +1,10 @@
+
+# Run AIDE db update at the end of the chef run, after everything has been installed.
+# Do it in the background, because this takes ~6m to run, and we don't want to delay
+# the host from going into service.
+Chef.event_handler do
+  on :run_completed do
+    pid = spawn("/usr/bin/aide.wrapper --update", :out => "/var/log/aideupdate.out", :err => "/var/log/aideupdate.err")
+    Process.detach(pid)
+  end
+end
