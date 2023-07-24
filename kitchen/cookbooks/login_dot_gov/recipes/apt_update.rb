f14c7736996e941d588fdb94083d07a1a2247606
Tim Spencer <timothy.spencer@gsa.gov>
2023-07-24 14:35:27 -0700

    enable ESM to get latest security fixes

--- kitchen/cookbooks/login_dot_gov/recipes/apt_update.rb
+++ kitchen/cookbooks/login_dot_gov/recipes/apt_update.rb
@@ -1,3 +1,6 @@
+# enable ESM before we update
+execute 'apt install ubuntu-advantage-tools && pro enable esm-apps'
+
 # assure that we're working with an updated package list in case anything is
 # yanked between the time the image is built and an instance is provisioned.
 execute 'apt update'
