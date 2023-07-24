7dda7e3fb1fde658b6d7c344890890dcb51c8444
Tim Spencer <timothy.spencer@gsa.gov>
2023-07-24 14:36:56 -0700

    make recipe more cheffy

--- kitchen/cookbooks/login_dot_gov/recipes/apt_update.rb
+++ kitchen/cookbooks/login_dot_gov/recipes/apt_update.rb
@@ -1,5 +1,6 @@
 # enable ESM before we update
-execute 'apt install ubuntu-advantage-tools && pro enable esm-apps'
+package 'ubuntu-advantage-tools'
+execute 'pro enable esm-apps'
 
 # assure that we're working with an updated package list in case anything is
 # yanked between the time the image is built and an instance is provisioned.
