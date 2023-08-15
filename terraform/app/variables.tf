e8e58a4bdf8c965b6de9b13969771ddf4949a5c8
Jonathan Pirro <jonathan.pirro@gsa.gov>
2023-08-15 16:45:42 -0500

    AMI update, 2023-08-15 (#6515)

--- terraform/app/variables.tf
+++ terraform/app/variables.tf
@@ -574,28 +574,28 @@ variable "bootstrap_private_git_clone_url" {
 #### us-west-2
 
 variable "base_ami_sandbox_uw2" {
-  default     = "ami-024d102efb9d0bfc2" # 2023-08-08 Ubuntu 20.04
+  default     = "ami-0094694c7661b4a8a" # 2023-08-15 Ubuntu 20.04
   description = <<EOM
 us-west-2 AMI ID for 'base' hosts (outboundproxy) in the sandbox account
 EOM
 }
 
 variable "base_ami_prod_uw2" {
-  default     = "ami-0f01ee99f47e70c0f" # 2023-08-08 Ubuntu 20.04
+  default     = "ami-04b4ebf31f45bedc6" # 2023-08-15 Ubuntu 20.04
   description = <<EOM
 us-west-2 AMI ID for 'base' hosts (outboundproxy) in the prod account
 EOM
 }
 
 variable "rails_ami_sandbox_uw2" {
-  default     = "ami-044b9f5a287cc7e86" # 2023-08-08 Ubuntu 20.04
+  default     = "ami-0b753687c1fea12c0" # 2023-08-15 Ubuntu 20.04
   description = <<EOM
 us-west-2 AMI ID for 'rails' hosts (IdP/PIVCAC servers) in the sandbox account
 EOM
 }
 
 variable "rails_ami_prod_uw2" {
-  default     = "ami-019dcb8e0d62ebddd" # 2023-08-08 Ubuntu 20.04
+  default     = "ami-02aae9a879a59c5cc" # 2023-08-15 Ubuntu 20.04
   description = <<EOM
 us-west-2 AMI ID for 'rails' hosts (IdP/PIVCAC servers) in the prod account
 EOM
@@ -617,7 +617,7 @@ variable "ami_id_map_uw2" {
 ##### us-east-1
 
 variable "base_ami_sandbox_ue1" {
-  default     = "ami-0d9aa6475f8a176b8" # 2023-08-08 Ubuntu 20.04
+  default     = "ami-0e0b4847a9dd463cd" # 2023-08-15 Ubuntu 20.04
   description = <<EOM
 us-east-1 AMI ID for 'base' hosts (outboundproxy) in the sandbox account
 EOM
@@ -631,7 +631,7 @@ EOM
 }
 
 variable "rails_ami_sandbox_ue1" {
-  default     = "ami-0a7c500b2cfda9e5e" # 2023-08-08 Ubuntu 20.04
+  default     = "ami-0652d82d476fc3e68" # 2023-08-15 Ubuntu 20.04
   description = <<EOM
 us-east-1 AMI ID for 'rails' hosts (IdP/PIVCAC servers) in the sandbox account
 EOM
