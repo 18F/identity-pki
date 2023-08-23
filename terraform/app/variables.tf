e44c47baaf9fb826a43d386934a2a2c04876adae
Jonathan Pirro <jonathan.pirro@gsa.gov>
2023-08-23 10:36:29 -0500

    AMI update, 2023-08-22 + increase passenger_prewarm_timeout to 45 seconds (#6560)
    * increase passenger_prewarm_timeout to 45 seconds
    
    * AMI update, 2023-08-22 + increase passenger_prewarm_timeout to 45 seconds

--- terraform/app/variables.tf
+++ terraform/app/variables.tf
@@ -575,28 +575,28 @@ variable "bootstrap_private_git_clone_url" {
 #### us-west-2
 
 variable "base_ami_sandbox_uw2" {
-  default     = "ami-0094694c7661b4a8a" # 2023-08-15 Ubuntu 20.04
+  default     = "ami-012b9e82ddb5d7a34" # 2023-08-22 Ubuntu 20.04
   description = <<EOM
 us-west-2 AMI ID for 'base' hosts (outboundproxy) in the sandbox account
 EOM
 }
 
 variable "base_ami_prod_uw2" {
-  default     = "ami-04b4ebf31f45bedc6" # 2023-08-15 Ubuntu 20.04
+  default     = "ami-0edc13efc82d17d3d" # 2023-08-22 Ubuntu 20.04
   description = <<EOM
 us-west-2 AMI ID for 'base' hosts (outboundproxy) in the prod account
 EOM
 }
 
 variable "rails_ami_sandbox_uw2" {
-  default     = "ami-0b753687c1fea12c0" # 2023-08-15 Ubuntu 20.04
+  default     = "ami-026e6e79feffa6dd1" # 2023-08-22 Ubuntu 20.04
   description = <<EOM
 us-west-2 AMI ID for 'rails' hosts (IdP/PIVCAC servers) in the sandbox account
 EOM
 }
 
 variable "rails_ami_prod_uw2" {
-  default     = "ami-02aae9a879a59c5cc" # 2023-08-15 Ubuntu 20.04
+  default     = "ami-06f2db6eefb74d701" # 2023-08-22 Ubuntu 20.04
   description = <<EOM
 us-west-2 AMI ID for 'rails' hosts (IdP/PIVCAC servers) in the prod account
 EOM
@@ -618,7 +618,7 @@ variable "ami_id_map_uw2" {
 ##### us-east-1
 
 variable "base_ami_sandbox_ue1" {
-  default     = "ami-0e0b4847a9dd463cd" # 2023-08-15 Ubuntu 20.04
+  default     = "ami-05424189f1402350e" # 2023-08-22 Ubuntu 20.04
   description = <<EOM
 us-east-1 AMI ID for 'base' hosts (outboundproxy) in the sandbox account
 EOM
@@ -632,7 +632,7 @@ EOM
 }
 
 variable "rails_ami_sandbox_ue1" {
-  default     = "ami-0652d82d476fc3e68" # 2023-08-15 Ubuntu 20.04
+  default     = "ami-015d427a57579400b" # 2023-08-22 Ubuntu 20.04
   description = <<EOM
 us-east-1 AMI ID for 'rails' hosts (IdP/PIVCAC servers) in the sandbox account
 EOM
