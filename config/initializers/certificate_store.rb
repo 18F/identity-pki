6228a71d81c132b8686cdb4397db57b3bf205c99
Mitchell Henke <mitchell.henke@gsa.gov>
2023-08-31 09:38:08 -0500

    move certificate store loading into after_initialize

--- config/initializers/certificate_store.rb
+++ config/initializers/certificate_store.rb
@@ -1,3 +1,5 @@
-unless File.basename($PROGRAM_NAME) == 'rake' && ARGV.any? { |arg| arg.start_with?('db:') }
-  CertificateStore.instance.load_certs!
+Rails.application.config.after_initialize do
+  unless File.basename($PROGRAM_NAME) == 'rake' && ARGV.any? { |arg| arg.start_with?('db:') }
+    CertificateStore.instance.load_certs!
+  end
 end
