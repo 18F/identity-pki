193fa2154092174c96e779ba09d90c38c05977f2
Alex Kritikos <alex.kritikos@gsa.gov>
2023-09-15 10:02:57 -0700

    Add more paths that need to be accessible from outside the VPN. (#6647)
    * Add more paths that need to be accessible from outside the VPN.

--- terraform/tooling/module/waf-sets.tf
+++ terraform/tooling/module/waf-sets.tf
@@ -12,19 +12,23 @@ module "waf_sets" {
 
   restricted_paths = {
     paths = [
-      "^/api.*",
+      "^/-/graphql-explorer.*",
       "^/admin.*",
-      "^/-/graphql-explorer.*"
+      "^/api.*",
     ]
     exclusions = [
-      "^/api/graphql.*",
-      "^/api/v4/users/[0-9]+/status",
-      "^/api/v4/users/[0-9]+",
-      "^/api/v4/projects/[0-9]+/repository/branches",
-      "^/api/v4/projects/[0-9]+/repository/tags",
+      "^/api/v4/analytics/group_activity/issues_count",
+      "^/api/v4/analytics/group_activity/merge_requests_count",
+      "^/api/v4/analytics/group_activity/new_members_count",
       "^/api/v4/projects/[0-9]+/issues/[0-9]+/related_merge_requests",
+      "^/api/v4/users.json",
+      "^/api/graphql.*",
       "^/api/v4/projects/[0-9]+/issues/[0-9]+/award_emoji",
       "^/api/v4/projects/[0-9]+/issues/[0-9]+/related_merge_requests",
+      "^/api/v4/projects/[0-9]+/repository/branches",
+      "^/api/v4/projects/[0-9]+/repository/tags",
+      "^/api/v4/users/[0-9]+",
+      "^/api/v4/users/[0-9]+/status",
     ]
   }
 }
