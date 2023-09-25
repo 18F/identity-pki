f39b291607265b74f87aee3aa9f6485a2001635c
Ryan Brown <86372754+ryan-brown-sb@users.noreply.github.com>
2023-09-25 10:38:36 -0700

    update packages (#6663)

--- kitchen/cookbooks/login_dot_gov/recipes/apt_update.rb
+++ kitchen/cookbooks/login_dot_gov/recipes/apt_update.rb
@@ -1,11 +1,7 @@
 # assure that we're working with an updated package list in case anything is
 # yanked between the time the image is built and an instance is provisioned.
 
-# HACK: on us-east-1 hosts, this must run first so ubuntu-advantage-tools is available
-case Chef::Recipe::AwsMetadata.get_aws_region
-when 'us-east-1'
-  execute 'apt-get -o DPkg::Lock::Timeout=240 update'
-end
+execute 'apt-get -o DPkg::Lock::Timeout=240 update'
 
 # enable ESM and then re-run apt update
 package 'ubuntu-advantage-tools' do
