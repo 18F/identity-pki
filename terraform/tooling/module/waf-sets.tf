0906045e00e9ce9e3ff326781e0a8d2b450c54b5
Jonathan Pirro <jonathan.pirro@gsa.gov>
2023-09-26 11:42:33 -0500

    update Gitlab restricted_paths_exclusions set to <10 patterns (#6658)

--- terraform/tooling/module/waf-sets.tf
+++ terraform/tooling/module/waf-sets.tf
@@ -17,16 +17,11 @@ module "waf_sets" {
       "^/api.*",
     ]
     exclusions = [
-      "^/api/v4/analytics/group_activity/issues_count",
-      "^/api/v4/analytics/group_activity/merge_requests_count",
-      "^/api/v4/analytics/group_activity/new_members_count",
-      "^/api/v4/projects/[0-9]+/issues/[0-9]+/related_merge_requests",
+      "^/api/v4/analytics/group_activity/(issues|merge_requests|new_members)_count",
       "^/api/v4/users.json",
       "^/api/graphql.*",
-      "^/api/v4/projects/[0-9]+/issues/[0-9]+/award_emoji",
-      "^/api/v4/projects/[0-9]+/issues/[0-9]+/related_merge_requests",
-      "^/api/v4/projects/[0-9]+/repository/branches",
-      "^/api/v4/projects/[0-9]+/repository/tags",
+      "^/api/v4/projects/[0-9]+/issues/[0-9]+/(related_merge_requests|award_emoji)",
+      "^/api/v4/projects/[0-9]+/repository/(branches|tags)",
       "^/api/v4/users/[0-9]+",
       "^/api/v4/users/[0-9]+/status",
     ]
