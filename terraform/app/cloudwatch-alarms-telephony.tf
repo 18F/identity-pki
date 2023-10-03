dd14aa9c4f307cd8a5440f947e955e328cbc37a7
Mitchell Henke <mitchell.henke@gsa.gov>
2023-10-03 11:48:35 -0500

    Adjust high SMS volume alarm to require two consecutive periods of high SMS (#6692)

--- terraform/app/cloudwatch-alarms-telephony.tf
+++ terraform/app/cloudwatch-alarms-telephony.tf
@@ -2,8 +2,8 @@
 resource "aws_cloudwatch_metric_alarm" "idp_sms_send_rate_high_alarm" {
   alarm_name                = "${var.env_name}-idp-sms-send-rate-high"
   comparison_operator       = "GreaterThanThreshold"
-  evaluation_periods        = "1"
-  datapoints_to_alarm       = "1"
+  evaluation_periods        = "2"
+  datapoints_to_alarm       = "2"
   metric_name               = "pinpoint-telephony-sms-sent"
   namespace                 = "${var.env_name}/idp-authentication"
   period                    = "60"
