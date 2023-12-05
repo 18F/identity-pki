1578ffeccd5386cfa777212a0d34cb3eea14f457
Mitchell Henke <mitchell.henke@gsa.gov>
2023-12-05 17:12:04 -0600

    Update OBP alarm descriptions from identity-terraform (#6862)

--- terraform/modules/outbound_proxy/cloudwatch-log.tf
+++ terraform/modules/outbound_proxy/cloudwatch-log.tf
@@ -12,11 +12,12 @@ resource "aws_cloudwatch_log_group" "squid_access_log" {
 # creates alarm on total requests following below a threshold
 # requests that notifies to the specified alarm SNS ARN.
 module "outboundproxy_cloudwatch_filters" {
-  source = "github.com/18F/identity-terraform//squid_cloudwatch_filters?ref=49bc02749966cef8ec7f14c4d181a2d3879721fc"
+  source = "github.com/18F/identity-terraform//squid_cloudwatch_filters?ref=e0b546eaef50dee0ae95487672f56a0de051fd33"
   #source = "../../../../identity-terraform/squid_cloudwatch_filters"
   depends_on              = [aws_cloudwatch_log_group.squid_access_log]
   log_group_name_override = aws_cloudwatch_log_group.squid_access_log.name
   env_name                = var.env_name
   alarm_actions           = [var.slack_events_sns_hook_arn] # notify slack on denied requests
   treat_missing_data      = var.cloudwatch_treat_missing_data
+  runbook_url             = "https://github.com/18F/identity-devops/wiki/Runbook%3A-Squid-Outbound-Proxies"
 }
