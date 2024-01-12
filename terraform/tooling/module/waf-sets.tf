341e60c26b7d2474db11ae4ba1d55b57c1e9c7e3
Connor McCarthy <Connor.McCarthy@gsa.gov>
2024-01-12 11:56:15 -0500

    Removing unused changes

--- terraform/tooling/module/waf-sets.tf
+++ terraform/tooling/module/waf-sets.tf
@@ -29,16 +29,4 @@ module "waf_sets" {
       "(?i)readme.md",
     ]
   }
-  
-  relaxed_uri_paths = {
-    "docauth_image_upload"        = "^/api/verify/images"                              # https://github.com/18F/identity-devops/issues/4092
-    "login_form"                  = "^/([a-z]{2}/)?$"                                  # https://github.com/18F/identity-devops/issues/4563
-    "password_screening_flow"     = "^/([a-z]{2}/)?verify/enter_password"              # https://github.com/18F/identity-devops/issues/4563
-    "OIDC_authorization"          = "^/openid_connect/authorize"                       # https://github.com/18F/identity-devops/issues/4563
-    "account_deletion"            = "^/([a-z]{2}/)?account/delete"                     # https://github.com/18F/identity-devops/issues/6127
-    "reauthn"                     = "^/([a-z]{2}/)?reauthn"                            # https://github.com/18F/identity-devops/issues/6221
-    "capture_password"            = "^/([a-z]{2}/)?login/password"                     # https://github.com/18F/identity-devops/issues/6389
-    "reactivate"                  = "^/([a-z]{2}/)?account/reactivate/verify_password" # https://github.com/18F/identity-devops/issues/6389
-    "sign_up_password"            = "^/([a-z]{2}/)?sign_up/create_password"            # https://github.com/18F/identity-devops/issues/6576
-  }
 }
