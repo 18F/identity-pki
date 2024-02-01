dcd0d7e53ab271e0dfcbdb03b045bd0e6538cdf0
Mike Ward <michael.w.ward@gsa.gov>
2024-02-01 10:54:50 -0500

    Update to 3.5.1 (#7013)

--- terraform/app/rds-variables.tf
+++ terraform/app/rds-variables.tf
@@ -181,7 +181,7 @@ EOM
 variable "dms_engine_version" {
   description = "Engine version of the DMS replication instance."
   type        = string
-  default     = "3.4.7"
+  default     = "3.5.1"
 }
 
 variable "enable_dms_analytics" {
