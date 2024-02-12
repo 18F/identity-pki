352ccea6a7eb919a6a99b9355cb9f29c11209d89
Jonathan Pirro <jonathan.pirro@gsa.gov>
2024-02-12 11:05:18 -0600

    add AIServiceRestriction IAM policy + attach to roles (BREAKING) (#7038)

--- terraform/all/module/iam-role-fraudops.tf
+++ terraform/all/module/iam-role-fraudops.tf
@@ -15,6 +15,7 @@ module "fraudops-assumerole" {
     aws_iam_policy.rds_delete_prevent.arn,
     aws_iam_policy.region_restriction.arn,
     var.dnssec_zone_exists ? data.aws_iam_policy.dnssec_disable_prevent[0].arn : "",
+    aws_iam_policy.ai_service_restriction.arn,
   ])
 
   iam_policies = [
