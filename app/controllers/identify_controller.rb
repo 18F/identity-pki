7e114a1cb186b18ee67aeb0e484e86074e9677a6
Jack Cody <johnjcody@fcoh2j-g1bzlvdf.gsa.gov>
2024-03-13 08:46:26 -0500

    Reverse prior commit

--- app/controllers/identify_controller.rb
+++ app/controllers/identify_controller.rb
@@ -121,9 +121,7 @@ class IdentifyController < ApplicationController
     }
 
     attributes.delete(:issuer) if validation_result == 'self-signed cert'
-    if valid
-      attributes[:matched_policy_oids] = cert.matched_policy_oids.map { |item| [item, true] }.to_h
-    end
+    attributes[:matched_policy_oids] = cert.matched_policy_oids if valid
 
     # Log certificate if it fails either OpenSSL validation, but passes our current validation or vice versa
     if valid != login_certs_openssl_result[:valid] || valid != ficam_certs_openssl_result[:valid]
