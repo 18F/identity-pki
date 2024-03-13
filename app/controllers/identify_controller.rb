61e21a299c7522dcaf060410b086e8f5d013f405
Jack Cody <johnjcody@fcoh2j-g1bzlvdf.gsa.gov>
2024-03-13 08:38:29 -0500

    Change payload format of matched_policy_oids

--- app/controllers/identify_controller.rb
+++ app/controllers/identify_controller.rb
@@ -121,7 +121,9 @@ class IdentifyController < ApplicationController
     }
 
     attributes.delete(:issuer) if validation_result == 'self-signed cert'
-    attributes[:matched_policy_oids] = cert.matched_policy_oids if valid
+    if valid
+      attributes[:matched_policy_oids] = cert.matched_policy_oids.map { |item| [item, true] }.to_h
+    end
 
     # Log certificate if it fails either OpenSSL validation, but passes our current validation or vice versa
     if valid != login_certs_openssl_result[:valid] || valid != ficam_certs_openssl_result[:valid]
