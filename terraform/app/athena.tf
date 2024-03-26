53c0fb10ade360a1a207c96f6385aaec421e49f7
sujanagurung <sujana.gurung@gsa.gov>
2024-03-25 23:38:54 -0400

    Adding saved queries for athena

--- terraform/app/athena.tf
+++ terraform/app/athena.tf
@@ -3,6 +3,19 @@ module "athena_environment_database" {
   env_name     = var.env_name
   region       = var.region
   apps_enabled = var.apps_enabled
+  queries = [{
+    name  = "Total Requests by HTTP Status Code"
+    query = "SELECT status, COUNT(*) AS request_count FROM ${var.env_name}_cloudfront_logs GROUP BY status ORDER BY request_count DESC;"
+    },
+    {
+      name  = "Top Requested URLs"
+      query = "SELECT uri, COUNT(*) AS request_count FROM ${var.env_name}_cloudfront_logs GROUP BY uri ORDER BY request_count DESC;"
+    },
+    {
+      name  = "Total Data Transferred"
+      query = "SELECT SUM(bytes) AS total_bytes_sent FROM ${var.env_name}_cloudfront_logs;"
+  }]
+
 }
 
 moved {
--- terraform/modules/athena_database/database.tf
+++ terraform/modules/athena_database/database.tf
@@ -10,3 +10,10 @@ resource "aws_athena_database" "logs_database" {
 output "database" {
   value = aws_athena_database.logs_database
 }
+
+resource "aws_athena_named_query" "cloudfront_logs_saved_query" {
+  for_each = { for idx, val in var.queries : idx => val }
+  name     = each.value.name
+  database = aws_athena_database.logs_database.name
+  query    = each.value.query
+}
