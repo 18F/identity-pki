cf6862ff3308419746b0bf45dc1dfdbb75f1289a
Zach Margolis <zachmargolis@users.noreply.github.com>
2024-04-26 12:00:33 -0700

    Use shared Identity::Hostdata.config reading config values (#449)
    * Use shared Identity::Hostdata.config reading config values
    * fix key_types reference

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -1,66 +1,10 @@
-class IdentityConfig
-  class << self
-    attr_reader :store, :key_types
+module IdentityConfig
+  def self.store
+    Identity::Hostdata.config
   end
 
-  CONVERTERS = {
-    # Allows loading a string configuration from a system environment variable
-    # ex: To read DATABASE_HOST from system environment for the database_host key
-    # database_host: ['env', 'DATABASE_HOST']
-    # To use a string value directly, you can specify a string explicitly:
-    # database_host: 'localhost'
-    string: proc do |value|
-      if value.is_a?(Array) && value.length == 2 && value.first == 'env'
-        ENV.fetch(value[1])
-      elsif value.is_a?(String)
-        value
-      else
-        raise 'invalid system environment configuration value'
-      end
-    end,
-    comma_separated_string_list: proc do |value|
-      value.split(',')
-    end,
-    integer: proc do |value|
-      Integer(value)
-    end,
-    json: proc do |value, options: {}|
-      JSON.parse(value, symbolize_names: options[:symbolize_names])
-    end,
-    boolean: proc do |value|
-      case value
-      when 'true', true
-        true
-      when 'false', false
-        false
-      else
-        raise 'invalid boolean value'
-      end
-    end,
-  }
-
-  attr_reader :key_types
-
-  def initialize(read_env)
-    @read_env = read_env
-    @written_env = {}
-    @key_types = {}
-  end
-
-  def add(key, type: :string, is_sensitive: false, options: {})
-    value = @read_env[key]
-    raise "#{key} is required but is not present" if value.nil?
-    converted_value = CONVERTERS.fetch(type).call(value, options: options)
-    raise "#{key} is required but is not present" if converted_value.nil?
-
-    @key_types[key] = type
-
-    @written_env[key] = converted_value
-    @written_env
-  end
-
-  def self.build_store(config_map)
-    config = IdentityConfig.new(config_map)
+  # rubocop:disable Metrics/BlockLength
+  CONFIG_BUILDER = proc do |config|
     config.add(:aws_http_timeout, type: :integer)
     config.add(:aws_region)
     config.add(:ca_issuer_host_allow_list, type: :comma_separated_string_list)
@@ -96,10 +40,7 @@ class IdentityConfig
     config.add(:token_encryption_key_salt)
     config.add(:token_encryption_key_pepper_old)
     config.add(:token_encryption_key_salt_old)
-    final_env = config.add(:trusted_ca_root_identifiers, type: :comma_separated_string_list)
-
-    @key_types = config.key_types
-    @store = RedactedStruct.new('IdentityConfig', *final_env.keys, keyword_init: true).
-      new(**final_env)
-  end
+    config.add(:trusted_ca_root_identifiers, type: :comma_separated_string_list)
+  end.freeze
+  # rubocop:enable Metrics/BlockLength
 end
