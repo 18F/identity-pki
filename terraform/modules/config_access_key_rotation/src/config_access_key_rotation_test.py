e279d7535a3ca029ad75406f1cfc1b871e470c68
ryan.d.brown <ryan.d.brown@gsa.gov>
2024-04-30 18:37:20 +0000

    Do Not Rotate Keys for SES SMTP User

--- terraform/modules/config_access_key_rotation/src/config_access_key_rotation_test.py
+++ terraform/modules/config_access_key_rotation/src/config_access_key_rotation_test.py
@@ -7,21 +7,27 @@ from .config_access_key_rotation import lambda_handler
 
 
 class TestConfigAccessKeyRotation(unittest.TestCase):
-    @mock.patch.dict(
-        os.environ,
-        {
-            "AWS_DEFAULT_REGION": "us-west-2",
-            "RotationPeriod": "80",
-            "InactivePeriod": "90",
-            "RetentionPeriod": "100",
-            "temp_role_arn": "temp:role:arn:12345676890",
-        },
-        clear=True,
-    )
+    ENV_VARS = {
+        "AWS_DEFAULT_REGION": "us-west-2",
+        "RotationPeriod": "80",
+        "InactivePeriod": "90",
+        "RetentionPeriod": "100",
+        "temp_role_arn": "temp:role:arn:12345676890",
+    }
+
+    @mock.patch.dict(os.environ, ENV_VARS, clear=True)
     @mock.patch("builtins.print")
     @mock_aws
     def test_lambda_handler(self, patched_print):
-        self.mock_iam()
+        self.mock_iam(
+            users=[
+                {
+                    "UserName": "user1",
+                    "Tags": [{"Key": "email", "Value": "user.dot.one@gsa.gov"}],
+                },
+                {"UserName": "user2"},
+            ],
+        )
 
         lambda_handler(event={}, context={})
 
@@ -37,6 +43,22 @@ class TestConfigAccessKeyRotation(unittest.TestCase):
             "falls back to username email",
         )
 
+    @mock.patch.dict(os.environ, ENV_VARS, clear=True)
+    @mock.patch("builtins.print")
+    @mock_aws
+    def test_lambda_handler_skips_users(self, patched_print):
+        self.mock_iam(
+            users=[{"UserName": "ses-smtp"}],
+        )
+
+        lambda_handler(event={}, context={})
+
+        self.assertPrinted(
+            patched_print,
+            "Skipping user from ignore list UserName=ses-smtp",
+            "skips SES user",
+        )
+
     def assertPrinted(self, patched_print, expected, message):
         did_print = None
         try:
@@ -50,10 +72,8 @@ class TestConfigAccessKeyRotation(unittest.TestCase):
 
         self.assertIsNotNone(did_print, message)
 
-    def mock_iam(self):
+    def mock_iam(self, users):
         iam = boto3.client("iam")
 
-        iam.create_user(
-            UserName="user1", Tags=[{"Key": "email", "Value": "user.dot.one@gsa.gov"}]
-        )
-        iam.create_user(UserName="user2")
+        for user in users:
+            iam.create_user(**user)
