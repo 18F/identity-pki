21664eb6754c20ee87e46ab80e1054cfe2062419
henry.richardson <henry.richardson@gsa.gov>
2024-07-18 21:04:25 +0000

    Unused IAM Permission Scanner

--- terraform/all/module/iamaa.tf
+++ terraform/all/module/iamaa.tf
@@ -1,3 +1,14 @@
 resource "aws_accessanalyzer_analyzer" "iam_access_analyzer" {
   analyzer_name = "IAMAccessAnalyzer"
 }
+
+resource "aws_accessanalyzer_analyzer" "unused_access" {
+  analyzer_name = "IAMUnusedAccess"
+  type          = "ACCOUNT_UNUSED_ACCESS"
+
+  configuration {
+    unused_access {
+      unused_access_age = 90
+    }
+  }
+}
