506523b9ba5c4202fd977f9f2fc2852a60cf6c59
henry.richardson <henry.richardson@gsa.gov>
2024-08-01 16:53:18 -0400

    Security Hub Standards

--- terraform/all/module/security-config.tf
+++ terraform/all/module/security-config.tf
@@ -224,3 +224,18 @@ resource "aws_config_configuration_recorder_status" "default" {
 resource "aws_securityhub_account" "default" {
   depends_on = [aws_config_configuration_recorder_status.default]
 }
+
+resource "aws_securityhub_standards_subscription" "nist_800_53" {
+  depends_on    = [aws_securityhub_account.default]
+  standards_arn = "arn:aws:securityhub:${var.region}::standards/nist-800-53/v/5.0.0"
+}
+
+resource "aws_securityhub_standards_subscription" "cis" {
+  depends_on    = [aws_securityhub_account.default]
+  standards_arn = "arn:aws:securityhub:${var.region}::standards/cis-aws-foundations-benchmark/v/3.0.0"
+}
+
+resource "aws_securityhub_standards_subscription" "foundational_best_practices" {
+  depends_on    = [aws_securityhub_account.default]
+  standards_arn = "arn:aws:securityhub:${var.region}::standards/aws-foundational-security-best-practices/v/1.0.0"
+}
