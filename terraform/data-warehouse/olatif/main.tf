87bf14c9086d32ab10e3b3a46c48c6a95a21618e
osman.latif <osman.latif@gsa.gov>
2024-08-21 14:52:18 +0000

    Olatif Environment

---
+++ terraform/data-warehouse/olatif/main.tf
@@ -0,0 +1,28 @@
+locals {
+  region     = "us-west-2"
+  account_id = "487317109730"
+}
+
+provider "aws" {
+  region              = local.region
+  allowed_account_ids = [local.account_id] # require login-analytics-sandbox
+}
+
+data "aws_caller_identity" "current" {
+}
+
+terraform {
+  backend "s3" {
+  }
+}
+
+module "data_warehouse" {
+  source                             = "../module"
+  env_name                           = "olatif"
+  bootstrap_main_git_ref_default     = "stages/olatif"
+  slack_events_sns_hook_arn          = "arn:aws:sns:us-west-2:${data.aws_caller_identity.current.account_id}:slack-otherevents"
+  slack_alarms_sns_hook_arn          = "arn:aws:sns:us-west-2:${data.aws_caller_identity.current.account_id}:slack-otherevents"
+  additional_low_priority_sns_topics = ["arn:aws:sns:us-west-2:${data.aws_caller_identity.current.account_id}:slack-data-warehouse-events"]
+  use_spot_instances                 = 1
+  autoscaling_schedule_name          = "dailyzero_normal"
+}
