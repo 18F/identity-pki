73ea8e2e301e28d4b370f987d22a82f0bc9720e3
lily.rappaport <lily.rappaport@gsa.gov>
2024-09-17 17:46:50 +0000

    Add SG rule to gitlab runners to allow ec2 us-west-2

--- terraform/modules/gitlab_runner_pool/net.tf
+++ terraform/modules/gitlab_runner_pool/net.tf
@@ -1,3 +1,8 @@
+data "aws_ip_ranges" "ec2_argo" {
+  regions  = ["us-west-2"]
+  services = ["ec2"]
+}
+
 resource "aws_security_group" "gitlab_runner" {
   description = "gitlab runner security group"
 
@@ -61,6 +66,32 @@ resource "aws_security_group" "gitlab_runner" {
 
   tags = {
     Name = "${var.name}-gitlab_runner_security_group-${var.env_name}"
+    role = "gitlab"
+  }
+
+  vpc_id = var.aws_vpc
+}
+
+# first security group ran out of rules so expand to another SG
+resource "aws_security_group" "gitlab_runner_2" {
+  description = "gitlab runner security group 2"
+
+  # argo load balancers are managed by aws and the ip addresses could be
+  # anything, this is the least obtrusive way we could find to do this without
+  # setting up privatelink or vpc peering (which may not work or could change if
+  # load balancer configs change out from under us anyway)
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = data.aws_ip_ranges.ec2_argo.cidr_blocks
+    description = "Allow connection to ec2 (argocd)"
+  }
+
+  name_prefix = "${var.name}-gitlabrunner2-${var.env_name}"
+
+  tags = {
+    Name = "${var.name}-gitlab_runner_security_group2-${var.env_name}"
     role = "gitlab"
   }
 
