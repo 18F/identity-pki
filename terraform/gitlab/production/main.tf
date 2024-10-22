e54ca86ea723fd305b5aa8d938676df431d2c73c
bleachbyte <jonathan.pirro@gsa.gov>
2024-10-22 15:49:28 -0500

    AMI update, 2024-10-22
    | ACCOUNT           | ID                    | DATE       | ROLE  | VERSION      | REGION    |
    | -----             | -----                 | -----      | ----- | -----        | -----     |
    | sandbox           | ami-0946ba32da9ac10fe | 2024-10-22 | base  | Ubuntu 20.04 | us-west-2 |
    | sandbox           | ami-01e8a4585607d75b3 | 2024-10-22 | rails | Ubuntu 20.04 | us-west-2 |
    | sandbox           | ami-003d9273d49cc2fed | 2024-10-22 | base  | Ubuntu 20.04 | us-east-1 |
    | sandbox           | ami-016b51ca47187fb48 | 2024-10-22 | rails | Ubuntu 20.04 | us-east-1 |
    | prod              | ami-0fb45f6f2ed5d42d2 | 2024-10-22 | base  | Ubuntu 20.04 | us-west-2 |
    | prod              | ami-0ae1450763ecf4602 | 2024-10-22 | rails | Ubuntu 20.04 | us-west-2 |
    | analytics-sandbox | ami-065e4f73a920e7f78 | 2024-10-22 | base  | Ubuntu 20.04 | us-west-2 |
    | analytics-sandbox | ami-064f8c7dc72db8b27 | 2024-10-22 | rails | Ubuntu 20.04 | us-west-2 |
    | tooling-prod      | ami-0f1df980988a740d0 | 2024-10-22 | base  | Ubuntu 20.04 | us-west-2 |

--- terraform/gitlab/production/main.tf
+++ terraform/gitlab/production/main.tf
@@ -24,7 +24,7 @@ module "main" {
   region                            = "us-west-2"
   slack_events_sns_hook_arn         = "arn:aws:sns:us-west-2:217680906704:slack-events"
   root_domain                       = "gitlab.login.gov"
-  default_ami_id                    = "ami-0b05e23b1a5aa0868" # 2024-10-15 Ubuntu 20.04
+  default_ami_id                    = "ami-0f1df980988a740d0" # 2024-10-22 Ubuntu 20.04
   route53_id                        = "Z07730471OKZ5T4V8NB2M"
   asg_gitlab_test_runner_desired    = 38
   asg_gitlab_build_runner_desired   = 4
