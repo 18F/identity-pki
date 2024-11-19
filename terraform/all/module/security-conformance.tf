51d2276770011a7853bc5b672ede1f4ec5a65f8c
henry.richardson <henry.richardson@gsa.gov>
2024-11-19 19:46:52 +0000

    Initial deprecation config_fedramp_conformance module

--- terraform/all/module/security-conformance.tf
+++ terraform/all/module/security-conformance.tf
@@ -1,5 +1,5 @@
 module "config_fedramp_conformance" {
-  source = "github.com/18F/identity-terraform//config_fedramp_conformance?ref=6cdd1037f2d1b14315cc8c59b889f4be557b9c17"
-  #source = "../../../../identity-terraform/config_fedramp_conformance"
+  source = "github.com/18F/identity-terraform//config_fedramp_conformance?ref=6e34b32c19fdddfdf21a9c71a82f025181238207"
+  #source     = "../../../../identity-terraform/config_fedramp_conformance"
   depends_on = [aws_config_configuration_recorder.default]
 }
