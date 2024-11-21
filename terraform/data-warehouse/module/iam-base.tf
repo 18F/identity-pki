14d3c144a51e1cbd24b6fe4b4dea4ad661a6d6f2
henry.richardson <henry.richardson@gsa.gov>
2024-11-21 16:19:11 +0000

    Data-Warehouse: Remove application-secret role

--- terraform/data-warehouse/module/iam-base.tf
+++ terraform/data-warehouse/module/iam-base.tf
@@ -130,21 +130,3 @@ data "aws_iam_policy_document" "flow_policy" {
     ]
   }
 }
-
-# Role that allows for download of application secrets from the application
-# secrets.
-#
-# This allows the application team to manage their own secrets, and decouples
-# config changes there from the infrastructure.
-resource "aws_iam_role" "application-secrets" {
-  name               = "${var.env_name}-application-secrets"
-  description        = "Allows retrieval of application secrets from AWS S3."
-  assume_role_policy = data.aws_iam_policy_document.assume_role_from_vpc.json
-}
-
-# Role policy that associates it with the application_secrets_role_policy
-resource "aws_iam_role_policy" "application-secrets" {
-  name   = "${var.env_name}-application-secrets"
-  role   = aws_iam_role.application-secrets.id
-  policy = data.aws_iam_policy_document.application_secrets_role_policy.json
-}
