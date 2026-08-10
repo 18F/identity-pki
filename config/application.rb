04f24717109c585774db841849bc51ebf6b90839
Malick DiarrA <malick.diarra@gsa.gov>
2026-08-10 15:16:25 -0400

    update to 8.1

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,20 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'active_model/railtie'
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_view/railtie'
-require 'identity/logging/railtie'
-require_relative '../lib/identity_config'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+# require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+# require "rails/test_unit/railtie"
+require "identity/logging/railtie"
+require_relative "../lib/identity_config"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,7 +23,7 @@ Bundler.require(*Rails.groups)
 module IdentityPki
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 8.1
 
     Identity::Hostdata.load_config!(
       app_root: Rails.root,
@@ -23,11 +32,15 @@ module IdentityPki
       &IdentityConfig::CONFIG_BUILDER
     )
 
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
