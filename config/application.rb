7872aa932f75b9ad157a14a36ecd9ea9aa17416b
Malick DiarrA <malick.diarra@gsa.gov>
2026-08-11 14:56:54 -0400

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,20 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
+require 'active_model/railtie'
 # require "active_job/railtie"
-require "active_record/railtie"
+require 'active_record/railtie'
 # require "active_storage/engine"
-require "action_controller/railtie"
+require 'action_controller/railtie'
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require "action_view/railtie"
+require 'action_view/railtie'
 # require "action_cable/engine"
 # require "rails/test_unit/railtie"
-require "identity/logging/railtie"
-require_relative "../lib/identity_config"
+require 'identity/logging/railtie'
+require_relative '../lib/identity_config'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,7 +23,7 @@ Bundler.require(*Rails.groups)
 module IdentityPki
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.1
+    config.load_defaults 7.0
 
     Identity::Hostdata.load_config!(
       app_root: Rails.root,
